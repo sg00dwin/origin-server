c40114b5df565642755ced194cbbbb3ab6669542
Mike McGrath <mmcgrath@redhat.com>
2010-12-21 12:02:25 -0600

    removed stderr->stdout errors and changed create_http to more advanced creation method

--- create_http.php
+++ create_http.php
@@ -3,6 +3,20 @@
 $customer_name = escapeshellarg(filter_var($_POST['username'], FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FLAG_STRIP_LOW));
 $application = escapeshellarg(filter_var($_POST['application'], FILTER_SANITIZE_STRING));
 
-exec("/usr/sbin/mc-rpc -I mserver.cloud.redhat.com libra create_http customer='$customer_name' application='$application' 2>&1", $out);
-print_r($out);
+function my_exec($cmd, $input='')
+         {$proc=proc_open($cmd, array(0=>array('pipe', 'r'), 1=>array('pipe', 'w'), 2=>array('pipe', 'w')), $pipes);
+          fwrite($pipes[0], $input);fclose($pipes[0]);
+          $stdout=stream_get_contents($pipes[1]);fclose($pipes[1]);
+          $stderr=stream_get_contents($pipes[2]);fclose($pipes[2]);
+          $rtn=proc_close($proc);
+          return array('stdout'=>$stdout,
+                       'stderr'=>$stderr,
+                       'return'=>$rtn
+                      );
+         }
+
+$results = my_exec("/usr/sbin/mc-rpc --np -I mserver.cloud.redhat.com libra create_http customer='$customer_name' application='$application'", $out);
+print_r("\nstdout: " . $results['stdout']);
+print_r("\nstderr: " . $results['stderr']);
+print_r("\nreturn: " . $results['return']);
 ?>
