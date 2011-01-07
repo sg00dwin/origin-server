b07680b21328af3611ea37e29ce7cc30e8706535
Mike McGrath <mmcgrath@redhat.com>
2011-01-07 09:30:37 -0600

    trying error response

--- php/cartridge_do.php
+++ php/cartridge_do.php
@@ -3,7 +3,6 @@
 $cartridge = escapeshellarg(filter_var($_POST['cartridge'], FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FLAG_STRIP_LOW));
 $action = escapeshellarg(filter_var($_POST['action'], FILTER_SANITIZE_EMAIL, FILTER_FLAG_STRIP_LOW));
 $args = escapeshellarg(filter_var($_POST['args'], FILTER_SANITIZE_STRING));
-header('HTTP/1.1 500 Internal Server Error', 500)
 
 function my_exec($cmd, $input='')
          {$proc=proc_open($cmd, array(0=>array('pipe', 'r'), 1=>array('pipe', 'w'), 2=>array('pipe', 'w')), $pipes);
@@ -18,6 +17,9 @@ function my_exec($cmd, $input='')
          }
 
 $results = my_exec("/usr/sbin/mc-rpc --np -I 'ip-10-101-6-42' libra create_customer cartridge='$cartridge' action='$action' args='$args'", $out);
+if($results['return'] != 0) {
+    header('HTTP/1.1 500 Internal Server Error', 500)
+}
 print_r("\nstdout: " . $results['stdout']);
 print_r("\nstderr: " . $results['stderr']);
 print_r("\nreturn: " . $results['return']);
