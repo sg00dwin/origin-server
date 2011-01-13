7233b85dd38120a01c6c61e3e2ee8605ea7705cb
Matt Hicks <mhicks@redhat.com>
2011-01-13 00:51:04 -0500

    Adding more cucumber tests

--- backend/controller/features/step_definitions/libra_steps.rb
+++ backend/controller/features/step_definitions/libra_steps.rb
@@ -16,3 +16,30 @@ end
 
 Then /^I should get an exception$/ do
 end
+
+Given /^a newly created user$/ do
+  user = "match"
+
+  while user do
+    # Generate a random username
+    chars = ("1".."9").to_a
+    username = "test" + Array.new(5, '').collect{chars[rand(chars.size)]}.join
+    user = User.find(username)
+  end
+
+  @user = User.create(username, 'blah', 'blah@example.org')
+end
+
+When /^I look up that user$/ do
+  @user = User.find(@user.username)
+end
+
+Then /^he should have no servers$/ do
+  @user.servers.should be_empty
+end
+
+Then /^he should have no applications$/ do
+  @user.apps.should be_empty
+  @user.apps_by_server.should be_empty
+end
+
--- backend/controller/features/test.feature
+++ backend/controller/features/test.feature
@@ -4,3 +4,9 @@ Feature: Setup a new application
     Given an existing 'mhicks' user
     When I try to create a 'mhicks' user
     Then I should get an exception
+
+  Scenario:
+    Given a newly created user
+    When I look up that user
+    Then he should have no servers
+    And he should have no applications
