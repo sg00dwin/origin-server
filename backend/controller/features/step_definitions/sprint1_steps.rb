559762cfcb5f896328366b5476b41c590e253ef0
Matt Hicks <mhicks@redhat.com>
2011-01-18 22:09:24 -0500

    Adding some threading tests for users

--- backend/controller/features/step_definitions/sprint1_steps.rb
+++ backend/controller/features/step_definitions/sprint1_steps.rb
@@ -1,4 +1,4 @@
-require 'pp'
+require 'thread'
 
 Given /^the libra client tools$/ do
   File.exists?("/usr/bin/libra_create_app").should be_true
@@ -6,11 +6,58 @@ Given /^the libra client tools$/ do
   File.exists?("/etc/libra/client.conf").should be_true
 end
 
-When /^(\d+) new customers are created$/ do |num_customers|
-  puts "Created #{num_customers}"
+When /^(\d+) new users are created$/ do |num_users|
+  mutex = Mutex.new
+
+  runs = 0
+  failed = false
+  quit = false
+  r_usernames = []
+
+  # Create users with multiple threads
+  threads = @@THREADS.times.collect do
+    Thread.new do
+      loop do
+        username = nil
+        mutex.synchronize do
+          # Increment the number of runs
+          runs += 1
+
+          # Break if thread has failed or we hit the limit
+          Thread.exit if failed or runs >= num_users.to_i
+
+          # Safely get the next username
+          username = get_unique_username(1, r_usernames)
+
+          # Add it to the reserved list
+          r_usernames << username
+        end
+
+
+        # Create the user
+        begin
+          run("/usr/bin/libra_create_customer -u #{username} -e nobody@example.com") if username
+        rescue
+          mutex.synchronize { failed = true }
+        end
+
+        mutex.synchronize do
+          # Remove the username from the reserved list
+          r_usernames.delete(username)
+        end
+
+        # Verify the user was created
+        @@logger.info("Looking up user: #{username}")
+        User.find(username).should_not be_nil
+      end
+    end
+  end
+
+  threads.each {|t| t.join }
 end
 
-When /^(\d+) applications of type '(.+)' are created per customer$/ do |num_apps, framework|
+When /^(\d+) applications of type '(.+)' are created per user$/ do |num_apps, framework|
+  @apps = (num_apps.to_i).times.collect{|num| "app#{num}" }
   puts "Num apps #{num_apps}"
   puts "Framework #{framework}"
 end
