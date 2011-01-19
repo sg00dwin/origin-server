5b7742a9d8a10a898a8b4d37318c44b92bfc94d4
Matt Hicks <mhicks@redhat.com>
2011-01-19 10:34:46 -0500

    Cleaning up the test code around forking

--- backend/controller/features/step_definitions/sprint1_steps.rb
+++ backend/controller/features/step_definitions/sprint1_steps.rb
@@ -1,10 +1,16 @@
 require 'net/http'
 require 'uri'
+include Libra::Test::User
+include Libra::Test::Util
 
 Given /^the libra client tools$/ do
   File.exists?("/usr/bin/libra_create_app").should be_true
   File.exists?("/usr/bin/libra_create_customer").should be_true
   File.exists?("/etc/libra/client.conf").should be_true
+end
+
+Given /^(\d+) concurrent processes$/ do |max_processes|
+  @max_processes = max_processes.to_i
 end
 
 When /^(\d+) new users are created$/ do |num_users|
@@ -16,36 +22,9 @@ When /^(\d+) new users are created$/ do |num_users|
     @usernames << get_unique_username(1, @usernames)
   end
 
-  # Copy the array of usernames for the fork processing
-  f_usernames = Array.new(@usernames)
-
-  # Create the users in subprocesses
-  loop do
-    # Don't fork more than 10 processes at a time
-    @@MAX_PROC.times do
-      # Pop a username off the array
-      username = f_usernames.pop
-
-      # Process the username in a subprocess
-      fork do
-        @@logger.info("Fork Creating user #{username}")
-        run("/usr/bin/libra_create_customer -u #{username} -e nobody@example.com")
-      end if username
-    end
-
-    # Wait for the 10 processes to finish
-    @@MAX_PROC.times do
-      begin
-        Process.wait
-        exit_code = $?.exitstatus
-        raise "User creation failed" if exit_code != 0
-      rescue
-        break
-      end
-    end
-
-    # Break if we've processed all the usernames
-    break if f_usernames.empty?
+  # Fork off the creation of usernames
+  fork_cmd(@usernames, @max_processes) do |username|
+    run("/usr/bin/libra_create_customer -u #{username} -e nobody@example.com")
   end
 end
 
@@ -55,39 +34,10 @@ When /^(\d+) applications of type '(.+)' are created per user$/ do |num_apps, fr
   # Generate the 'product' of username / app combinations
   user_apps = @usernames.product(@apps)
 
-  # Create the users in subprocesses
-  loop do
-    # Don't fork more than 10 processes at a time
-    @@MAX_PROC.times do
-      # Pop a user_app tuple off the array
-      user_app = user_apps.pop
-
-      # Process the username in a subprocess
-      fork do
-        # Parse the tuple
-        username = user_app[0]
-        app = user_app[1]
-
-        # Create the application
-        @@logger.info("Fork application creation #{username} / #{app}")
-        run("/usr/bin/libra_create_app -u #{username} -a #{app} -r /tmp/#{username}_#{app}_repo -t php-5.3.2 -b")
-      end if user_app
-    end
-
-    # Wait for the 10 processes to finish
-    @@MAX_PROC.times do
-      begin
-        @@logger.info("Waiting for processes to complete")
-        Process.wait
-        exit_code = $?.exitstatus
-        raise "App creation failed" if exit_code != 0
-      rescue
-        break
-      end
-    end
-
-    # Break if we've processed all the usernames
-    break if user_apps.empty?
+  # Fork off the creation of apps
+  fork_cmd(user_apps, @max_processes) do |user_app|
+    username, app = user_app[0], user_app[1]
+    run("/usr/bin/libra_create_app -u #{username} -a #{app} -r /tmp/#{username}_#{app}_repo -t php-5.3.2 -b")
   end
 end
 
--- backend/controller/features/support/env.rb
+++ backend/controller/features/support/env.rb
@@ -5,63 +5,141 @@ require 'logger'
 
 World(MCollective::RPC)
 
-# Setup a logger for everyone
+# Setup logging for the entire test run
+FileUtils.rm('/tmp/libra-test.log')
 @@logger = Logger.new('/tmp/libra-test.log')
 @@logger.level = Logger::DEBUG
 Libra.c[:logger] = @@logger
 
-# Maximum number of subprocesses to allow
-@@MAX_PROC = 10
+module Libra
+  module Test
+    class User
+      #
+      # Obtain a unique username from S3.
+      #
+      #   sprint = The number of the sprint which, if supplied,
+      #     will be added as a prefix to the username to result
+      #     in something like "sprint1username"
+      #   reserved_usernames = A list of reserved names that may
+      #     not be in the global store
+      #
+      def get_unique_username(sprint=nil, reserved_usernames=[])
+        result=nil
 
-def get_unique_username(sprint=nil, reserved_usernames=[])
-  result=nil
+        @@logger.info("reserved = #{reserved_usernames}")
 
-  @@logger.info("reserved = #{reserved_usernames}")
+        loop do
+          # Generate a random username
+          chars = ("1".."9").to_a
+          prefix = sprint ? "sprint#{sprint}" : "test"
+          username = prefix + Array.new(5, '').collect{chars[rand(chars.size)]}.join
+          @@logger.info("attempting = #{username}")
 
-  loop do
-    # Generate a random username
-    chars = ("1".."9").to_a
-    prefix = sprint ? "sprint#{sprint}" : "test"
-    username = prefix + Array.new(5, '').collect{chars[rand(chars.size)]}.join
-    @@logger.info("attempting = #{username}")
+          user = User.find(username)
 
-    user = User.find(username)
+          @@logger.info("looked up user = #{user}")
 
-    @@logger.info("looked up user = #{user}")
+          unless user or reserved_usernames.index(username)
+            result = username
+            break
+          end
+        end
 
-    unless user or reserved_usernames.index(username)
-      result = username
-      break
+        @@logger.info("returning username = #{result}")
+
+        return result
+      end
+
+      #
+      # Create a user with a unique username and a testing
+      # email and ssh key
+      #
+      def create_unique_test_user(sprint=nil)
+        @user = create_test_user(get_unique_username(sprint))
+      end
+
+      #
+      # Create a user with the supplied username and a testing
+      # email and ssh key
+      #
+      def create_test_user(username)
+        User.create(username, @test_ssh_key, @test_email)
+      end
+    end
+
+    class Util
+      #
+      # Run a command with logging.  If the command
+      # returns a non-zero error code, raise an exception
+      #
+      def run(cmd)
+          @@logger.info("Running: #{cmd}")
+
+          output = `#{cmd} 2>&1`
+          exit_code = $?
+
+          # Raise an exception on a non-zero exit code
+          unless exit_code.to_i
+            @@logger.error("ERROR running #{cmd}.  Output:\n#{output}")
+            raise RuntimeError, "Running #{cmd} failed"
+          else
+            @@logger.info("Output :\n#{output}")
+          end
+
+          return exit_code
+      end
+
+      #
+      # Fork processes to run the supplied block of code.  Each
+      # processes will pop an entry off the in_data array until
+      # the array is exhausted.
+      #
+      #   in_data = an array of data elements to process
+      #   max_processes = the number of processes to use
+      #   delay = sleep between 0-1 seconds between forks
+      #
+      def fork_cmd(in_data, max_processes, delay=true)
+        # Copy the data array
+        f_data = Array.new(in_data)
+
+        # Create the users in subprocesses
+        loop do
+          # Don't fork more processes than we have data elements
+          num_processes = f_data.length < max_processes ? f_data.length: max_processes
+
+          num_processes.times do
+            # Pop a piece of data off the array
+            data = f_data.pop
+
+            # Fork off the command
+            @@logger.info("Forking with data element #{data}")
+            fork do
+              yield data
+            end
+
+            # if delaying, sleep to delay forks
+            sleep rand if delay
+          end
+
+          # Wait for the processes to finish
+          num_processes.times do
+              Process.wait
+              exit_code = $?.exitstatus
+              process_id = $?.to_i
+              raise "Forking failed / pid=#{process_id} / exit_code=#{exit_code}" if exit_code != 0
+          end
+
+          # Break if we've processed all the usernames
+          break if f_data.empty?
+        end
+      end
     end
   end
-
-  @@logger.info("returning username = #{result}")
-
-  return result
 end
 
-def create_unique_test_user(sprint=nil)
-  @user = create_test_user(get_unique_username(sprint))
-end
-
-def create_test_user(username)
-  User.create(username, @test_ssh_key, @test_email)
-end
-
-# Run a command with logging.  If the command
-# returns a non-zero error code, raise an exception
-def run(cmd)
-    @@logger.info("Thread #{Thread.current.object_id} / Running command: #{cmd}")
-
-    output = `#{cmd} 2>&1`
-    exit_code = $?
-    raise RuntimeError, "Thread #{Thread.current.object_id} / Command #{cmd} failed" unless exit_code.to_i
-
-    @@logger.info("Thread #{Thread.current.object_id} / Command output: #{output}")
-
-    return exit_code
-end
-
+#
+# Cucumber test setup
+#
 Before do
   # Setup the MCollective options
   Libra.c[:rpc_opts][:config] = "test/etc/client.cfg"
@@ -69,4 +147,7 @@ Before do
   # Setup test user info
   @test_ssh_key = ssh_key = File.open("test/id_rsa.pub").gets.chomp.split(' ')[1]
   @test_email = "libra-test@redhat.com"
+
+  # Default the maximum number of processes
+  @max_processes = 10
 end
