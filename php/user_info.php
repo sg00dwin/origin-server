19eab83405003c44855ff105183cd64881f0561e
Mike McGrath <mmcgrath@redhat.com>
2011-01-24 15:38:52 -0600

    Added debug info

--- php/user_info.php
+++ php/user_info.php
@@ -16,6 +16,7 @@ function my_exec($cmd, $input='')
                       );
          }
 
+print_r("\n/usr/bin/rhc-user-info -u $username $flags");
 $results = my_exec("/usr/bin/rhc-user-info -u $username $flags", $out);
 
 if($results['return'] != 0) {
