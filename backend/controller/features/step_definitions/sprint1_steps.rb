7eb39172d405ce0e9bfaf7a3a9810050f3fda31d
root <root@domU-12-31-39-0B-79-61.compute-1.internal>
2011-01-26 16:43:20 -0500

    Added debug line to the create_app_script call

--- backend/controller/features/step_definitions/sprint1_steps.rb
+++ backend/controller/features/step_definitions/sprint1_steps.rb
@@ -41,7 +41,7 @@ When /^(\d+) applications of type '(.+)' are created per user$/ do |num_apps, fr
       # Create the user and the apps
       run("#{$create_user_script} -u #{username} -e #{$email}")
       @apps.each do |app|
-        run("#{$create_app_script} -u #{username} -a #{app} -r #{@temp}/#{username}_#{app}_repo -t php-5.3.2 -b")
+        run("#{$create_app_script} -u #{username} -a #{app} -r #{@temp}/#{username}_#{app}_repo -t php-5.3.2 -b -d")
       end
     end
 
