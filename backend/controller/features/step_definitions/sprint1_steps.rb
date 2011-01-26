a5ac8fc51f2c2ed67bd58d60c7950a92048487f7
Matt Hicks <mhicks@redhat.com>
2011-01-25 22:47:27 -0500

    Fixing restructure

--- backend/controller/features/step_definitions/sprint1_steps.rb
+++ backend/controller/features/step_definitions/sprint1_steps.rb
@@ -39,7 +39,7 @@ When /^(\d+) applications of type '(.+)' are created per user$/ do |num_apps, fr
 
     processes << fork do
       # Create the user and the apps
-      run("#{$create_user_script} -u #{username} -e #{$email}") if count == 0
+      run("#{$create_user_script} -u #{username} -e #{$email}")
       @apps.each do |app|
         run("#{$create_app_script} -u #{username} -a #{app} -r #{@temp}/#{username}_#{app}_repo -t php-5.3.2 -b")
       end
