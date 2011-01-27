cb8197584998b6728374dd671fd08ab6365dca05
Mike McGrath <mmcgrath@redhat.com>
2011-01-27 09:07:38 -0600

    disable git repos check

--- backend/facter/libra.rb
+++ backend/facter/libra.rb
@@ -47,12 +47,12 @@ if File.exists?("/var/lib/libra")
             end
         end
 
-        if File.exists?("/var/lib/libra/#{customer}/git/")
-            git_repos = Dir.glob("/var/lib/libra/#{customer}/git/*.git")
-            Facter.add("git_#{customer}") do
-                setcode do git_repos.join(',') end
-            end
-        end
+#        if File.exists?("/var/lib/libra/#{customer}/git/")
+#            git_repos = Dir.glob("/var/lib/libra/#{customer}/git/*.git")
+#            Facter.add("git_#{customer}") do
+#                setcode do git_repos.join(',') end
+#            end
+#        end
     end
 end
 
