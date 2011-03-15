4db2365494ff00efdc4b850aa16ea4b79d4cd57b
Matt Hicks <mhicks@redhat.com>
2011-03-15 14:34:04 -0400

    Handling migration cases for the new ~/.li config dir

--- client/rhc-common.rb
+++ client/rhc-common.rb
@@ -4,14 +4,14 @@ require "net/http"
 require "net/https"
 require "json"
 require "parseconfig"
-  
-module RHC  
+
+module RHC
 
   TYPES = { "php-5.3.2" => :php,
     "rack-1.1.0" => :rack,
     "wsgi-3.2.1" => :wsgi
   }
-  
+
   def RHC.get_type_keys(sep)
     i = 1
     type_keys = ''
@@ -24,7 +24,7 @@ module RHC
     end
     type_keys
   end
-  
+
   def RHC.check_rhlogin(rhlogin)
     if rhlogin =~ /([^@]+)@([a-zA-Z0-9\.])+\.([a-zA-Z]{2,3})/
       if $1 =~ /[^a-zA-Z0-9\.\-\+]/
@@ -36,15 +36,15 @@ module RHC
       return false
     end
   end
-  
+
   def RHC.check_app(app)
     check_field(app, 'application')
   end
-  
+
   def RHC.check_namespace(namespace)
     check_field(namespace, 'namespace')
   end
-  
+
   def RHC.check_field(field, type)
     if field
       if field =~ /[^0-9a-zA-Z]/
@@ -57,10 +57,10 @@ module RHC
     end
     true
   end
-  
+
   def RHC.get_type(type)
-    if type   
-      if !(RHC::TYPES.has_key?(type))        
+    if type
+      if !(RHC::TYPES.has_key?(type))
         puts 'type must be ' << RHC::get_type_keys(' or ')
       else
         return RHC::TYPES[type]
@@ -70,7 +70,7 @@ module RHC
     end
     nil
   end
-  
+
   def RHC.print_post_data(h, debug)
     if (debug)
       puts 'DEBUG: Submitting form:'
@@ -87,34 +87,34 @@ module RHC
       end
     end
   end
-    
-  def RHC.get_user_info(li_server, rhlogin, password, net_http, debug)  
-    
+
+  def RHC.get_user_info(li_server, rhlogin, password, net_http, debug)
+
     puts "Contacting https://#{li_server}"
     data = {'rhlogin' => rhlogin, 'password' => password}
     print_post_data(data, debug)
-    json_data = JSON.generate(data)    
-    
+    json_data = JSON.generate(data)
+
     url = URI.parse("https://#{li_server}/php/user_info.php")
     response = http_post(net_http, url, json_data)
-    
+
     puts "DEBUG:" if debug
     p response if debug
     json_resp = JSON.parse(response.body)
-    
+
     unless json_resp['return'].to_s.strip == "0"
         puts "Problem with server. Response code was #{response.code}"
         puts "HTTP response from server is #{response.body}"
         exit 255
     end
-    
+
     if debug
         puts "HTTP response from server is #{response.body}"
     end
-    user_info = JSON.parse(json_resp['stdout'].to_s)  
+    user_info = JSON.parse(json_resp['stdout'].to_s)
     user_info
   end
-  
+
   def RHC.get_password
     password = nil
     begin
@@ -123,14 +123,14 @@ module RHC
       password = gets.chomp
     ensure
       system "stty echo"
-    end  
+    end
     puts "\n"
     password
   end
-  
+
   def RHC.http_post(http, url, json_data)
     req = http::Post.new(url.path)
-    
+
     req.set_form_data({'json_data' => json_data})
     http = http.new(url.host, url.port)
     if url.scheme == "https"
@@ -140,7 +140,7 @@ module RHC
     response = http.start {|http| http.request(req)}
     response
   end
-    
+
 end
 
 #
@@ -148,7 +148,14 @@ end
 #
 @config_path = File.exists?('client.conf') ? 'client.conf' : '/etc/libra/client.conf'
 @local_config_path = "#{ENV['HOME']}/.li/client.conf"
+@legacy_config_path = "#{ENV['HOME']}/.li"
 
+unless File.directory?(@legacy_config_path)
+  puts "Backing up old config file at ~/.li"
+  FileUtils.mv @legacy_config_path, @legacy_config_path + ".bak"
+end
+
+FileUtils.mkdir_p "#{ENV['HOME']}/.li"
 FileUtils.touch @local_config_path
 
 begin
