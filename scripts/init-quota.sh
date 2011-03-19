1f007eddc8cccbddfc4087d249344d1f0e05d847
Mark Lamourine <markllama@redhat.com>
2011-03-18 23:41:32 -0400

    reworked init-quota.sh and controller cartridge configure to work on AWS

--- scripts/init-quota.sh
+++ scripts/init-quota.sh
@@ -30,13 +30,20 @@ libra_dir=`echo $libra_dir | tr -s / | perl -p -e 's:(.)/$:$1 :'`
 #    df $1 | tail -1 | tr -s ' ' | cut -d' ' -f 1
 #}
 
+# find the mount point above a given file or directory
 function get_mountpoint() {
     df $1 | tail -1 | tr -s ' ' | cut -d' ' -f 6 | sort -u
 }
 
-function get_mount_options() {
+# get the fstab options for a given mount point
+function get_fstab_mount_options() {
+    cat /etc/fstab | tr -s ' ' | grep " $1 "| awk '{print $4;}'
+}
+
+# get the current mount options for a given mount point
+function get_current_mount_options() {
+    # options are the comma delimited string between parens
     mount | grep " $1 " | sed -e 's/^.*(// ; s/).*$// ' | sort -u
-    #cat /etc/fstab | tr -s ' ' | grep $1 | awk '{print $4;}'
 }
 
 #LIBRA_FILESYSTEM=`get_filesystem $libra_dir`
@@ -49,12 +56,12 @@ QUOTA_FILE=$( echo ${LIBRA_MOUNTPOINT}/aquota.user | tr -s /)
 #
 function update_fstab() {
     # LIBRA_MOUNTPOINT=$1
-    CURRENT_OPTIONS=`get_mount_options $1`
+    FSTAB_OPTIONS=`get_fstab_mount_options $1`
     QUOTA_OPTIONS=usrjquota=aquota.user,jqfmt=vfsv0
-    NEW_OPTIONS=${CURRENT_OPTIONS},${QUOTA_OPTIONS}
+    NEW_OPTIONS=${FSTAB_OPTIONS},${QUOTA_OPTIONS}
     # check before you replace.
     # NOTE: double quotes - Variables are shell-substituted before perl runs
-    perl -p -i -e "m:$1: && s/${CURRENT_OPTIONS}/${NEW_OPTIONS}/" /etc/fstab
+    perl -p -i -e "m: $1 : && s/${FSTAB_OPTIONS}/${NEW_OPTIONS}/" /etc/fstab
 }
 
 #
@@ -77,7 +84,7 @@ function init_quota_db() {
 update_fstab $LIBRA_MOUNTPOINT
 
 # remount to enable
-CURRENT_OPTIONS=`get_mount_options $LIBRA_MOUNTPOINT`
+CURRENT_OPTIONS=`get_current_mount_options $LIBRA_MOUNTPOINT`
 QUOTA_OPTIONS=usrjquota=aquota.user,jqfmt=vfsv0
 # check to avoid doing it again
 NEW_OPTIONS=${CURRENT_OPTIONS},${QUOTA_OPTIONS}
