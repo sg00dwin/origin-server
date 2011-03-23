c0ee7ace5b599df1744f4fc2e75bda278db836c1
Matt Hicks <mhicks@redhat.com>
2011-03-23 18:12:11 -0400

    Fixing create app script

--- tests/step_definitions/sprint_steps.rb
+++ tests/step_definitions/sprint_steps.rb
@@ -42,7 +42,7 @@ When /^(\d+) applications of type '(.+)' are created per user$/ do |num_apps, fr
       # Create the user and the apps
       run("#{$create_domain_script} -n #{username} -l #{login} -p fakepw")
       @apps.each do |app|
-        run("#{$create_app_script} -l #{login} -a #{app} -r #{$temp}/#{username}_#{app}_repo -t php-5.3.2 -b -d -p fakepw")
+        run("#{$create_app_script} -l #{login} -a #{app} -r #{$temp}/#{username}_#{app}_repo -t php-5.3.2 -p fakepw")
       end
     end
 
