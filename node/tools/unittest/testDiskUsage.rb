a26ebdac27bdfb6aeefc55775d05540e003bc4c2
Mark Lamourine <markllama@gmail.com>
2011-03-28 06:32:30 -0400

    silenced puts in disk usage test

--- node/tools/unittest/testDiskUsage.rb
+++ node/tools/unittest/testDiskUsage.rb
@@ -15,29 +15,30 @@ class TestDiskUsage < Test::Unit::TestCase
     d0 = Libra::Node::DiskUsage.new
 
     d0.check
-    puts d0
+    #puts d0
   end
   
   def testToXml
     d0 = Libra::Node::DiskUsage.new
 
     d0.check
-    puts d0.to_xml
+    #puts d0.to_xml
   end
 
   def testToJson
     d0 = Libra::Node::DiskUsage.new
 
     d0.check
-    puts d0.to_json
+    #puts d0.to_json
   end
 
   def testFromJson
     d0 = Libra::Node::DiskUsage.new
 
     d0.check
-    puts d0.to_json
+    #puts d0.to_json
 
     d1 = JSON.parse(d0.to_json)
+    assert_equal(d0.to_xml, d1.to_xml)
   end
 end
