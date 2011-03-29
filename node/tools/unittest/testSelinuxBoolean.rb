b80282601046f0d6e46efe4ee8de678aba1d0774
Mark Lamourine <markllama@gmail.com>
2011-03-29 18:22:01 -0400

    re-added simple test methods to testSysctl and testSelinuxBoolean

--- node/tools/unittest/testSelinuxBoolean.rb
+++ node/tools/unittest/testSelinuxBoolean.rb
@@ -10,7 +10,19 @@ class TestSelinuxBoolean < Test::Unit::TestCase
     se0 = Libra::Node::SelinuxBoolean.new
     se0.check
     #puts sc0
-    p se0
+    puts se0
+  end
+
+  def testSelinuxBooleanToXml
+    se0 = Libra::Node::SelinuxBoolean.new
+    se0.check
+    puts sc0.to_xml
+  end
+
+  def testSelinuxBooleanToJson
+    se0 = Libra::Node::SelinuxBoolean.new
+    se0.check
+    puts sc0.to_json
   end
 
   
