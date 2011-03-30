db8c98aef88b0707a7cc6882fbe993916793bb43
Mark Lamourine <markllama@gmail.com>
2011-03-29 20:44:00 -0400

    added test for single booleans

--- node/tools/unittest/testSelinuxBoolean.rb
+++ node/tools/unittest/testSelinuxBoolean.rb
@@ -9,21 +9,30 @@ class TestSelinuxBoolean < Test::Unit::TestCase
   def testSelinuxBooleanAll
     se0 = Libra::Node::SelinuxBoolean.new
     se0.check
-    #puts sc0
-    puts se0
+    #puts se0
   end
 
   def testSelinuxBooleanToXml
     se0 = Libra::Node::SelinuxBoolean.new
     se0.check
-    puts se0.to_xml
+    #puts se0.to_xml
+
+    se1 = Libra::Node::SelinuxBoolean.new 'httpd_can_network_relay'
+    se1.check
+    puts se1.to_xml
+
   end
 
   def testSelinuxBooleanToJson
     se0 = Libra::Node::SelinuxBoolean.new
     se0.check
-    puts se0.to_json
+    #puts se0.to_json
+
+    se1 = Libra::Node::SelinuxBoolean.new 'httpd_can_network_relay'
+    se1.check
+    #puts se1.to_json
   end
 
+  
   
 end
