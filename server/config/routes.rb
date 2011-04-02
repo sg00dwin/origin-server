1de027fe4ca6c20321a37147078f42d0a0955874
Matt Hicks <mhicks@redhat.com>
2011-04-02 11:54:07 -0400

    Changing User model to WebUser

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,10 +1,10 @@
 RedHatCloud::Application.routes.draw do
   scope "app" do
     # Map all the actions on the home controller
-  
+
     # The priority is based upon order of creation:
     # first created -> highest priority.
-  
+
     # Sample of regular route:
     #   match 'products/:id' => 'catalog#view'
     # Keep in mind you can assign values other than :controller and :action
@@ -12,20 +12,20 @@ RedHatCloud::Application.routes.draw do
     match 'index2' => 'home#index2'
     match 'appscale' => 'home#appscale'
     match 'getting_started' => 'home#getting_started'
-    
+
     match 'email_confirm' => 'email_confirm#confirm'
-    
+
     match 'broker/cartridge' => 'broker#cartridge_post', :via => [:post]
     match 'broker/domain' => 'broker#domain_post', :via => [:post]
-    match 'broker/userinfo' => 'broker#user_info_post', :via => [:post]    
-  
+    match 'broker/userinfo' => 'broker#user_info_post', :via => [:post]
+
     # Sample of named route:
     #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
     # This route can be invoked with purchase_url(:id => product.id)
-  
+
     # Sample resource route (maps HTTP verbs to controller actions automatically):
-    resources :users, :constraints => { :protocol => "https" }
-  
+    resources :users, :as => "web_users", :constraints => { :protocol => "https" }
+
     # Sample resource route with options:
     #   resources :products do
     #     member do
@@ -37,13 +37,13 @@ RedHatCloud::Application.routes.draw do
     #       get 'sold'
     #     end
     #   end
-  
+
     # Sample resource route with sub-resources:
     #   resources :products do
     #     resources :comments, :sales
     #     resource :seller
     #   end
-  
+
     # Sample resource route with more complex sub-resources
     #   resources :products do
     #     resources :comments
@@ -51,20 +51,20 @@ RedHatCloud::Application.routes.draw do
     #       get 'recent', :on => :collection
     #     end
     #   end
-  
+
     # Sample resource route within a namespace:
     #   namespace :admin do
     #     # Directs /admin/products/* to Admin::ProductsController
     #     # (app/controllers/admin/products_controller.rb)
     #     resources :products
     #   end
-  
+
     # You can have the root of your site routed with "root"
     # just remember to delete public/index.html.
-    root :to => "home#index"    
-  
+    root :to => "home#index"
+
     # See how all your routes lay out with "rake routes"
-  
+
     # This is a legacy wild controller route that's not recommended for RESTful applications.
     # Note: This route will make all actions in every controller accessible via GET requests.
     # match ':controller(/:action(/:id(.:format)))'
