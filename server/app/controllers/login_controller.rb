5188e05991c21577d8e862abe0a6e6a549662d6c
Matt Hicks <mhicks@redhat.com>
2011-04-06 16:26:50 -0400

    Adding rails server support for SSL

--- server/app/controllers/login_controller.rb
+++ server/app/controllers/login_controller.rb
@@ -3,28 +3,8 @@ require 'pp'
 class LoginController < ApplicationController
 
   def index
-    @redirectUrl = "http://openshift.redhat.com/app"
-    @errorUrl = "http://openshift.redhat.com/app/login/error"
-  end
-
-  def create
-    @user = WebUser.new(params[:web_user])
-    Rails.logger.debug "Logging in user #{user}"
-
-    @user.login
-
-    render and return if @user.errors.any?
-
-    Rails.logger.debug "User is logged in"
-
-    # Redirect to a running workflow if it exists
-    if session[:workflow]
-      Rails.logger.debug "Found existing workflow, redirecting to #{session[:workflow]}"
-      redirect_to session[:workflow] if session[:workflow]
-    else
-      Rails.logger.debug "No existing workflow, redirecting to landing page"
-      redirect_to landing_index_path, :protocol => "https"
-    end
+    @redirectUrl = "https://openshift.redhat.com/app"
+    @errorUrl = "https://openshift.redhat.com/app/login/error"
   end
 
   def show
