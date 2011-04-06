8828d16b45836bc1e7e3e92c0cc22ccd2df4fd2b
Matt Hicks <mhicks@redhat.com>
2011-04-06 17:53:13 -0400

    Adding Express Access routing

--- server/app/controllers/home_controller.rb
+++ server/app/controllers/home_controller.rb
@@ -2,14 +2,15 @@ class HomeController < ApplicationController
   def index
     # Handle any workflow routing
     if session[:workflow]
-      redirect_to session[:workflow]
-      session[:workflow].clear
+      workflow = session[:workflow]
+      session[:workflow] = nil
+      redirect_to workflow and return
     end
 
     # If the user is logged in, send them to the landing page
     # otherwise, send them to the home
     if session[:login]
-      render "protected/index"
+      redirect_to protected_path
     else
       render :index
     end
