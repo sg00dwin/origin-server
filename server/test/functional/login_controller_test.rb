ab606c50a2843d7c52a4e5e920b12a98f73abfa0
Matt Hicks <mhicks@redhat.com>
2011-04-06 21:54:31 -0400

    More login tests

--- server/test/functional/login_controller_test.rb
+++ server/test/functional/login_controller_test.rb
@@ -3,11 +3,22 @@ require 'test_helper'
 class LoginControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
+    assert assigns(:redirectUrl)
+    assert assigns(:errorUrl)
     assert_response :success
   end
 
-  test "should get logout" do
-    get logout_path
+  test "should get error" do
+    get :show, :id => "error"
+    assert assigns(:user)
+    assert !assigns(:user).errors.empty?
     assert_response :success
+  end
+
+  test "should post login" do
+    post(:create, {'login' => "tester"})
+    assert_equal session[:login], "tester"
+    assert_not_nil session[:ticket]
+    assert_redirected_to protected_path
   end
 end
