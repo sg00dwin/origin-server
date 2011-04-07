71f075cbb0dc58fc81e384457731b1c17de32677
Dan McPherson <dmcphers@redhat.com>
2011-04-07 14:29:43 -0400

    base flex controller test

--- server/test/functional/access/flex_controller_test.rb
+++ server/test/functional/access/flex_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Access::ExpressControllerTest < ActionController::TestCase
+class Access::FlexControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_redirected_to login_index_path
