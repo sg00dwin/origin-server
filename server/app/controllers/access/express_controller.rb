02c9c0e9dd5ee0faeb3a83c42919e4bfe754771f
Dan McPherson <dmcphers@redhat.com>
2011-04-07 18:54:20 -0400

    dont auto request express access

--- server/app/controllers/access/express_controller.rb
+++ server/app/controllers/access/express_controller.rb
@@ -7,10 +7,10 @@ class Access::ExpressController < ApplicationController
     login = session[:login]
 
     if login
-      Rails.logger.debug "User is logged in"
-      user = WebUser.find_by_ticket(session[:ticket])
-      Rails.logger.debug "Requesting Express access for user #{user}"
-      user.request_access(CloudAccess::EXPRESS)
+      #Rails.logger.debug "User is logged in"
+      #user = WebUser.find_by_ticket(session[:ticket])
+      #Rails.logger.debug "Requesting Express access for user #{user}"
+      #user.request_access(CloudAccess::EXPRESS)
     else
       Rails.logger.debug "User is not logged in - rerouting to login / register"
       session[:workflow] = access_express_index_path
