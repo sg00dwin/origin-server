fdcc6326afefc8a39244257e564dbc6776cee4e4
Dan McPherson <dmcphers@redhat.com>
2011-04-14 10:11:47 -0400

    remove unnecessary clear

--- server/app/controllers/logout_controller.rb
+++ server/app/controllers/logout_controller.rb
@@ -5,7 +5,6 @@ class LogoutController < ApplicationController
     reset_session
     Rails.logger.debug "Removing current SSO cookie value of '#{cookies[:rh_sso]}'"
     cookies.delete :rh_sso, :domain => '.redhat.com'
-    session.clear
     redirect_to login_path
   end
 end
