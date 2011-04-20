332f04845859251b5d053154cd34ff515e207e37
Emily Dirsh <edirsh@redhat.com>
2011-04-19 20:58:40 -0400

    Modified try it button on product pages to respond to users roles

--- server/app/controllers/product_controller.rb
+++ server/app/controllers/product_controller.rb
@@ -2,10 +2,12 @@ class ProductController < ApplicationController
 
   def express
     @product = 'express'
+    @try_link_points_to = try_it_destination(1)
   end
   
   def flex
     @product = 'flex'
+    @try_link_points_to = try_it_destination(2)
     return
   end
 
@@ -13,5 +15,17 @@ class ProductController < ApplicationController
     @product = 'power'
     render :layout => 'application'
   end
+  
+  def try_it_destination(product_number)
+    return 'register' unless session[:login]
+    
+    if session[:roles]
+      return 'getting_started' if session[:roles].include? "cloud_access_#{product_number}"
+      
+      return 'queue' if session[:roles].include? "cloud_access_request_#{product_number}"
+    end
+    return 'request'
+
+  end  
   
 end
