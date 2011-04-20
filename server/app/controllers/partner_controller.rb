39b111bf7f399a7368a25467da4a3ddbe4e1aab8
Dan McPherson <dmcphers@redhat.com>
2011-04-20 15:58:44 -0400

    more partner work

--- server/app/controllers/partner_controller.rb
+++ server/app/controllers/partner_controller.rb
@@ -3,7 +3,12 @@ class PartnerController < ApplicationController
   def index
     file = File.open("db/partners.json", "rb")
     contents = file.read
-    @partners = JSON.parse(contents)
-  end  
+    partners = JSON.parse(contents)
+    @partners = Array.new(partners.length)
+    partners.each_with_index do |(partner_id, partner), i|    
+      @partners[i] = Partner.new(partner)
+      @partners[i].id = partner_id 
+    end
+  end
   
 end
--- server/app/models/partner.rb
+++ server/app/models/partner.rb
@@ -1,5 +1,10 @@
-class WebUser < ActiveModel::Naming
+class Partner
   
   attr_accessor :name, :id, :blurb, :url, :logo
-
+  
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
 end
