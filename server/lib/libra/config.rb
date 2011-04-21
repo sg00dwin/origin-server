23acd16210a6f128695146607929c999f971b6b2
Matt Hicks <mhicks@redhat.com>
2011-04-21 00:22:14 -0400

    Increasing mcollective command timeout to 20 seconds

--- server/lib/libra/config.rb
+++ server/lib/libra/config.rb
@@ -72,7 +72,7 @@ module Libra
 
     # Now, initialize the MCollective options
     @@config[:rpc_opts] = {:disctimeout => 3,
-                           :timeout     => 10,
+                           :timeout     => 20,
                            :verbose     => false,
                            :progress_bar=> false,
                            :filter      => {"identity"=>[], "fact"=>[], "agent"=>[], "cf_class"=>[]},
