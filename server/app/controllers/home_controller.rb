a4d311c59d20e4e0082a16cf9eb3fcce4159324a
Emily Dirsh <edirsh@redhat.com>
2011-04-21 05:50:00 -0400

    Changes to main site navigation. Front page pruning as requested by Issac.

--- server/app/controllers/home_controller.rb
+++ server/app/controllers/home_controller.rb
@@ -12,10 +12,6 @@ class HomeController < ApplicationController
       redirect_to workflow and return
     else
       # If workflow didn't send them elsewhere then they go to the index.
-      # Assuming at some point news will come from a news source
-      @news = [ 'Lorem ipsum dolor si',
-                'Amet, consetetur sadipscing elitr, sed diam nonumyeirmod',
-                'Tempor invidunt ut labore et dolore magna aliquyam erat, sed' ]
     end
   end
 
