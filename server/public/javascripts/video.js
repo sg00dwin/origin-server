8f025d768b2c87c3d2dbc131df837cd5e156c8d5
Dan McPherson <dmcphers@redhat.com>
2011-04-22 14:56:01 -0400

    enable video controls and fullscreen

--- server/public/javascripts/video.js
+++ server/public/javascripts/video.js
@@ -4,6 +4,8 @@ $(function() {
   projekktor('video',{
     /* path to flash player fallback */
     playerFlashMP4: '/app/javascripts/jarisplayer.swf',
-    playerFlashMP3: '/app/javascripts/jarisplayer.swf'
+    playerFlashMP3: '/app/javascripts/jarisplayer.swf',
+		controls: true,
+		enableFullscreen: true
   });
 });
