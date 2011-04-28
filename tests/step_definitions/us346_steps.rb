3d85472be40271ec1890062e16c9a789ad5c008f
xuliu <xuliu@redhat.com>
2011-04-28 18:10:53 +0800

    add time limit and get app_name from @data

--- tests/step_definitions/us346_steps.rb
+++ tests/step_definitions/us346_steps.rb
@@ -12,8 +12,8 @@ Then /^users are able to create new rails app using rails new$/ do
     $logger.info("Changing to dir=#{repo}")
     Dir.chdir(repo)
 
-    app_file = "public/index.html"
-    app_name = user_app[0]+"_"+user_app[1]
+    app_file = "public/index.html"    
+    app_name = value[:app]
 
     #Create new rails app
     run("rails new #{app_name}")
@@ -50,6 +50,31 @@ Then /^users are able to create new rails app using rails new$/ do
   @data.each_pair do |url, value|
     $logger.info("#{value[:change_code]} - #{url} (#{value[:type]})")
     results << value[:change_code]
+  end
+
+  # Get all the unique responses
+  # There should only be 1 result [0]
+  uniq_responses = results.uniq
+  uniq_responses.length.should == 1
+  uniq_responses[0].should == 0
+end
+
+Then /^they should all be accessible within (\d+) seconds$/ do |timeout|
+  # Hit the health check page for each app
+  @data.each_pair do |url, value|
+    connect(url, "/config.ru", timeout.to_i) do |code, time, body|
+      value[:code] = code
+      value[:time] = time
+    end unless value[:failed]
+  end
+
+  # Print out the results:
+  #  Format = code - url
+  $logger.info("Accessibility Results")
+  results = []
+  @data.each_pair do |url, value|
+    $logger.info("#{value[:code]} / #{value[:time]} - #{url} (#{value[:type]})")
+    results << value[:code]
   end
 
   # Get all the unique responses
