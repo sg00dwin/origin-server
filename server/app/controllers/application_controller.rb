0af2ea7b72a4a968dcdd35634c8396299148b3d8
Emily Dirsh <edirsh@redhat.com>
2011-04-27 21:00:15 -0400

    Front page altered to work with single large image; Added promotional banners; Added flex console link to logout box

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :check_credentials
-
+  
   def set_no_cache
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
