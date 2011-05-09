f209e373da7ae9fc0a97c35028e81db6b22de2b1
Emily Dirsh <edirsh@redhat.com>
2011-05-09 14:00:58 -0400

    Added memory to nav menu - it now remembers which section was last expanded; Delayed menu width calculation to allow for font loading, thus improving accuracy;

--- server/public/javascripts/universal.src.js
+++ server/public/javascripts/universal.src.js
@@ -2,7 +2,7 @@
 
 $(function(){
   
-  var navigation, menus, labels, menu_widths;
+  var navigation, menus, labels, menu_widths, width_recalculated, expanded_menu, expanded_menu_id;
   
   /** 
    * Navigation bar animations
@@ -10,13 +10,28 @@ $(function(){
   navigation = $('#nav');
   menus = $('ul', navigation);
   labels = $('.category', navigation);
+  menu_widths = null;
+  
+  //Set expanded menu based on cookie
+  expanded_menu_id = $.cookie('os_menu_expanded');
+  //console.log(expanded_menu_id);
+  //console.log(document.cookie);
+  if (expanded_menu_id != null) {
+    expanded_menu = menus.filter('#' + expanded_menu_id);
+    //Check that expanded menu is not the default
+    if (expanded_menu.hasClass('collapsed')) {
+      //remove class from default expanded menu
+      menus.filter('.expanded').removeClass('expanded').addClass('collapsed');
+      labels.filter('.expanded').removeClass('expanded').addClass('collapsed');
+      //add class to previously expanded menu
+      expanded_menu.removeClass('collapsed').addClass('expanded');
+      labels.filter('[data-category=' + expanded_menu_id + ']').removeClass('collapsed').addClass('expanded');
+    }
+  }
   
   //Initialize menus
-  menu_widths = {};
   menus.each(function(){
     //console.log($(this).attr('id'));
-    //Record width
-    menu_widths[$(this).attr('id')] = $(this).width();
     //Restrict height
     $(this).css('height', $(this).height());
     
@@ -25,8 +40,7 @@ $(function(){
       $(this).css({width:0});
     }
   });
-  //console.log(menu_widths);
-  
+
   labels.click(function(){
     var id, menu;
     id = $(this).attr('data-category');
@@ -56,7 +70,36 @@ $(function(){
   }
   
   function expandMenu(menu, label) {
-    var menu_width = menu_widths[menu.attr('id')];
+    var id, menu_width;
+    id = menu.attr('id');
+    // If widths haven't already been calculated,
+    // do so now.
+    //
+    // We're doing this here because calculating the
+    // widths earlier can result in incorrect numbers
+    // due to incomplete font loading.
+    //
+    // We're making the assumption that by the time
+    // the user makes a decision to click on a menu,
+    // the font file will have had enough time to load.
+    if (menu_widths == null) {
+      menu_widths = {};
+      menus.each(function(){
+        var total_width = 0;
+        $(this).children('li').each(function(){
+          // calculate width using the children 
+          // since some menus are already collapsed
+          total_width += $(this).width();
+        });
+        menu_widths[$(this).attr('id')] = total_width;
+      });
+      console.log(menu_widths);
+    }
+    menu_width = menu_widths[id];
+    //Set cookie to remember which menu was last expanded
+    $.cookie('os_menu_expanded', id, {path: '/'});
+    //console.log('expanded ' + id);
+    //console.log(document.cookie);
     label.removeClass('collapsed').addClass('expanded');
     menu.removeClass('collapsed').addClass('expanded');
     menu.stop().animate({width: menu_width}, 300);
