c3907a0458ff41d5cbb9cb33b5fa825e1e2de611
Dan McPherson <dmcphers@redhat.com>
2011-05-13 17:40:47 -0400

    Bug 698947

--- client/lib/rhc-common.rb
+++ client/lib/rhc-common.rb
@@ -298,8 +298,18 @@ _home_conf = File.expand_path('~/.openshift')
 @config_path = File.exists?(_linux_cfg) ? _linux_cfg : _gem_cfg
 
 FileUtils.mkdir_p _home_conf unless File.directory?(_home_conf)
-
-FileUtils.touch File.expand_path(@local_config_path)
+local_config_path = File.expand_path(@local_config_path)
+if !File.exists? local_config_path
+  FileUtils.touch local_config_path
+  puts ""
+  puts "Created empty local express.conf config file: " + local_config_path
+  puts "You can use this file to modify config for the current system user (you probably won't need to do this)."  
+  puts "Search order (on a per variable basis):"
+  puts "  1) ~/.openshift/express.conf"
+  puts "  2) /etc/openshift/express.conf (RPM install path)"
+  puts "  3) <ruby_gem_dir>/gems/rhc-<version>/conf/express.conf (RubyGem install path)"
+  puts ""
+end
 
 begin
   @global_config = ParseConfig.new(@config_path)
@@ -320,7 +330,10 @@ else
 end
 
 #
-# Check for local var in ~/.openshift/express.conf use it, else use $GEM/../conf/express.conf
+# Check for local var in 
+#   1) ~/.openshift/express.conf
+#   2) /etc/openshift/express.conf
+#   3) $GEM/../conf/express.conf
 #
 def get_var(var)
   @local_config.get_value(var) ? @local_config.get_value(var) : @global_config.get_value(var)
