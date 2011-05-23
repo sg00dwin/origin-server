119378a7e268e7a986c59181294e8df791fa8158
Matt Hicks <mhicks@redhat.com>
2011-05-23 17:42:13 -0400

    Switching gem installations to RPM installations

--- build/ami.rb
+++ build/ami.rb
@@ -498,13 +498,12 @@ END_OF_MESSAGE
           puts "Done"
 
           print "Installing the mechanize gem..."
-          ssh("yum -y install rubygem-nokogiri", 120)
-          ssh("gem install mechanize", 120)
+          ssh("yum -y install mechanize", 120)
           puts "Done"
 
           print "Installing rails for client testing..."
-          ssh("gem install rails -d --no-rdoc --no-ri", 120)
           ssh("yum -y install sqlite*", 120)
+          ssh("yum -y install rubygem-rails", 120)
           puts "Done"
 
           print "Bouncing Apache to pick up the change..."
