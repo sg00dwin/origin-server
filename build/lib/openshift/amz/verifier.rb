e694acca2e5dd4ea6b3f856d34f601eb2458ceb0
Matt Hicks <mhicks@redhat.com>
2011-06-08 14:02:18 -0400

    Switching build verifications to go to libra-express

--- build/lib/openshift/amz/verifier.rb
+++ build/lib/openshift/amz/verifier.rb
@@ -2,17 +2,18 @@ require 'net/smtp'
 
 module OpenShift
   module AWS
-    def send_verified_email(amz_image_id)
+    def send_verified_email(amz_image_id, amz_image_name)
       msg = <<END_OF_MESSAGE
 From: Jenkins <noreply@redhat.com>
-To: Matt Hicks <mhicks@redhat.com>
-Subject: DevEnv Image #{amz_image_id} QE Ready
+To: Libra Team <libra-express@redhat.com>
+Subject: DevEnv Image #{amz_image_name} (#{amz_image_id}) is QE Ready
 
-Image #{amz_image_id} is ready for QE.
+Image #{amz_image_name} (#{amz_image_id}) has passed validation tests and is ready for QE.
+
 END_OF_MESSAGE
 
       Net::SMTP.start('localhost') do |smtp|
-        smtp.send_message msg, "noreply@redhat.com", "mhicks@redhat.com"
+        smtp.send_message msg, "noreply@redhat.com", "libra-express@redhat.com"
       end
     end
   end
