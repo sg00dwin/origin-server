d7021e502d1da5e1212f5ce11c6ab507b561c385
Matt Hicks <mhicks@redhat.com>
2011-06-10 14:25:30 -0400

    Selenium test cleanup

---
+++ tests/selenium/example.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'headless'
+require 'selenium-webdriver'
+
+headless = Headless.new
+headless.start
+
+driver = Selenium::WebDriver.for :firefox
+driver.navigate.to 'http://google.com'
+puts driver.title
+
+headless.destroy
