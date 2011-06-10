c75827464d519c84f0fb3af62d4a26e61d3ce50f
Matt Hicks <mhicks@redhat.com>
2011-06-10 15:24:07 -0400

    Better WebDriver Example

--- tests/selenium/example.rb
+++ tests/selenium/example.rb
@@ -4,11 +4,24 @@ require 'rubygems'
 require 'headless'
 require 'selenium-webdriver'
 
+# Create a headless display
 headless = Headless.new
 headless.start
 
 driver = Selenium::WebDriver.for :firefox
-driver.navigate.to 'http://google.com'
-puts driver.title
 
+puts "Navigating to main page"
+driver.navigate.to 'http://localhost'
+
+puts "Logging in"
+driver.find_element(:link_text, 'LOGIN').click()
+driver.find_element(:id, 'login_input').send_keys('test@redhat.com')
+pwd = driver.find_element(:id, 'pwd_input')
+pwd.send_keys('none')
+pwd.submit()
+
+puts "Saving a screenshot"
+driver.save_screenshot('logged_in.png')
+
+# Cleanup headless display
 headless.destroy
