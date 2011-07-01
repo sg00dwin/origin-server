86cf3d655dcb37ecf13696e7fc6a48e0ba0e5ec2
Mark Lamourine <markllama@redhat.com>
2011-07-01 15:24:02 -0400

    created assertion stubs from cartridge_php.feature run

---
+++ tests/step_definitions/cartridge-php_steps.rb
@@ -0,0 +1,59 @@
+When /^I configure a PHP application$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a php application http proxy file will exist$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a php application git repo will exist$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a php application source tree will exist$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a php application httpd will be running$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a new PHP application$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I deconfigure the PHP application$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a php application http proxy file will not exist$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a php application git repo will not exist$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a php application source tree will not exist$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a php application httpd will not be running$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the php application is stopped$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I start the PHP application$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the php application is running$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I stop the PHP application$/ do
+  pending # express the regexp above with the code you wish you had
+end
