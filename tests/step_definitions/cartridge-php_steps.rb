f4d52d0762cc746ecef1ddfbc44fbf6a2da9979c
Mark Lamourine <markllama@redhat.com>
2011-07-12 14:00:25 -0400

    switch to actually checking the test instead of a status var

--- tests/step_definitions/cartridge-php_steps.rb
+++ tests/step_definitions/cartridge-php_steps.rb
@@ -40,12 +40,10 @@ Then /^a php application http proxy file will( not)? exist$/ do | negate |
   conf_file_name = "#{acct_name}_#{namespace}_#{app_name}.conf"
   conf_file_path = "#{$libra_httpd_conf_d}/#{conf_file_name}"
 
-  status = File.exists?(conf_file_path)
-
   if not negate
-    status.should be_true
+    File.exists?(conf_file_path).should be_true
   else
-    status.should be_false
+    File.exists?(conf_file_path).should be_false
   end
 end
 
@@ -103,11 +101,12 @@ Then /^a php application httpd will( not)? be running$/ do | negate |
     match and (match[1] if match[2] == 'httpd')
   }.compact!
 
+  $logger.debug("stderr: " + (stderr)
   status = (http_daemons and http_daemons.size > 0)
   if not negate
-    status.should be_true
+    http_daemons.should_not be_nil and http_daemons.size.should be > 0
   else
-    status.should be_false
+    http_daemons.should be_nil or http_daemons.size.should be == 0
   end
 
 end
