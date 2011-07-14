8868b6204a24b5961de29807aa86e9a394682c7b
Mark Lamourine <markllama@redhat.com>
2011-07-14 14:48:00 -0400

    added skeleton steps for mysql cartridge

--- tests/step_definitions/cartridge-mysql_steps.rb
+++ tests/step_definitions/cartridge-mysql_steps.rb
@@ -1,1 +1,43 @@
 # step descriptions for MySQL cartridge behavior.
+
+When /^I (de)?configure (a|the) mysql database$/ do |deconfig, dummy|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the mysql directory will( not)? exist$/ do |negate|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the mysql configuration file will( not)? exist$/ do |negate|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the mysql database will( not)? +exist$/ do |negate|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the mysql control script will( not)? exist$/ do |negate|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the mysql daemon will be (running|stopped)$/ do |state|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the admin user will have access$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a new mysql database$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the mysql database is (running|stopped)$/ do |state|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I (start|stop|restart) the mysql database$/ do |action|
+  pending # express the regexp above with the code you wish you had
+end
+
+
