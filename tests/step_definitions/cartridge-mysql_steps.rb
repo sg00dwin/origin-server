6d9add0a76c7ec2c8a466873c7d19a0977c2de68
Mark Lamourine <markllama@redhat.com>
2011-07-14 16:27:07 -0400

    added config/deconfig and test for mysql directory

--- tests/step_definitions/cartridge-mysql_steps.rb
+++ tests/step_definitions/cartridge-mysql_steps.rb
@@ -1,11 +1,48 @@
 # step descriptions for MySQL cartridge behavior.
 
-When /^I (de)?configure (a|the) mysql database$/ do |deconfig, dummy|
-  pending # express the regexp above with the code you wish you had
+$mysql_version = "5.1"
+$mysql_cart_root = "/usr/libexec/li/cartridges/embedded/mysql-#{$mysql_version}"
+$mysql_hooks = $mysql_cart_root + "/info/hooks"
+$mysql_config = $mysql_hooks + "/configure"
+$mysql_config_format = "#{$mysql_config} %s %s %s"
+$mysql_deconfig = $mysql_hooks + "/deconfigure"
+$mysql_deconfig_format = "#{$mysql_deconfig} %s %s %s"
+
+When /^I configure a mysql database$/ do
+  account_name = @account['accountname']
+  namespace = @app['namespace']
+  app_name = @app['name']
+  command = $mysql_config_format % [app_name, namespace, account_name]
+  runcon command,  'unconfined_u', 'system_r', 'libra_initrc_t'
+end
+
+When /^I deconfigure the mysql database$/ do
+  account_name = @account['accountname']
+  namespace = @app['namespace']
+  app_name = @app['name']
+  command = $mysql_deconfig_format % [app_name, namespace, account_name]
+  runcon command,  'unconfined_u', 'system_r', 'libra_initrc_t'
 end
 
 Then /^the mysql directory will( not)? exist$/ do |negate|
-  pending # express the regexp above with the code you wish you had
+  account_name = @account['accountname']
+  namespace = @app['namespace']
+  app_name = @app['name']
+
+  mysql_user_root = "#{$home_root}/#{account_name}/mysql-#{$mysql_version}"
+  begin
+    mysql_dir = Dir.new mysql_user_root
+    puts "Found the directory: #{mysql_dir}"
+  rescue Errno::ENOENT
+    mysql_dir = nil
+    puts "did not find the directory #{mysql_user_root}"
+  end
+
+  unless negate
+    mysql_dir.should be_a(Dir)
+  else
+    mysql_dir.should be_nil
+  end
 end
 
 Then /^the mysql configuration file will( not)? exist$/ do |negate|
@@ -29,7 +66,11 @@ Then /^the admin user will have access$/ do
 end
 
 Given /^a new mysql database$/ do
-  pending # express the regexp above with the code you wish you had
+  account_name = @account['accountname']
+  namespace = @app['namespace']
+  app_name = @app['name']
+  command = $mysql_config_format % [app_name, namespace, account_name]
+  runcon command,  'unconfined_u', 'system_r', 'libra_initrc_t'
 end
 
 Given /^the mysql database is (running|stopped)$/ do |state|
