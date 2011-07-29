11cca0614cbd089e26a11224005eaca3ba5af823
Emily Dirsh <edirsh@redhat.com>
2011-07-29 16:44:30 -0400

    Revert "Revert "Changed api urls in config to https""
    This reverts commit 77df690ed96d07beeac14334a59055f9b3730067.

--- site/config/environments/development.rb
+++ site/config/environments/development.rb
@@ -37,7 +37,7 @@ RedHatCloud::Application.configure do
   config.captcha_secret = 'zvw5LiixMB0I4mjk06aR'
   
   # Express API base url
-  config.express_api_url = 'http://openshift.redhat.com'
+  config.express_api_url = 'https://openshift.redhat.com'
 
 end
 
