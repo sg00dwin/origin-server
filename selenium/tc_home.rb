9b22bdf19d79447f0ee9f6f9bbe483075de122b6
Dan McPherson <dmcphers@redhat.com>
2011-08-05 18:35:56 -0400

    get selenium up and running again

--- selenium/tc_home.rb
+++ selenium/tc_home.rb
@@ -8,7 +8,7 @@ class Home < Test::Unit::TestCase
   include ::OpenShift::TestBase 
 
   def test_check_home_navigationbar
-    $logger.info "start to test  navigation bar"
+    $logger.info "start to test navigation bar"
     @driver.navigate.to @url+"/app/"
     sleep 3
     begin
@@ -16,14 +16,13 @@ class Home < Test::Unit::TestCase
     rescue Test::Unit::AssertionFailedError
         @verification_errors << $!
     end
-    @driver.find_element(:class,"express").click()
+    @driver.find_element(:class,"services").click()
     sleep 2
     begin
         assert_equal "OpenShift by Red Hat | Express", @driver.title
     rescue Test::Unit::AssertionFailedError
         @verification_errors << $!
     end
-    @driver.navigate.back
     retry_on_no_elem do
       @driver.find_element(:class,"flex").click()
     end
@@ -42,8 +41,8 @@ class Home < Test::Unit::TestCase
         @verification_errors << $!
     end
     @driver.navigate.back
-    @driver.find_element(:xpath,".//div[@id='nav']/h5[2]").click() 
-    @driver.find_element(:xpath,".//a[contains(text(),'Forums')]").click()
+    @driver.navigate.back
+    @driver.find_element(:xpath,".//a[contains(text(),'Forum')]").click()
     sleep 1
     begin
         assert_equal "Forums | Red Hat Openshift Forum", @driver.title
@@ -51,9 +50,6 @@ class Home < Test::Unit::TestCase
         @verification_errors << $!
     end
     @driver.navigate.back
-    retry_on_no_elem do
-      @driver.find_element(:xpath,".//div[@id='nav']/h5[2]").click()
-    end
     @driver.find_element(:xpath,".//a[contains(text(),'Blog')]").click()
     sleep 1
     begin
@@ -62,53 +58,10 @@ class Home < Test::Unit::TestCase
         @verification_errors << $!
     end
     @driver.navigate.back
-    @driver.find_element(:xpath,".//div[@id='nav']/h5[2]").click() 
-    @driver.find_element(:xpath,".//a[contains(text(),'Partners')]").click()
+    @driver.find_element(:xpath,".//a[contains(text(),'Partner Program')]").click()
     sleep 1
     begin
         assert_equal "OpenShift by Red Hat | Meet Our Partners", @driver.title
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    @driver.navigate.back
-    retry_on_no_elem do
-      @driver.find_element(:xpath,".//div[@id='nav']/h5[3]").click()
-    end
-    @driver.find_element(:link_text,"KB").click()
-    sleep 1
-    begin
-        assert_equal "Knowledge Base | Red Hat Openshift Forum", @driver.title
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    @driver.navigate.back
-    retry_on_no_elem do
-      @driver.find_element(:xpath,".//div[@id='nav']/h5[3]").click()
-    end
-    @driver.find_element(:xpath,".//a[contains(text(),'Docs')]").click()
-    sleep 1
-    begin
-        assert_equal "Documents | Red Hat Openshift Forum", @driver.title
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    @driver.navigate.back
-    @driver.find_element(:xpath,".//div[@id='nav']/h5[3]").click()
-    @driver.find_element(:xpath,".//a[contains(text(),'Videos')]").click()
-    sleep 1
-    begin
-        assert_equal "Videos | Red Hat Openshift Forum", @driver.title
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    @driver.navigate.back
-    retry_on_no_elem do
-      @driver.find_element(:xpath,".//div[@id='nav']/h5[3]").click()
-    end
-    @driver.find_element(:xpath,".//a[contains(text(),'FAQ')]").click()
-    sleep 1
-    begin
-        assert_equal "Frequently Asked Questions | Red Hat Openshift Forum", @driver.title
     rescue Test::Unit::AssertionFailedError
         @verification_errors << $!
     end
@@ -125,26 +78,13 @@ class Home < Test::Unit::TestCase
         @verification_errors << $!
     end
     retry_on_no_elem do
-      @driver.find_element(:class,"try_it").click()
+      @driver.find_element(:class,"sign_up").click()
     end
-    retry_on_no_elem do
-      begin
-          assert_equal "TRY EXPRESS",@driver.find_element(:xpath,".//div[@id='title']/h2").text
-      rescue Test::Unit::AssertionFailedError
-          @verification_errors << $!
-      end
+    begin
+        assert_equal "OpenShift by Red Hat | Sign up for OpenShift", @driver.title
+    rescue Test::Unit::AssertionFailedError
+        @verification_errors << $!
     end
-    @driver.navigate.back
-    retry_on_no_elem do
-      @driver.find_element(:xpath,".//div[@id='app_promos']").click()
-    end
-    @driver.find_element(:xpath,".//div[@id='app_promos']/div[1]/div/ul/li[4]/a").click()
-    @driver.navigate.back
-    @driver.find_element(:xpath,".//div[@id='app_promos']").click()
-    @driver.find_element(:xpath,".//div[@id='app_promos']/div[2]/div/ul/li[4]/a").click()
-    @driver.navigate.back
-    @driver.find_element(:xpath,".//div[@id='app_promos']").click()
-    @driver.find_element(:xpath,".//div[@id='app_promos']/div[3]/div/ul/li[4]/a").click()
     @driver.navigate.back
     @driver.save_screenshot('home_link.png')
   end
@@ -158,52 +98,7 @@ class Home < Test::Unit::TestCase
         @verification_errors << $!
     end
     begin
-        assert_equal "LOGIN", @driver.find_element(:id,"login").text
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert_equal "TRY IT NOW!", @driver.find_element(:id,"button").text
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert_equal "Deploy in Minutes", @driver.find_element(:xpath,".//div[@id='deploy_in_minutes']/h3").text
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert_equal "Upload code and go! Whether you prefer a CLI or browser-based UI, OpenShift provides the fastest and easiest on-ramp to the cloud.", @driver.find_element(:xpath,".//div[@id='deploy_in_minutes']/p").text
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert_equal "Focus on your App,\nnot the Stack", @driver.find_element(:xpath,".//div[@id='focus_on_your_app']/h3").text
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert_equal "OpenShift supports and manages multiple frameworks, languages, middleware and clouds so you don't have to.", @driver.find_element(:xpath,".//div[@id='focus_on_your_app']/p").text
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert_equal "Auto-scale", @driver.find_element(:xpath,".//div[@id='auto_scale']/h3").text
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert_equal "Take advantage of the elasticity of the cloud, regardless if your app was designed for it or not.", @driver.find_element(:xpath,".//div[@id='auto_scale']/p").text
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert  @driver.find_element(:xpath,".//img[@id='front_image']").displayed?
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert  @driver.find_element(:xpath,".//div[@id='app_promos']").displayed?
+        assert_equal "Sign In", @driver.find_element(:class,"user").text
     rescue Test::Unit::AssertionFailedError
         @verification_errors << $!
     end
@@ -224,20 +119,10 @@ class Home < Test::Unit::TestCase
     rescue Test::Unit::AssertionFailedError
         @verification_errors << $!
     end
-    begin
-        assert_equal "Copyright © 2011 Red Hat, Inc.",@driver.find_element(:xpath,".//div[@id='footer']/p").text
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
     @driver.find_element(:xpath,".//a[contains(@href, '/app/legal')]").click()
     sleep 2
     begin
         assert_equal "OpenShift by Red Hat | Terms and Conditions", @driver.title
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert_equal "LEGAL TERMS",@driver.find_element(:xpath,".//div[@id='title']/h2").text
     rescue Test::Unit::AssertionFailedError
         @verification_errors << $!
     end
@@ -251,15 +136,10 @@ class Home < Test::Unit::TestCase
     rescue Test::Unit::AssertionFailedError
         @verification_errors << $!
     end
-    begin
-        assert_equal "PRIVACY STATEMENT",@driver.find_element(:xpath,".//div[@id='title']/h2").text
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
     @driver.navigate.back
     retry_on_no_elem do
       begin
-          assert @driver.find_element(:xpath,".//a[contains(text(),'Contact Us')]").displayed?
+          assert @driver.find_element(:xpath,".//a[contains(text(),'Contact')]").displayed?
       rescue Test::Unit::AssertionFailedError
           @verification_errors << $!
       end
@@ -268,11 +148,6 @@ class Home < Test::Unit::TestCase
     sleep 2
     begin
         assert_equal "access.redhat.com | Security contacts and procedures", @driver.title
-    rescue Test::Unit::AssertionFailedError
-        @verification_errors << $!
-    end
-    begin
-        assert_equal "Security contacts and procedures",@driver.find_element(:xpath,".//div[@id='main']/h1").text
     rescue Test::Unit::AssertionFailedError
         @verification_errors << $!
     end
