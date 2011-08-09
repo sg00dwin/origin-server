4dfd804aa9328231d80343d54602b1b6e1b1667b
Mark Lamourine <markllama@redhat.com>
2011-08-09 14:40:30 -0400

    added get_unique_username from other places

---
+++ tests/support/user_helper.rb
@@ -0,0 +1,32 @@
+require "openshift"
+
+module UserHelper
+  #
+  # Obtain a unique username from S3.
+  #
+  #   reserved_usernames = A list of reserved names that may
+  #     not be in the global store
+  #
+  def get_unique_username(reserved_usernames=[])
+    result={}
+
+    loop do
+      # Generate a random username
+      chars = ("1".."9").to_a
+      namespace = "unit" + Array.new(8, '').collect{chars[rand(chars.size)]}.join
+      login = "libra-test+#{namespace}@redhat.com"
+      has_txt = Libra::Server.has_dns_txt?(namespace)
+      user = Libra::User.find(login)
+
+      unless user or has_txt or reserved_usernames.index(login)
+        result[:login] = login
+        result[:namespace] = namespace
+        break
+      end
+    end
+
+    return result
+  end
+
+end
+World(UserHelper)
