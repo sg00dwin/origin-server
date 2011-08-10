977eab427214281e6262f6ff8614cd856452cd10
Mike McGrath <mmcgrath@redhat.com>
2011-08-10 14:17:01 -0400

    storing dump in data dir

--- cartridges/mysql-5.1/info/configuration/mysql_restore.sh
+++ cartridges/mysql-5.1/info/configuration/mysql_restore.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-if [ -f /tmp/mysql_dump_snapshot.gz ]
+if [ -f $OPENSHIFT_DATA_DIR/mysql_dump_snapshot.gz ]
 then
-    /bin/zcat /tmp/mysql_dump_snapshot.gz | /usr/bin/mysql -h $OPENSHIFT_DB_HOST -P $OPENSHIFT_DB_PORT -u $OPENSHIFT_DB_USERNAME --password="$OPENSHIFT_DB_PASSWORD"
+    /bin/zcat $OPENSHIFT_DATA_DIR/mysql_dump_snapshot.gz | /usr/bin/mysql -h $OPENSHIFT_DB_HOST -P $OPENSHIFT_DB_PORT -u $OPENSHIFT_DB_USERNAME --password="$OPENSHIFT_DB_PASSWORD"
     if [ ! ${PIPESTATUS[1]} -eq 0 ]
     then
         echo 1>&2
         echo "Error: Could not import MySQL Database!  Continuing..." 1>&2
         echo 1>&2
     fi
-    /bin/rm -rf /tmp/mysql_dump_snapshot.gz
+    /bin/rm -rf $OPENSHIFT_DATA_DIR/mysql_dump_snapshot.gz
 fi
