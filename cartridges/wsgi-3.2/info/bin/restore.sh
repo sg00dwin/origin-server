e45fc21875c86329cb2172ec6a097e70cab56f47
Dan McPherson <dmcphers@redhat.com>
2011-08-12 15:40:37 -0400

    add stop deploy start to restore

--- cartridges/wsgi-3.2/info/bin/restore.sh
+++ cartridges/wsgi-3.2/info/bin/restore.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+set -e
 
 application="$1"
 include_git="$2"
-
+  
+GIT_DIR=~/git/${application}.git/
+  
 if [ "$include_git" = "INCLUDE_GIT" ]
 then
+  GIT_DIR="$GIT_DIR" $GIT_DIR/hooks/pre-receive
   /bin/rm -rf ~/git/${application}.git/[^h]*/*
 fi
 
 /bin/rm -rf ~/${application}/data/* ~/${application}/data/.[^.]*
 
-
 if [ "$include_git" = "INCLUDE_GIT" ]
 then
   /bin/tar --overwrite -xvmz ./${application}/data ./git --exclude=git/${application}.git/hooks
+  GIT_DIR="$GIT_DIR" $GIT_DIR/hooks/post-receive
 else
   /bin/tar --overwrite -xvmz ./${application}/data
 fi
