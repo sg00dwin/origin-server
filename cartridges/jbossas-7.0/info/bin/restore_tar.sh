2c3700c12649c9394cd5a650b7f81cee403236bd
Dan McPherson <dmcphers@redhat.com>
2011-08-16 19:08:39 -0400

    redo the start/stop changes

---
+++ cartridges/jbossas-7.0/info/bin/restore_tar.sh
@@ -0,0 +1,16 @@
+include_git="$1"
+
+# Import Environment Variables
+for f in ~/.env/*
+do
+    . $f
+done
+
+if [ "$include_git" = "INCLUDE_GIT" ]
+then
+  echo "Restoring ~/git/${OPENSHIFT_APP_NAME}.git, ~/${OPENSHIFT_APP_NAME}/data and ~/.m2" 1>&2
+  /bin/tar --strip=2 --overwrite -xmz "./*/${OPENSHIFT_APP_NAME}/data" "./*/git" "./*/.m2" --exclude="./*/git/${OPENSHIFT_APP_NAME}.git/hooks" 1>&2  
+else
+  echo "Restoring ~/${OPENSHIFT_APP_NAME}/data" 1>&2
+  /bin/tar --strip=2 --overwrite -xmz "./*/${OPENSHIFT_APP_NAME}/data" 1>&2
+fi
--- node/cartridges/li-controller/info/bin/restore.sh
+++ node/cartridges/li-controller/info/bin/restore.sh
@@ -13,23 +13,24 @@ done
 
 if [ "$include_git" = "INCLUDE_GIT" ]
 then
+  ~/git/${application}.git/hooks/pre-receive 1>&2
   echo "Removing old git repo - ~/git/${application}.git/" 1>&2
   /bin/rm -rf ~/git/${application}.git/[^h]*/*
+else
+  stop_app.sh 1>&2
 fi
 
 echo "Removing old data dir - ~/${application}/data/*" 1>&2
 /bin/rm -rf ~/${application}/data/* ~/${application}/data/.[^.]*
 
+restore_tar.sh $include_git
 
 if [ "$include_git" = "INCLUDE_GIT" ]
 then
-  echo "Restoring ~/git/${application}.git and ~/${application}/data" 1>&2
-  /bin/tar --strip=2 --overwrite -xmz "./*/${application}/data" "./*/git" --exclude="./*/git/${application}.git/hooks" 1>&2
+  GIT_DIR=~/git/${application}.git/ ~/git/${application}.git/hooks/post-receive 1>&2  
 else
-  /bin/tar --strip=2 --overwrite -xmz "./*/${application}/data" 1>&2
+  start_app.sh 1>&2
 fi
-
-GIT_DIR=~/git/${application}.git/ ~/git/${application}.git/hooks/post-receive 1>&2
 
 for cmd in `awk 'BEGIN { for (a in ENVIRON) if (a ~ /_RESTORE$/) print ENVIRON[a] }'`
 do
