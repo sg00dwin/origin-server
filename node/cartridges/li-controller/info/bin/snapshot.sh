db315c78d693dfec5b8a127359f3e8a41f5eb246
Mike McGrath <mmcgrath@redhat.com>
2011-08-16 17:28:53 -0400

    prefixing backup and restore with UUID

--- node/cartridges/li-controller/info/bin/snapshot.sh
+++ node/cartridges/li-controller/info/bin/snapshot.sh
@@ -6,8 +6,8 @@ do
     . $f
 done
 
-# stop
-stop_app.sh 1>&2
+# pre-receive
+~/git/${OPENSHIFT_APP_NAME}.git/hooks/pre-receive 1>&2
 
 # Run pre-dump dumps
 for cmd in `awk 'BEGIN { for (a in ENVIRON) if (a ~ /_DUMP$/) print ENVIRON[a] }'`
@@ -17,13 +17,16 @@ do
 done
 
 # Run tar, saving to stdout
+cd ~
+cd ..
 echo "Creating and sending tar.gz" 1>&2
-/bin/tar --ignore-failed-read -czf - -C ~ \
-        --exclude=./.tmp \
-        --exclude=./.ssh \
-        --exclude=./$OPENSHIFT_APP_NAME/%s_ctl.sh \
-        --exclude=./$OPENSHIFT_APP_NAME/conf.d/libra.conf \
-        --exclude=./$OPENSHIFT_APP_NAME/run/httpd.pid .
+/bin/tar --ignore-failed-read -czf - \
+        --exclude=./$OPENSHIFT_APP_UUID/.tmp \
+        --exclude=./$OPENSHIFT_APP_UUID/.ssh \
+        --exclude=./$OPENSHIFT_APP_UUID/$OPENSHIFT_APP_NAME/%s_ctl.sh \
+        --exclude=./$OPENSHIFT_APP_UUID/$OPENSHIFT_APP_NAME/conf.d/libra.conf \
+        --exclude=./$OPENSHIFT_APP_UUID/$OPENSHIFT_APP_NAME/run/httpd.pid \
+        ./$OPENSHIFT_APP_UUID
 
 # Cleanup
 for cmd in `awk 'BEGIN { for (a in ENVIRON) if (a ~ /_DUMP_CLEANUP$/) print ENVIRON[a] }'`
@@ -32,5 +35,6 @@ do
     $cmd
 done
 
-# start_app
-start_app.sh 1>&2
+
+# post-receive
+GIT_DIR=~/git/${OPENSHIFT_APP_NAME}.git/ ~/git/${OPENSHIFT_APP_NAME}.git/hooks/post-receive 1>&2
