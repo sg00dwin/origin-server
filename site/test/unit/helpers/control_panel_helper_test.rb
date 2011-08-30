639f0de795766ebb0a57b194fe3f757ae6346271
Dan McPherson <dmcphers@redhat.com>
2011-08-29 23:33:31 -0400

    get test cases running

--- site/test/unit/helpers/control_panel_helper_test.rb
+++ site/test/unit/helpers/control_panel_helper_test.rb
@@ -4,17 +4,17 @@ class ControlPanelHelperTest < ActionView::TestCase
 
   def setup
     @userinfo = Object.new
-    @userinfo.namespace = 'testns'
-    @userinfo.rhc_domain = 'testdom'
-    @userinfo.app_info = { 'testapp' => { 'uuid' => 'testuuid' }}
+    @userinfo.stubs(:namespace).returns('testns')
+    @userinfo.stubs(:rhc_domain).returns('testdom')
+    @userinfo.stubs(:app_info).returns({ 'testapp' => { 'uuid' => 'testuuid' }})
   end
   
   test 'app url is correct' do
-    assert app_url_for @userinfo, 'testapp' == 'testapp-testns.testdom'
+    assert app_url_for (@userinfo, 'testapp') == 'testapp-testns.testdom'
   end
   
   test 'git url is correct' do
-    assert git_url_for @userinfo, 'testapp' == 'ssh://testuuid@testapp-testns.testdom/~/git/testapp.git/'
+    assert git_url_for (@userinfo, 'testapp') == 'ssh://testuuid@testapp-testns.testdom/~/git/testapp.git/'
   end
 
 end
