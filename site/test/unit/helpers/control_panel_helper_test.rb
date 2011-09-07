05fe2ad4a86a330d21046420810bfc1a8855ddde
Fotios Lindiakos <fotios@redhat.com>
2011-09-07 13:23:25 -0400

    Fixed warnings in control_panel_helper_test

--- site/test/unit/helpers/control_panel_helper_test.rb
+++ site/test/unit/helpers/control_panel_helper_test.rb
@@ -10,11 +10,11 @@ class ControlPanelHelperTest < ActionView::TestCase
   end
   
   test 'app url is correct' do
-    assert app_url_for (@userinfo, 'testapp') == 'testapp-testns.testdom'
+    assert app_url_for(@userinfo, 'testapp') == 'testapp-testns.testdom'
   end
   
   test 'git url is correct' do
-    assert git_url_for (@userinfo, 'testapp') == 'ssh://testuuid@testapp-testns.testdom/~/git/testapp.git/'
+    assert git_url_for(@userinfo, 'testapp') == 'ssh://testuuid@testapp-testns.testdom/~/git/testapp.git/'
   end
 
 end
