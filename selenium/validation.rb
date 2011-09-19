be4924eaf1a51060bd111dab2daa4103d4ef0e88
Fotios Lindiakos <fotios@redhat.com>
2011-09-19 14:41:59 -0400

    Refactored selenium/validation.rb to use Net::HTTP and be more efficient

--- selenium/validation.rb
+++ selenium/validation.rb
@@ -5,129 +5,158 @@ require "test/unit"
 require 'rubygems'
 require 'hpricot'
 require 'open-uri'
+require 'logger'
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'yaml'
 
 class Validation < Test::Unit::TestCase
   
   @@base_url = 'https://localhost'
   @@base_path = '/app/'
-  @@valid_responses = ['200', '302']
   @@ticket = 'test'
+  @@retry_limit = 3
+  @@local_hosts = [
+    'localhost',
+    'openshift.redhat.com'
+  ]
   
   @@blacklist = [
     '/wapps/sso/lostPassword.html'
   ]
-  
-  def test_all_links_are_valid
-    @tested_pages = []
-    invalid_links = get_invalid_links
-    @tested_pages = []
-    invalid_links.concat get_invalid_links('', true)
-    $logger.info "invalid links: #{invalid_links.inspect}"
-    puts "invalid links: #{invalid_links.inspect}"
-    assert invalid_links.empty?
+
+  $logger ||= Logger.new(STDERR)
+  $logger.level = Logger::INFO
+
+  def setup
+    puts "Making sure links are empty"
+    @@invalid_links = []
+    @@tested_pages = []
   end
   
-  def get_invalid_links(url='', logged_in=false, retries=0)
-    # get fully qualified url if needed
-    unless url.index 'http://' or url.index 'https://'
-      unless url[0, 1] == '/'
-        url = "#{@@base_url}#{@@base_path}#{url}"
+  def test_logged_in_links_are_valid
+    results = check_page('', true)
+    check_results
+  end
+
+  def test_all_public_links_are_valid
+    results = check_page('')
+    check_results
+  end
+
+  def check_results
+    $logger.info "Checked pages: #{YAML.dump @@tested_pages.sort}"
+    assert @@invalid_links.empty?, YAML.dump(@@invalid_links.sort)
+  end
+
+  def check_page(url, logged_in = false)
+    uri = build_uri(url)
+    # Make sure we don't retest pages
+    if @@tested_pages.include? uri.to_s
+      $logger.debug "Skipping #{uri.to_s}"
+      return
+    end
+    @@tested_pages << uri.to_s
+
+    # Make sure we're working with a valid URI
+    $logger.debug "Testing URI: #{uri.to_s}"
+
+    if link_is_testable? uri
+      # Only HEAD remote sites to save time
+      method = @@local_hosts.include?( uri.host ) ? :get : :head
+
+      # Make sure the page is valid
+      response = fetch(uri,{:logged_in => logged_in,:method => method})
+      case response
+      when Net::HTTPSuccess, Net::HTTPRedirection
+        if response.body
+          links = get_links(response.body)
+          links.each{|link| check_page(link,logged_in)}
+        end
       else
-        url = "#{@@base_url}#{url}"
+        @@invalid_links << url
       end
     end
-        
-    # make sure this link is valid
-    res_code = response_code(url)
-    unless @@valid_responses.include? res_code
-      $logger.info "res_code: #{res_code}  url: #{url}"
-      puts "res_code: #{res_code}  url: #{url}"
-      if retries > 2
-        return [url] #this is an invalid link
-      else
-        get_invalid_links(url, logged_in, retries + 1)
+  end
+
+  def build_uri(url)
+    $logger.debug("Building URI: #{url}")
+
+    uri = URI.parse(url)
+    if [URI::Generic].include? uri.class
+      # Some sort of relative URI
+      unless url.start_with?('/app')
+        url = @@base_path + url
       end
+      uri = URI.join(@@base_url,url)
     end
-    
-    links = (get_elements :a, url, logged_in).collect do |a|
-      a[:href]
+    uri
+  end
+
+  def fetch(uri, args)
+    args = {
+      :logged_in => false, 
+      :redirect => true, 
+      :limit => 10,
+      :method => :get,
+      :retries => 0
+    }.merge(args)
+
+    args[:limit]-=1
+
+    # You should choose better exception.
+    raise ArgumentError, 'HTTP redirect too deep' if args[:limit] == 0
+
+    $logger.debug "#{args[:method].to_s.upcase} #{"(retry #{args[:retries]})" if args[:retries] > 0} #{uri.to_s}"
+    headers = {
+      'Cookie' => "rh_sso=#{@ticket}"
+    }
+
+    opts = {}
+
+    http = Net::HTTP.new( uri.host, uri.port )
+    http.open_timeout = 10
+    http.read_timeout = 10
+    if uri.scheme == 'https'
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     end
-      
-    invalid_links = []
-    links.each do |link|
-      if link_is_testable? link
-        if link_is_local? link
-          unless @tested_pages.include? link
-            # recurse through openshift pages (once)
-            @tested_pages.push link
-            invalid_links.concat get_invalid_links(link)
-          end
+
+    response = http.start{|http|
+      begin
+        response = http.__send__(
+          args[:method],
+          (uri.path == '' ? '/' : uri.path),headers
+        )
+        if response == Net::HTTPRedirection && args[:redirect]
+          args[:limit] -= 1
+          fetch(response['location'], args)
         else
-          invalid_links << "page: #{url}; url: #{link}" unless @@valid_responses.include? response_code(link)
+          response
+        end
+      rescue Timeout::Error
+        args[:retries] += 1
+        if args[:retries] <= @@retry_limit
+          fetch(uri,args)
+        else
+          Net::HTTPResponse::CODE_CLASS_TO_OBJ['4']
         end
       end
-    end
-    return invalid_links
+    }
   end
-  
-  def get_page_source(url, logged_in = false, redirect = true)
-    opts = ''
-    opts << " --cookie rh_sso=#{@ticket}" if logged_in 
-    opts << ' -L' if redirect
-    `curl -s -k#{opts} #{url}`
+
+  # Source: http://goo.gl/enmv7
+  def get_links(doc)
+    (Hpricot(doc)/"a").map{|el| el[:href]}
   end
-  
-  def get_elements(elem, url, logged_in = false)
-    src = get_page_source url, logged_in
-    doc = Hpricot(src)
-    elems = []
-    (doc/elem).each do |el|
-      elems << el
-    end
-    return elems
-  end
-  
-  # determine if a link points to another openshift page
-  def link_is_local?(link)
-    # test absolute links
-    if link.index 'http://' or link.index 'https://'
-      if link.index 'openshift.redhat.com' or link.index @@base_url
-        true
-      else
-        false
-      end
-    else
-      # relative link, assume true
-      true
-    end
-  end
-  
+
   # determine if a link should be tested
-  def link_is_testable?(link)
-    if link[0, 1] == '#' or link.index 'mailto:' or in_blacklist? link
-      false
-    else
-      true
+  def link_is_testable?(uri)
+    retval = false
+    if [URI::HTTP,URI::HTTPS].include? uri.class 
+      retval = !(@@blacklist.include? uri.path) && uri.fragment.nil?
     end
+    return retval
   end
-  
-  def in_blacklist?(link)
-    @@blacklist.each do |blacklisted|
-      return true if link.index blacklisted
-    end
-    false
-  end
-  
-  def response_code(url, logged_in=false, redirect=true)
-    opts = ''
-    opts << " --cookie rh_sso=#{@ticket}" if logged_in 
-    opts << ' -L' if redirect 
-    headers = `curl -Ik#{opts} #{url}`
-    unless headers.nil? 
-      last_header = headers.split("\r\n\r\n").last
-      status = last_header.split("\r\n").first unless last_header.nil?
-      status.split(' ')[1] unless status.nil?
-    end
-  end
-  
 end
