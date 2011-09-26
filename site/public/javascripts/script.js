23b485a401e9d61e25bbbabc9bff6e53b9548e1d
Fotios Lindiakos <fotios@redhat.com>
2011-09-26 16:15:44 -0400

    Made AJAX forms work with validation

--- site/public/javascripts/script.js
+++ site/public/javascripts/script.js
@@ -1,4 +1,4 @@
-/* DO NOT MODIFY. This file was compiled Fri, 23 Sep 2011 19:53:44 GMT from
+/* DO NOT MODIFY. This file was compiled Mon, 26 Sep 2011 16:45:52 GMT from
  * /home/fotios/li/site/app/coffeescripts/script.coffee
  */
 
@@ -6,7 +6,7 @@
   var $;
   $ = jQuery;
   $(function() {
-    var ann_height, ann_list, announcements, body, close_btn, close_dialog, current, dialogs, links, login_complete, nav, nav_top, on_announcement, open_dialog, registration_complete, reset, reset_password_complete, scroll_announcements, sections, signin, signup, sticky_css, top, unsticky_css;
+    var ann_height, ann_list, announcements, body, current, links, nav, nav_top, on_announcement, scroll_announcements, sections, sticky_css, top, unsticky_css;
     body = $('body');
     nav = ($('header.universal > nav')).first();
     nav_top = nav.offset().top;
@@ -28,87 +28,6 @@
         return nav.css(unsticky_css);
       }
     });
-    dialogs = $('.dialog');
-    open_dialog = function(dialog) {
-      dialogs.hide();
-      dialog.show();
-      return ($(window, 'html', 'body')).scrollTop(0);
-    };
-    close_dialog = function(dialog) {
-      return dialog.hide();
-    };
-    close_btn = $('.close_button');
-    signup = $('#signup');
-    signin = $('#signin');
-    reset = $('#reset_password');
-    ($('a.sign_up')).click(function(event) {
-      event.preventDefault();
-      return open_dialog(signup);
-    });
-    ($('a.sign_in')).click(function(event) {
-      event.preventDefault();
-      return open_dialog(signin);
-    });
-    ($('a.password_reset')).click(function(event) {
-      event.preventDefault();
-      return open_dialog(reset);
-    });
-    close_btn.click(function(event) {
-      return close_dialog(($(this)).parent());
-    });
-    login_complete = function(xhr, status) {
-      var $err_div, json;
-      json = $.parseJSON(status.responseText);
-      console.log(json);
-      $(this).parent().find('div.message.error').remove();
-      $err_div = $('<div>').addClass('message error').hide().insertBefore(this);
-      switch (status.status) {
-        case 200:
-          window.location.replace(json.redirectUrl);
-          break;
-        case 401:
-          $err_div.text(json.error).show();
-          break;
-        default:
-          $err_div.html(json.error || "Some unknown error occured,<br/> please try again.").show();
-          return console.log('Some unknown AJAX error with the login', status.status);
-      }
-    };
-    signin.find('form').bind('ajax:complete', login_complete);
-    ($('#login-form')).find('form').bind('ajax:complete', login_complete);
-    registration_complete = function(xhr, status) {
-      var $err_div, form, json, messages;
-      form = $(this);
-      json = $.parseJSON(status.responseText);
-      console.log("Reg complete, got JSON", json);
-      $(this).parent().find('div.message.error').remove();
-      $err_div = $('<div>').addClass('message error').hide().insertBefore(this);
-      messages = $.map(json, function(k, v) {
-        return k;
-      });
-      if (json['redirectUrl'] === void 0 || json['redirectUrl'] === null) {
-        $.each(messages, function(i, val) {
-          return $err_div.addClass('error').append($('<div>').html(val));
-        });
-        $err_div.show();
-        if (typeof Recaptcha !== 'undefined') {
-          return Recaptcha.reload();
-        }
-      } else {
-        return window.location.replace(json['redirectUrl']);
-      }
-    };
-    signup.find('form').bind('ajax:complete', registration_complete);
-    ($('#new_web_user')).bind('ajax:complete', registration_complete);
-    reset_password_complete = function(xhr, status) {
-      var $div, form, json;
-      form = $(this);
-      json = $.parseJSON(status.responseText);
-      console.log("Reset password complete, got JSON", json);
-      $(this).parent().find('div.message').remove();
-      return $div = $('<div>').addClass("message " + json.status).text(json.message).insertBefore(this);
-    };
-    reset.find('form').bind('ajax:complete', reset_password_complete);
     announcements = $('#announcements');
     ann_list = $('ul', announcements);
     on_announcement = 0;
