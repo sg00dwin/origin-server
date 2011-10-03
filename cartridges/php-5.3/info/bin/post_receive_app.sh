a7d7ccd4efd65f1d02a26f2c5de67c6b86247a0e
Mike McGrath <mmcgrath@redhat.com>
2011-10-03 14:42:58 -0400

    correcting dir names for pear cleanup

--- cartridges/php-5.3/info/bin/post_receive_app.sh
+++ cartridges/php-5.3/info/bin/post_receive_app.sh
@@ -10,10 +10,11 @@ done
 
 redeploy_repo_dir.sh
 
-if [ -f "${OPENSHIFT_APP_DIR}/.openshift/markers/force_clean_build" ]
+if [ -f "${OPENSHIFT_REPO_DIR}/.openshift/markers/force_clean_build" ]
+then
     echo ".openshift/markers/force_clean_build found!  Recreating pear libs" 1>&2
     rm -rf "${OPENSHIFT_APP_DIR}"/phplib/pear/*
-    mkdir -p phplib/pear/{docs,ext,php,cache,cfg,data,download,temp,tests,www}
+    mkdir -p "${OPENSHIFT_APP_DIR}"/phplib/pear/{docs,ext,php,cache,cfg,data,download,temp,tests,www}
     pear -c ~/.pearrc config-set php_ini "${OPENSHIFT_APP_DIR}/conf/php.ini"
 fi
 
