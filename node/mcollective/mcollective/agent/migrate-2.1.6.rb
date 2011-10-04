57d23823855d749b73aa9781e9f4a30a8d5e7c4e
Dan McPherson <dmcphers@redhat.com>
2011-10-04 18:48:47 -0400

    beginning of migrate 2.1.6

--- node/mcollective/mcollective/agent/migrate-2.1.6.rb
+++ node/mcollective/mcollective/agent/migrate-2.1.6.rb
@@ -10,18 +10,16 @@ module LibraMigration
     node_config = ParseConfig.new('/etc/libra/node.conf')
     libra_home = '/var/lib/libra' #node_config.get_value('libra_dir')
     libra_server = node_config.get_value('libra_server')
+    libra_domain = node_config.get_value('libra_domain')
     app_home = "#{libra_home}/#{uuid}"
-    #app_dir = "#{app_home}/#{app_name}"
+    app_dir = "#{app_home}/#{app_name}"
     output = ''
     exitcode = 0
     if (File.exists?(app_home) && !File.symlink?(app_home))
-      post_receive = "#{app_home}/git/#{app_name}.git/hooks/post-receive"
-      if Util.contains_value?(post_receive, 'curl -s -O /dev/null -d')
-        output += "post-receive located at '#{post_receive}' already contains the necessary nurture curl\n"
-      else
-        nurture_curl = "curl -s -O /dev/null -d \"json_data=\\\"{\\\"app_uuid\\\":\\\"#{uuid}\\\",\\\"action\\\":\\\"push\\\"}\\\"\" https://#{libra_server}/broker/nurture >/dev/null 2>&1 &"
-        output += Util.append_to_file(post_receive, nurture_curl)
-      end
+      
+      cartridge_root_dir = "/usr/libexec/li/cartridges"
+      cartridge_dir = "#{cartridge_root_dir}/#{app_type}"
+      
     else
       exitcode = 127
       output += "Application not found to migrate: #{app_home}\n"
