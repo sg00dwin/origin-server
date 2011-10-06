a207f2466608e5d419132ca0c048ee8c1bdd7296
Dan McPherson <dmcphers@redhat.com>
2011-10-06 17:26:05 -0400

    cleanup

--- cartridges/raw-0.1/info/bin/post_receive_app.sh
+++ cartridges/raw-0.1/info/bin/post_receive_app.sh
@@ -16,21 +16,6 @@ fi
 
 redeploy_repo_dir.sh
 
-if [ -f ~/${OPENSHIFT_APP_NAME}/repo/deplist.txt ]
-then
-    for f in $(cat ~/${OPENSHIFT_APP_NAME}/repo/deplist.txt)
-    do
-        echo "Checking pear: $f"
-        echo
-        if pear list "$f" > /dev/null
-        then
-            pear upgrade "$f"
-        else
-            pear install --alldeps "$f"
-        fi
-    done
-fi
-
 if $JENKINS_ENABLED
 then
   jenkins-cli build -s ${OPENSHIFT_APP_NAME}-build 
