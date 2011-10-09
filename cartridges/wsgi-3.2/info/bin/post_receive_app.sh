151d2545f47a481fcc5a4d34c902f9fb8b25c80e
Mike McGrath <mmcgrath@redhat.com>
2011-10-08 22:37:02 -0400

    prepping for jenkins integration

--- cartridges/wsgi-3.2/info/bin/post_receive_app.sh
+++ cartridges/wsgi-3.2/info/bin/post_receive_app.sh
@@ -8,44 +8,66 @@ do
     . $f
 done
 
-JENKINS_ENABLED=false
 if [ -f ~/.env/OPENSHIFT_CI_TYPE ]
 then
   JENKINS_ENABLED=true
 fi
 
-redeploy_repo_dir.sh
-
-if [ -f "${OPENSHIFT_REPO_DIR}/.openshift/markers/force_clean_build" ]
+if [ -z "$JENKINS_ENABLED" ]
 then
-    echo ".openshift/markers/force_clean_build found!  Recreating virtenv" 1>&2
-    rm -rf "${OPENSHIFT_APP_DIR}"/virtenv/*
+    # Run only if jenkins is not enabled
+    redeploy_repo_dir.sh
 fi
 
-
-if [ -f ${OPENSHIFT_REPO_DIR}setup.py ]
+if [ -z "$JENKINS_ENABLED" ] || [ -n "$BUILD_NUMBER" ]
 then
-    echo "setup.py found.  Setting up virtualenv"
+    # Run when jenkins is not being used or run when inside a build
+    if [ -f "${OPENSHIFT_REPO_DIR}/.openshift/markers/force_clean_build" ]
+    then
+        echo ".openshift/markers/force_clean_build found!  Recreating virtenv" 1>&2
+        rm -rf "${OPENSHIFT_APP_DIR}"/virtenv/*
+    fi
+
+    if [ -f ${OPENSHIFT_REPO_DIR}setup.py ]
+    then
+        echo "setup.py found.  Setting up virtualenv"
+        cd ~/${OPENSHIFT_APP_NAME}/virtenv
+        virtualenv ~/${OPENSHIFT_APP_NAME}/virtenv
+        . ./bin/activate
+        python ${OPENSHIFT_REPO_DIR}setup.py develop
+        virtualenv --relocatable ~/${OPENSHIFT_APP_NAME}/virtenv
+    fi
+
+fi
+
+if [ -n "$JENKINS_ENABLED" ]
+then
+    echo "Executing Jenkins build..."
+    echo "You can track your build at http://jenktest-mmcgrath000.dev.rhcloud.com/job/${OPENSHIFT_APP_NAME}-build"
+    jenkins-cli build -s ${OPENSHIFT_APP_NAME}-build 
+fi
+
+if [ -z "$BUILD_NUMBER" ]
+then
+    # Not inside a a build
+    # Run build
+    #virtualenv --relocatable ~/${OPENSHIFT_APP_NAME}/virtenv
+    #. ./bin/activate
     cd ~/${OPENSHIFT_APP_NAME}/virtenv
-    virtualenv ~/${OPENSHIFT_APP_NAME}/virtenv
     . ./bin/activate
-    python ${OPENSHIFT_REPO_DIR}setup.py develop
+    user_build.sh
 fi
 
-if $JENKINS_ENABLED
+if [ -n "$JENKINS_ENABLED" ]
 then
-  jenkins-cli build -s ${OPENSHIFT_APP_NAME}-build 
-fi
-
-# Run build
-user_build.sh
-
-if $JENKINS_ENABLED
-then
-  restart_app.sh
-else
   # Start the app
   start_app.sh
+else
+  restart_app.sh
 fi
 
-nurture_app_push.sh $libra_server
+if [ -z "$BUILD_NUMBER" ]
+then
+    # Not running inside a build
+    nurture_app_push.sh $libra_server
+fi
