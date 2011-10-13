843647c0e59d687104643d4aee949b248fa5721b
Fotios Lindiakos <fotios@redhat.com>
2011-10-13 14:51:14 -0400

    Added code to automatically download the Sauce-Connect.jar if it doesn't already exist

--- selenium/sauce/helper.rb
+++ selenium/sauce/helper.rb
@@ -2,6 +2,8 @@
 require "rubygems"
 require "sauce"
 require 'socket'
+require 'net/http'
+require 'zipruby'
 
 # Load OpenShift modules
 
@@ -17,20 +19,34 @@ Sauce.config do |config|
   # config.application_port = "443"
 end
 
-@ready_file = 'sauce_ready'
+ready_file = 'sauce_ready'
+sauce_file = 'Sauce-Connect.jar'
+
+unless File.exists?(sauce_file)
+  puts "Fetching Sauce-Connect.jar"
+  url = "http://saucelabs.com/downloads/Sauce-Connect-latest.zip"
+
+  Zip::Archive.open_buffer(Net::HTTP.get(URI.parse(url))) do |zf|
+    zf.fopen(sauce_file) do |f|
+      open(sauce_file,'wb') do |file|
+        file.write(f.read)
+      end
+    end
+  end
+end
 
 # Get the Sauce configuration information
 cfg = Sauce::Config.new()
 # Create a command to spawn the jar file
-cmd = "java -jar Sauce-Connect.jar -f #{@ready_file} #{cfg.opts[:username]} #{cfg.opts[:access_key]}"
+cmd = "java -jar #{sauce_file} -f #{ready_file} #{cfg.opts[:username]} #{cfg.opts[:access_key]}"
 
 # Run the command
 `#{cmd} > /dev/null 2>&1 &`
-@pid = $?.pid
-puts "Connect PID: #{@pid}"
+@sauce_connect_pid = $?.pid
+puts "Connect PID: #{@sauce_connect_pid}"
 
 # Wait for Sauce to be ready
-until File.exists?(@ready_file)
+until File.exists?(ready_file)
   puts "Waiting for Sauce Connect"
   sleep 5
 end
