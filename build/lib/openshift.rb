6d78eee9d4747ad05e17302b0d2046f033afd69d
Matt Hicks <mhicks@redhat.com>
2011-10-18 20:22:02 -0400

    Adding back send verified email

--- build/lib/openshift.rb
+++ build/lib/openshift.rb
@@ -1,4 +1,5 @@
 require 'logger'
+require 'net/smtp'
 require 'lib/openshift/constants'
 require 'lib/openshift/ssh'
 require 'lib/openshift/tito'
@@ -18,4 +19,19 @@ end
 def exit_msg(msg)
   puts msg
   exit 0
+end
+
+def send_verified_email(image_id, image_name)
+  msg = <<END_OF_MESSAGE   
+From: Jenkins <noreply@redhat.com>
+To: Libra Team <libra-express@redhat.com>
+Subject: [Jenkins] DevEnv Image #{image_name} (#{image_id}) is QE Ready
+
+Image #{image_name} (#{image_id}) has passed validation tests and is ready for QE.
+
+END_OF_MESSAGE
+
+  Net::SMTP.start('localhost') do |smtp|
+    smtp.send_message msg, "noreply@redhat.com", "libra-express@redhat.com"
+  end
 end
