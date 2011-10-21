13a2346adcd7419a611cf6b3004f1da5a6fefa8e
Dan McPherson <dmcphers@redhat.com>
2011-10-21 11:14:39 -0400

    bug 747529

--- cartridges/wsgi-3.2/info/bin/deploy.sh
+++ cartridges/wsgi-3.2/info/bin/deploy.sh
@@ -7,9 +7,14 @@ do
 done
 
 # Run build
-#virtualenv --relocatable ~/${OPENSHIFT_APP_NAME}/virtenv
+#virtualenv --relocatable ${OPENSHIFT_APP_DIR}virtenv
 #. ./bin/activate
-cd ~/${OPENSHIFT_APP_NAME}/virtenv
-. ./bin/activate
+
+if [ -d ${OPENSHIFT_APP_DIR}virtenv ]
+then 
+    pushd ${OPENSHIFT_APP_DIR}virtenv > /dev/null
+    . ./bin/activate
+    popd > /dev/null
+fi
 
 user_deploy.sh
