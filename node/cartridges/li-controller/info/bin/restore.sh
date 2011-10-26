67133737f4d71b12b3ea49dd5f2059327e716a0a
Dan McPherson <dmcphers@redhat.com>
2011-10-26 11:21:34 -0400

    bug 749133

--- node/cartridges/li-controller/info/bin/restore.sh
+++ node/cartridges/li-controller/info/bin/restore.sh
@@ -11,13 +11,13 @@ done
 if [ "$include_git" = "INCLUDE_GIT" ]
 then
   ~/git/${OPENSHIFT_APP_NAME}.git/hooks/pre-receive 1>&2
-  echo "Removing old git repo - ~/git/${OPENSHIFT_APP_NAME}.git/" 1>&2
+  echo "Removing old git repo: ~/git/${OPENSHIFT_APP_NAME}.git/" 1>&2
   /bin/rm -rf ~/git/${OPENSHIFT_APP_NAME}.git/[^h]*/*
 else
   stop_app.sh 1>&2
 fi
 
-echo "Removing old data dir - ~/${OPENSHIFT_APP_NAME}/data/*" 1>&2
+echo "Removing old data dir: ~/${OPENSHIFT_APP_NAME}/data/*" 1>&2
 /bin/rm -rf ~/${OPENSHIFT_APP_NAME}/data/* ~/${OPENSHIFT_APP_NAME}/data/.[^.]*
 
 restore_tar.sh $include_git
