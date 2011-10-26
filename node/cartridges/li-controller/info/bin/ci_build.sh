a80eb6a37b90540b5da461ed8d05228cb2ed8186
Dan McPherson <dmcphers@redhat.com>
2011-10-26 00:15:50 -0400

    bug 749069

--- node/cartridges/li-controller/info/bin/ci_build.sh
+++ node/cartridges/li-controller/info/bin/ci_build.sh
@@ -8,8 +8,8 @@ done
 
 if [ -n "$JENKINS_URL" ]
 then
-	REPO_LINK=`echo "${OPENSHIFT_REPO_DIR}" | sed -e "s/\/*$//" `
-    rm -rf REPO_LINK
+	REPO_LINK=${OPENSHIFT_APP_DIR}/runtime/repo
+    rm -rf $REPO_LINK
     ln -s ~/$WORKSPACE $REPO_LINK
 fi
 
