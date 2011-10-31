b3fe5c5b264ed1e4199460a8e19264c6ecfd10d8
Troy Dawson <tdawson@redhat.com>
2011-10-31 10:39:08 -0500

    adding local mirror capability

--- cartridges/perl-5.10/info/bin/build.sh
+++ cartridges/perl-5.10/info/bin/build.sh
@@ -9,11 +9,17 @@ done
 # Run when jenkins is not being used or run when inside a build
 export PERL5LIB="${OPENSHIFT_REPO_DIR}libs:~/${OPENSHIFT_APP_NAME}/perl5lib"
 
+if `echo $OPENSHIFT_APP_DNS | grep -q stg.rhcloud.com` ; then 
+	LOCALMIRROR="http://mirror1.stg.rhcloud.com/mirror/perl/CPAN/"
+else 
+	LOCALMIRROR="http://mirror1.prod.rhcloud.com/mirror/perl/CPAN/"
+fi
+
 if [ -f ${OPENSHIFT_REPO_DIR}deplist.txt ]
 then
     for f in $( (find ${OPENSHIFT_REPO_DIR} -type f | grep -e "\.pm$\|.pl$" | xargs /usr/lib/rpm/perl.req | awk '{ print $1 }' | sed 's/^perl(\(.*\))$/\1/'; cat ${OPENSHIFT_REPO_DIR}deplist.txt ) | sort | uniq)
     do
-        cpanm -L ~/${OPENSHIFT_APP_NAME}/perl5lib "$f"
+        cpanm --mirror $LOCALMIRROR -L ~/${OPENSHIFT_APP_NAME}/perl5lib "$f"
     done
 fi
 
