0a406b6ddedce39ac61d4430f7ef39907f0d86e0
Troy Dawson <tdawson@redhat.com>
2011-11-04 14:57:13 -0500

    dev machines now mirror to the stg mirror

--- cartridges/perl-5.10/info/bin/build.sh
+++ cartridges/perl-5.10/info/bin/build.sh
@@ -9,7 +9,8 @@ done
 # Run when jenkins is not being used or run when inside a build
 export PERL5LIB="${OPENSHIFT_REPO_DIR}libs:~/${OPENSHIFT_APP_NAME}/perl5lib"
 
-if `echo $OPENSHIFT_APP_DNS | grep -q stg.rhcloud.com` ; then 
+if `echo $OPENSHIFT_APP_DNS | grep -q .stg.rhcloud.com` || `echo $OPENSHIFT_APP_DNS | grep -q .dev.rhcloud.com`
+then 
 	LOCALMIRROR="http://mirror1.stg.rhcloud.com/mirror/perl/CPAN/"
 else 
 	LOCALMIRROR="http://mirror1.prod.rhcloud.com/mirror/perl/CPAN/"
@@ -19,7 +20,12 @@ if [ -f ${OPENSHIFT_REPO_DIR}deplist.txt ]
 then
     for f in $( (find ${OPENSHIFT_REPO_DIR} -type f | grep -e "\.pm$\|.pl$" | xargs /usr/lib/rpm/perl.req | awk '{ print $1 }' | sed 's/^perl(\(.*\))$/\1/'; cat ${OPENSHIFT_REPO_DIR}deplist.txt ) | sort | uniq)
     do
-        cpanm --mirror $LOCALMIRROR -L ~/${OPENSHIFT_APP_NAME}/perl5lib "$f"
+        if [ -n "$LOCALMIRROR" ]
+        then
+            cpanm --mirror $LOCALMIRROR -L ~/${OPENSHIFT_APP_NAME}/perl5lib "$f"
+        else
+            cpanm -L ~/${OPENSHIFT_APP_NAME}/perl5lib "$f"
+        fi
     done
 fi
 
