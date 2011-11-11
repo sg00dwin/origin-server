020f5ab2c6e49df941cafd6f2116802e240889bb
Alex Boone <aboone@redhat.com>
2011-11-11 15:41:18 -0500

    Add flags to devenv#test for setting Sauce os/browser/version

--- build/lib/openshift/constants.rb
+++ build/lib/openshift/constants.rb
@@ -13,5 +13,8 @@ VERIFIED_TAG = "qe-ready"
 RSA = File.expand_path("~/.ssh/libra.pem")
 SAUCE_USER = "openshift_ci"
 SAUCE_SECRET = "3d67e770-ce7d-482a-8c7f-07aec039d564"
+SAUCE_OS = "Windows 2008"
+SAUCE_BROWSER = "firefox"
+SAUCE_BROWSER_VERSION = "7"
 
 $amz_options = {:key_name => KEY_PAIR, :instance_type => TYPE, :availability_zone => ZONE}
