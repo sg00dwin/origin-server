73dd6b71a41b6ec34d3a4f79c753688b2d94b60c
Matt Hicks <mhicks@redhat.com>
2011-11-11 09:47:18 -0500

    Cutover to running Selenium in Sauce Labs

--- build/lib/openshift/constants.rb
+++ build/lib/openshift/constants.rb
@@ -11,5 +11,7 @@ DEVENV_CLEAN_WILDCARD = "devenv-clean_*"
 TERMINATE_REGEX = /terminate/
 VERIFIED_TAG = "qe-ready"
 RSA = File.expand_path("~/.ssh/libra.pem")
+SAUCE_USER = "openshift_ci"
+SAUCE_SECRET = "3d67e770-ce7d-482a-8c7f-07aec039d564"
 
 $amz_options = {:key_name => KEY_PAIR, :instance_type => TYPE, :availability_zone => ZONE}
