db7f72f1f89da9e76b826e17d83826d086cc674c
Fotios Lindiakos <fotios@redhat.com>
2011-12-02 17:44:50 -0500

    Added server side validation for SSH keys for BZ 759362 and 755375

--- site/app/controllers/express_domain_controller.rb
+++ site/app/controllers/express_domain_controller.rb
@@ -4,42 +4,59 @@ class ExpressDomainController < ApplicationController
   def create
     # Get only relevant parameters
     domain_params = params[:express_domain]
+
+    # Make sure that if there is an SSH key provided, it's valid
+    if ssh = domain_params[:ssh]
+      @ssh_key_validation = validate_ssh(ssh)
+    end
+
     # check for which domain action we should call
     @dom_action = domain_params.delete :dom_action
     form_type = domain_params.delete :form_type
-    Rails.logger.debug "dom_action: #{@dom_action}"
-    domain_params[:rhlogin] = session[:login]
-    domain_params[:ticket] = cookies[:rh_sso]
-    domain_params[:password] = ''
-    @domain = ExpressDomain.new(domain_params)
     @event = "#{form_type}_form_return"
-    ajax_response = {}
-    if @domain.valid?
-      begin
-        if @dom_action == 'create'
-          Rails.logger.debug 'creating domain'
-          @domain.create do |json_response|
-            ajax_response = process_response json_response
-          end
-        elsif @dom_action == 'update'
-          @domain.update do |json_response|
-            ajax_response = process_response json_response
-          end
-        end #end if action
-      rescue Exception
-        # Exception messages are recorded in the error hash in ExpressApi
+
+    if @ssh_key_validation[:valid]
+      # Ensure we only send the key until the broker supports comments
+      domain_params[:ssh] = @ssh_key_validation[:key]
+
+      Rails.logger.debug "dom_action: #{@dom_action}"
+      domain_params[:rhlogin] = session[:login]
+      domain_params[:ticket] = cookies[:rh_sso]
+      domain_params[:password] = ''
+      @domain = ExpressDomain.new(domain_params)
+      ajax_response = {}
+      if @domain.valid?
+        begin
+          if @dom_action == 'create'
+            Rails.logger.debug 'creating domain'
+            @domain.create do |json_response|
+              ajax_response = process_response json_response
+            end
+          elsif @dom_action == 'update'
+            @domain.update do |json_response|
+              ajax_response = process_response json_response
+            end
+          end #end if action
+        rescue Exception
+          # Exception messages are recorded in the error hash in ExpressApi
+          @message = @domain.errors.full_messages.join("; ")
+          @message_type = :error
+          ajax_response = {:status => 'error', :data => @message, :event => @event}
+        end
+      else
+        # display validation errors
         @message = @domain.errors.full_messages.join("; ")
         @message_type = :error
+        Rails.logger.error "Validation error: #{@message}"
         ajax_response = {:status => 'error', :data => @message, :event => @event}
       end
     else
-      # display validation errors
-      @message = @domain.errors.full_messages.join("; ")
+      @message = "The SSH key supplied was invalid"
       @message_type = :error
       Rails.logger.error "Validation error: #{@message}"
       ajax_response = {:status => 'error', :data => @message, :event => @event}
     end
-    
+
     # respond based on requested format
     respond_to do |format|
       format.html do
@@ -51,7 +68,7 @@ class ExpressDomainController < ApplicationController
       end
     end
   end
-  
+
   def process_response(json_response)
     Rails.logger.debug "Domain api result: #{json_response.inspect}"
     # check that we have expected result
@@ -72,5 +89,38 @@ class ExpressDomainController < ApplicationController
       response = {:status => 'error', :data => @message, :event => @event}
     end
     return response
+  end
+
+  def validate_ssh(ssh)
+    type_regex = /^ssh-(rsa|dss)$/
+    key_regex =  /^[A-Za-z0-9+\/]+[=]*$/
+    values = { :valid => true }
+    parts = ssh.split
+
+    case parts.length
+    when 1
+      values[:key] = parts[0]
+    when 2
+      if type_regex.match(parts[0])
+        values[:type] = parts[0]
+        values[:key] = parts[1]
+      else
+        values[:key] = parts[0]
+        values[:comment] = parts[1]
+      end
+    when 3
+      values[:type] = parts[0]
+      values[:key] = parts[1]
+      values[:comment] = parts[2]
+    end
+
+    if values[:type] && !type_regex.match(values[:type])
+      values[:valid] = false
+    end
+
+    if values[:key] && !key_regex.match(values[:key])
+      values[:valid] = false
+    end
+    values
   end
 end
