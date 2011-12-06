562606e233d785bb14882a1548d6e0f8362dc46a
Fabiano Franz <ffranz@redhat.com>
2011-12-06 19:43:16 -0200

    Removed all references to Power on /app

--- site/app/models/cloud_access.rb
+++ site/app/models/cloud_access.rb
@@ -1,15 +1,13 @@
 class CloudAccess
   EXPRESS = 1
   FLEX = 2
-  POWER = 3
   
-  IDS = [EXPRESS, FLEX, POWER]
+  IDS = [EXPRESS, FLEX]
   
   EXPRESS_NAME = 'express'
   FLEX_NAME = 'flex'
-  POWER_NAME = 'power'
   
-  NAME_TO_ID = {EXPRESS_NAME => EXPRESS, FLEX_NAME => FLEX, POWER_NAME => POWER}
+  NAME_TO_ID = {EXPRESS_NAME => EXPRESS, FLEX_NAME => FLEX}
   ID_TO_NAME = NAME_TO_ID.invert
   
   def self.access_id(name)
