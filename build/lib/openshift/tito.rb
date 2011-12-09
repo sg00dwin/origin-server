07ed2948c364ecf44bd7adcccb0fbe1d1406efd8
Matt Hicks <mhicks@redhat.com>
2011-12-09 00:18:28 -0500

    Fixing the tito tag to directory mapping logic

--- build/lib/openshift/tito.rb
+++ build/lib/openshift/tito.rb
@@ -4,13 +4,13 @@ module OpenShift
       # Figure out what needs to be built
       packages = `tito report --untagged-commits`
       build_dirs = packages.split("\n").collect do |package|
-        if package =~ /^rhc-devenv-0/
+        if package =~ /^rhc-devenv-\d/
           "misc/devenv"
-        elsif package =~ /^rubygem-cloud-sdk-0/
+        elsif package =~ /^rubygem-cloud-sdk-\d/
           "cloud-sdk"
-        elsif package =~ /^rhc-(cartridge-.*)-0/
+        elsif package =~ /^rhc-(cartridge-[\w-]*\d\.\d)-/
           "cartridges/" + $1['cartridge-'.length..-1]
-        elsif package =~ /^rhc-(.*)-0/
+        elsif package =~ /^(rhc-[\w-]*\d\.\d)-/
           $1
         end
       end.compact
