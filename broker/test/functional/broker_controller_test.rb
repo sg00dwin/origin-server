11407c8f7b6878f92d7889f62705711555bf5be0
Alex Boone <aboone@redhat.com>
2011-12-13 14:06:42 -0800

    Cache cartridge list, default TTL of 1 hour

---
+++ broker/test/functional/broker_controller_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class BrokerControllerTest < ActionController::TestCase
+  test "cart list" do
+    # setup cache
+    Rails.cache.clear
+    Rails.application.config.action_controller.perform_caching = true
+
+    # should be a cache miss
+    resp = post(:cart_list_post, {:json_data => '{"cart_type" : "standalone"}'})
+    assert_equal 200, resp.status
+    body1 = resp.body
+
+    # should be a cache hit
+    resp = post(:cart_list_post, {:json_data => '{"cart_type" : "standalone"}'})
+    assert_equal 200, resp.status
+    body2 = resp.body
+
+    assert body1 == body2
+  end
+
+  test "embedded cart list" do
+    # setup cache
+    Rails.cache.clear
+    Rails.application.config.action_controller.perform_caching = true
+
+    # should be a cache miss
+    resp = post(:cart_list_post, {:json_data => '{"cart_type" : "embedded"}'})
+    assert_equal 200, resp.status
+    body1 = resp.body
+
+    # should be a cache hit
+    resp = post(:cart_list_post, {:json_data => '{"cart_type" : "embedded"}'})
+    assert_equal 200, resp.status
+    body2 = resp.body
+
+    assert body1 == body2
+  end
+
+end
+
