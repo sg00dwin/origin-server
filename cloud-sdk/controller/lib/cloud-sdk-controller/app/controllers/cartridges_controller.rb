4954bba713ca5b83d069f8aa0b928cdbf108e4c5
Lili Nader <lnader@dhcp-240-165.mad.redhat.com>
2012-01-11 19:54:35 -0800

    REST API bug fixes

--- cloud-sdk/controller/lib/cloud-sdk-controller/app/controllers/cartridges_controller.rb
+++ cloud-sdk/controller/lib/cloud-sdk-controller/app/controllers/cartridges_controller.rb
@@ -3,16 +3,20 @@ class CartridgesController < BaseController
   
   # GET /cartridges
   def index
+    cart_type = "standalone"
+    cache_key = "cart_list_#{cart_type}"
     carts = get_cached(cache_key, :expires_in => 21600.seconds) {
-      Application.get_available_cartridges("standalone")}
+      Application.get_available_cartridges(cart_type)}
     @result = Result.new(:ok, "cartridges", carts)
     respond_with(@result, :status => :ok)
   end
   
   # GET /cartridges/embedded
   def embedded
+    cart_type = "embedded"
+    cache_key = "cart_list_#{cart_type}"
     carts = get_cached(cache_key, :expires_in => 21600.seconds) {
-      Application.get_available_cartridges("embedded")}
+      Application.get_available_cartridges(cart_type)}
     @result = Result.new(:ok, "embedded cartridges", carts)
     respond_with(@result, :status => :ok)
   end
