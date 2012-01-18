f0dae73edd34d905be61793b8b09e96cca3e05a4
Ravi Sankar <rpenta@redhat.com>
2012-01-18 09:15:48 -0800

    enable auth for mongo connection + misc bug fixes

--- cloud-sdk/common/lib/cloud-sdk-common/models/user_model.rb
+++ cloud-sdk/common/lib/cloud-sdk-common/models/user_model.rb
@@ -11,20 +11,19 @@ module Cloud
         data = DataStore.instance.find(self.name,login,id)
         return nil unless data
         
-        json = data.values[0]
-        obj = self.new.from_json(json)
+        obj = self.new.from_json(data.values[0])
         obj.instance_variable_set("@#{id_var}", data.keys[0])
         obj.reset_state
         obj
       end
       
       def self.find_all(login)
-        id_var = @primary_key || "uuid"
-        data = DataStore.instance.find_all(self.name,login)
-        return [] unless data
-        data.map do |id, json|
-          obj = self.new.from_json(json)
-          obj.instance_variable_set("@#{id_var}", id)
+        id_var = @primary_key || "uuid"        
+        data_list = DataStore.instance.find_all(self.name,login)
+        return [] unless data_list
+        data_list.map! do |data|
+          obj = self.new.from_json(data.values[0])
+          obj.instance_variable_set("@#{id_var}", data.keys[0])
           obj.reset_state
           obj
         end
@@ -42,7 +41,7 @@ module Cloud
         @new_record = false
         @persisted = true
         @deleted = false
-        DataStore.instance.save(self.class.name, login, instance_variable_get("@#{id_var}"), self.to_json)
+        DataStore.instance.save(self.class.name, login, instance_variable_get("@#{id_var}"), self.attributes)
         self
       end
       
