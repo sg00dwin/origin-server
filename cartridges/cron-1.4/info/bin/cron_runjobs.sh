94cc13d9077119664e0e95d1a9d66821cf6c67b4
Ram Ranganathan <ramr@redhat.com>
2012-01-24 19:03:36 -0800

    Add run time limits + added some log messages for auditing purposes.

--- cartridges/cron-1.4/info/bin/cron_runjobs.sh
+++ cartridges/cron-1.4/info/bin/cron_runjobs.sh
@@ -9,6 +9,12 @@ CART_INSTALL_DIR=/usr/libexec/li/cartridges
 CART_INFO_DIR=$CART_INSTALL_DIR/embedded/$CART_DIRNAME/info
 CART_DIR=${CART_DIR:-$CART_INSTALL_DIR}
 
+function log_message() {
+   msg=${1-""}
+   [ -z "$msg" ]  &&  return 0
+   logger -i -s "user-cron-jobs" -p user.info "`date`: $msg"
+}
+
 
 #
 # main():
@@ -28,6 +34,10 @@ for f in ~/.env/*; do
     . $f
 done
 
+source "$CART_INFO_DIR/configuration/limits"
+
+log_message ":START: $freq cron run for libra user '$OPENSHIFT_APP_UUID'"
+
 # First up check if the cron jobs are enabled.
 CART_INSTANCE_DIR="$OPENSHIFT_HOMEDIR/$CART_DIRNAME"
 if [ ! -f $CART_INSTANCE_DIR/run/jobs.enabled ]; then
@@ -39,10 +49,18 @@ fi
 SCRIPTS_DIR="$OPENSHIFT_REPO_DIR/.openshift/cron/$freq"
 if [ -d "$SCRIPTS_DIR" ]; then
    # Run all scripts in the scripts directory serially.
-   for f in `ls "$SCRIPTS_DIR"`; do
-      [ ! -x "$SCRIPTS_DIR/$f" ]  &&  chmod +x "$SCRIPTS_DIR/$f"
-      "$SCRIPTS_DIR/$f" >> $CART_INSTANCE_DIR/log/cron.$freq.log 2>&1
-   done
+   executor=bash
+   [ -n "$MAX_RUN_TIME" ]  &&  executor="timeout $MAX_RUN_TIME bash"
+
+   script_list=$(ls "$SCRIPTS_DIR")
+   $executor <<RUN_USER_SCRIPTS_EOF
+      ls "$SCRIPTS_DIR" | while read f; do
+         [ ! -x "$SCRIPTS_DIR/\$f" ]  &&  chmod +x "$SCRIPTS_DIR/\$f"
+         "$SCRIPTS_DIR/\$f" >> $CART_INSTANCE_DIR/log/cron.$freq.log 2>&1
+      done
+RUN_USER_SCRIPTS_EOF
+
 fi
 
+log_message ":END: $freq cron run for libra user '$OPENSHIFT_APP_UUID'"
 exit 0
