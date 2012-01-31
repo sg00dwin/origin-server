8bcf9d9e184ff92e646d9472cacac048b8c5e4cb
Dan McPherson <dmcphers@redhat.com>
2012-01-31 14:14:36 -0500

    better logging

--- broker/lib/express/broker/application_container_proxy.rb
+++ broker/lib/express/broker/application_container_proxy.rb
@@ -305,6 +305,7 @@ module Express
             end
           end
           destination_container = ApplicationContainerProxy.find_available_impl(app.node_profile, destination_district_uuid)
+          log_debug "DEBUG: Destination container: #{destination_container.id}"
           destination_district_uuid = destination_container.get_district_uuid if allow_change_district
         else
           if destination_district_uuid
