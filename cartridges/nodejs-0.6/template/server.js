814b8b3b7eb6542cf90f54719dc8e597ea942650
Ram Ranganathan <ramr@redhat.com>
2012-02-10 18:01:02 -0800

    Use links in sample server.js rather than a redirect.

--- cartridges/nodejs-0.6/template/server.js
+++ cartridges/nodejs-0.6/template/server.js
@@ -23,7 +23,8 @@ app.get('/health', function(req, res){
 
 // Handler for GET /asciimo
 app.get('/asciimo', function(req, res){
-    res.redirect("https://a248.e.akamai.net/assets.github.com/img/d84f00f173afcf3bc81b4fad855e39838b23d8ff/687474703a2f2f696d6775722e636f6d2f6b6d626a422e706e67");
+    var link="https://a248.e.akamai.net/assets.github.com/img/d84f00f173afcf3bc81b4fad855e39838b23d8ff/687474703a2f2f696d6775722e636f6d2f6b6d626a422e706e67";
+    res.send("<html><body><img src='" + link + "'></body></html>");
 });
 
 // Handler for GET /
