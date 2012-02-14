10855361453ec73124d27a6fadb02f4a2293c14e
Lili Nader <lnader@redhat.com>
2012-02-14 15:26:00 -0800

    bug 790370

--- cloud-sdk/controller/lib/cloud-sdk-controller/app/controllers/domains_controller.rb
+++ cloud-sdk/controller/lib/cloud-sdk-controller/app/controllers/domains_controller.rb
@@ -2,16 +2,15 @@ class DomainsController < BaseController
   respond_to :xml, :json
   before_filter :authenticate, :validate_params
   before_filter :lookup_user, :except => [:create]
-  
-  NAMESPACE_MAX_LENGTH = 16
 
+  NAMESPACE_MAX_LENGTH = 16
   # GET /domains
   def index
     domain = RestDomain.new(@cloud_user.namespace)
     @reply = RestReply.new(:ok, "domains", [domain])
     respond_with @reply, :status => @reply.status
   end
-  
+
   # GET /domains/<id>
   def show
     id = params[:id]
@@ -19,24 +18,24 @@ class DomainsController < BaseController
       @reply = RestReply.new(:not_found)
       @reply.messages.push(message = Message.new(:error, "Domain #{id} not found."))
       respond_with @reply, :status => @reply.status
-      return
+    return
     end
     domain = RestDomain.new(@cloud_user.namespace)
     @reply = RestReply.new(:ok, "domain", domain)
     respond_with @reply, :status => @reply.status
   end
-  
+
   # POST /domains
   def create
     namespace = params[:namespace]
     Rails.logger.debug "Creating domain with namespace #{namespace}"
     cloud_user = CloudUser.find(@login)
-    
+
     if cloud_user
       @reply = RestReply.new(:conflict)
       @reply.messages.push(Message.new(:error, "User already has a domain associated. Update the domain to modify."))
       respond_with @reply, :status => @reply.status
-      return
+    return
     end
     Rails.logger.debug "Validating user"
     cloud_user = CloudUser.new(@login, nil, namespace)
@@ -46,9 +45,9 @@ class DomainsController < BaseController
         @reply.messages.push(Message.new(:error, message))
       end
       respond_with @reply, :status => @reply.status
-      return
+    return
     end
-    
+
     begin
       result_io = cloud_user.save
     rescue Cloud::Sdk::UserException => e
@@ -62,86 +61,90 @@ class DomainsController < BaseController
       @reply.process_result_io(e.resultIO)
       @reply.messages.push(e.message)
       respond_with @reply, :status => @reply.status
-      return
+    return
     end
-    
+
     domain = RestDomain.new(cloud_user.namespace)
     @reply = RestReply.new(:created, "domain", domain)
     @reply.process_result_io(result_io)
     respond_with @reply, :status => @reply.status
   end
-  
-  
+
   # PUT /domains/<id>
   def update
     id = params[:id]
     new_namespace = params[:namespace]
-    
+
     if @cloud_user.namespace != id
       @reply = RestReply.new(:not_found)
       @reply.messages.push(Message.new(:error, "Domain #{id} not found."))
       respond_with @reply, :status => @reply.status
-      return
+    return
     end
-    
+
     result_io = ResultIO.new
     result_io.append @cloud_user.update_namespace(new_namespace) unless params[:namespace].nil?
-    
+
     domain = RestDomain.new(@cloud_user.namespace)
     @reply = RestReply.new(:ok, "domain", domain)
     @reply.process_result_io(result_io)
     respond_with @reply, :status => @reply.status
   end
-  
-  
+
   # DELETE /domains/<id>
   def destroy
     id = params[:id]
-    force = params[:force]
+    force_str = params[:force]
+    if not force_str.nil? and force_str.upcase == "TRUE"
+      force = true
+    else
+      force = false
+    end
 
     if(@cloud_user.namespace != id)
       @reply = RestReply.new(:not_found)
       @reply.messages.push(Message.new(:error, "Domain #{id} not found."))
       #respond_with @reply, :status => @reply.status
       respond_with(@reply) do |format|
-         format.xml { render :xml => @reply, :status => @reply.status }
-         format.json { render :json => @reply, :status => @reply.status }
+        format.xml { render :xml => @reply, :status => @reply.status }
+        format.json { render :json => @reply, :status => @reply.status }
       end
-      return
+    return
     end
-    
+
     result_io = ResultIO.new
-    if (!@cloud_user.applications.empty? and !force)
+    if force
+      Rails.logger.debug "Force deleting domain #{id}"
+      @cloud_user.applications.each do |app|
+        result_io.append app.cleanup_and_delete()
+      end
+    elsif not @cloud_user.applications.empty?
       @reply = RestReply.new(:bad_request)
       @reply.messages.push(Message.new(:error, "Domain contains applications. Delete applications first or set force to true."))
       #respond_with @reply, :status => @reply.status
       respond_with(@reply) do |format|
-         format.xml { render :xml => @reply, :status => @reply.status }
-         format.json { render :json => @reply, :status => @reply.status }
+        format.xml { render :xml => @reply, :status => @reply.status }
+        format.json { render :json => @reply, :status => @reply.status }
       end
-      return
-    elsif force
-      @cloud_user.applications.each do |app|
-        result_io.append app.cleanup_and_delete()
-      end
+      return  
     end
-    
+
     @reply = RestReply.new(:no_content)
     result_io.append @cloud_user.delete
     @reply.process_result_io(result_io)
     @reply.messages.push(Message.new(:info, "Damain deleted."))
     #respond_with @reply, :status => @reply.status
     respond_with(@reply) do |format|
-       format.xml { render :xml => @reply, :status => @reply.status }
-       format.json { render :json => @reply, :status => @reply.status }
+      format.xml { render :xml => @reply, :status => @reply.status }
+      format.json { render :json => @reply, :status => @reply.status }
     end
   end
-  
+
   protected
-  
+
   def validate_params
     errors = []
-    
+
     unless params[:namespace].nil? and params[:id].nil?
       val = params[:namespace] || params[:id]
       if !(val =~ /\A[A-Za-z0-9]+\z/)
@@ -154,24 +157,24 @@ class DomainsController < BaseController
         error.push({:message => "Namespace (#{val}) is not allowed.  Please choose another.", :exit_code => 106})
       end
     end
-    
+
     unless errors.empty?
       @reply = RestReply.new(:bad_request)
       errors.each do |msg|
         @reply.messages.push(Message.new(:error, msg))
       end
       respond_with @reply, :status => @reply.status
-      return
+    return
     end
   end
-  
+
   def lookup_user
     @cloud_user = CloudUser.find(@login)
     if @cloud_user.nil?
       @reply = RestReply.new(:not_found)
       @reply.messages.push(Message.new(:error, "User #{@login} not found"))
       respond_with @reply, :status => @reply.status
-      return
+    return
     end
   end
 end
