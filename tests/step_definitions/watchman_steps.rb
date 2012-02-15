3df18c04cb77cf4c66341ba4ad0f179a385a5c9c
Jhon Honce <jhonce@redhat.com>
2012-02-14 17:03:13 -0700

    BZ790622

--- tests/step_definitions/watchman_steps.rb
+++ tests/step_definitions/watchman_steps.rb
@@ -1,8 +1,39 @@
-load '/usr/bin/rhc-watchman'
+load 'li-test/node/scripts/bin/rhc-watchman'
 
-Given /^a Watchman object using "([^"]*)" and "([^"]*)/ do |log, epoch|
-  class Watchman
+
+Given /^a Watchman object using "([^"]*)" and "([^"]*)"$/ do |log, epoch|
+  class Watchman1 < Watchman
     attr_accessor :restarted
+    def restart(uuid, env)
+      @restarted = @restarted.nil? ? 1 : @restarted += 1
+    end
+
+    def now() DateTime.new(2012, 02, 14, 18, 55, 00, 0, "+05:00") end
+  end
+
+  home = File.expand_path("../misc/watchman", File.expand_path(File.dirname(__FILE__)))
+  messages = "#{home}/#{log}"
+
+  raise "Watchman tests missing #{messages} file" if not File.exist?(messages)
+
+  @watchman = Watchman1.new(messages, 2, false, DateTime.strptime(epoch, '%b %d %T'), home)
+  @watchman.run
+end
+
+Then /^I should see "([^"]*)" restarts$/ do |restarts|
+  count = @watchman.restarted.nil? ? 0: @watchman.restarted
+  count.should be restarts.to_i
+end
+
+Given /^a JBoss application the Watchman Service using "([^"]*)" and "([^"]*)" at "([^"]*)"$/ do |log, epoch, timestamp|
+  class Watchman2 < Watchman
+    attr_accessor :restarted, :now
+
+    def initialize(timestamp, message_file, period, daemon, epoch, libra_var_lib)
+     super(message_file, period, daemon, epoch, libra_var_lib)
+     @now = timestamp
+    end
+
     def restart(uuid, env)
       @restarted = @restarted.nil? ? 1 : @restarted += 1
     end
@@ -13,10 +44,6 @@ Given /^a Watchman object using "([^"]*)" and "([^"]*)/ do |log, epoch|
 
   raise "Watchman tests missing #{messages} file" if not File.exist?(messages)
 
-  @watchman = Watchman.new(messages, 2, false, DateTime.strptime(epoch, '%b %d %T'), home)
+  @watchman = Watchman2.new(DateTime.strptime(timestamp, "%b %d %T"), messages, 2, false, DateTime.strptime(epoch, '%b %d %T'), home)
   @watchman.run
-end
-
-Then /^I should see "([^"]*)" restarts$/ do |restarts|
-  @watchman.restarted == restarts
 end
--- tests/watchman.feature
+++ tests/watchman.feature
@@ -1,12 +1,23 @@
-@internals
+@internals @watchman
 Feature: Watchman Service
-  Scenario Outline: Watchman not run as daemon
+  Scenario Outline: Watchman monitoring misc applications
     Given a Watchman object using "<log_file>" and "<epoch>"
     Then I should see "<restarts>" restarts
 
   Examples:
     | log_file     | epoch           | restarts |
-    | messages.log | Feb  9 18:20:44 | 0 |
-    | messages.log | Feb  7 18:20:44 | 1 |
-    | messages.log | Feb  5 18:20:44 | 2 |
-    | empty.log    | Jan  1 01:01:01 | 0 | 
+    | messages.log | Feb  9 18:20:44 | 1 |
+    | messages.log | Feb  7 18:20:44 | 2 |
+    | messages.log | Feb  5 18:20:44 | 3 |
+    | empty.log    | Jan  1 01:01:01 | 1 | 
+
+  Scenario Outline:  Watchman monitoring JBoss application
+    Given a JBoss application the Watchman Service using "<log_file>" and "<epoch>" at "<timestamp>"
+    Then I should see "<restarts>" restarts
+
+  # timestamp only used to get date, time pulled from server.log 17:55:00
+  Examples:
+    | log_file     | epoch           | timestamp      | restarts |
+    | messages.log | Feb  9 18:00:00 | Feb 9 00:00:00 | 0 |
+    | messages.log | Feb  9 16:00:00 | Feb 9 00:00:00 | 1 |
+    | empty.log    | Feb  9 16:00:00 | Feb 9 00:00:00 | 1 | 
