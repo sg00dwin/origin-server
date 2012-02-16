7da94405870de4c687e0ecc54d319fbe273dad13
Mike McGrath <mmcgrath@redhat.com>
2012-02-16 15:17:12 -0600

    fixing ssh permissions

--- cartridges/haproxy-1.4/info/bin/resync_repo.sh
+++ cartridges/haproxy-1.4/info/bin/resync_repo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ~/git/${OPENSHIFT_APP_NAME}
+cd ~/git/${OPENSHIFT_APP_NAME}.git
 rm -rf *
+export GIT_SSH=/usr/libexec/li/cartridges/haproxy-1.4/info/bin/ssh
 git clone --bare $1
 git add remote haproxy $1
