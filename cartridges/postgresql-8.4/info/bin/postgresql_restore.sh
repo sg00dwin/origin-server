9e6f348d6b28ef7966fc430066f8cdb9917932a5
Ram Ranganathan <ramr@redhat.com>
2012-02-24 11:23:08 -0800

    Fix for bugz 797140 - restore PostgreSQL data using snapshot tarball

--- cartridges/postgresql-8.4/info/bin/postgresql_restore.sh
+++ cartridges/postgresql-8.4/info/bin/postgresql_restore.sh
@@ -7,7 +7,7 @@ do
 done
 
 
-if [ -f $OPENSHIFT_DATA_DIR/postgresql_dump_snapshot.gz ]
+if [ -f "$OPENSHIFT_DATA_DIR/postgresql_dump_snapshot.gz" ]
 then
 	CART_DIR=${CART_DIR:=/usr/libexec/li/cartridges}
 	CART_INFO_DIR=$CART_DIR/embedded/postgresql-8.4/info
@@ -15,8 +15,21 @@ then
 
     start_postgresql_as_user
 
+    old_dbname=$OPENSHIFT_APP_NAME
+    old_dbuser=$OPENSHIFT_APP_UUID
+    [ -f "$OPENSHIFT_DATA_DIR/postgresql_dbname" ] &&  old_dbname=$(cat "$OPENSHIFT_DATA_DIR/postgresql_dbname")
+    [ -f "$OPENSHIFT_DATA_DIR/postgresql_dbuser" ] &&  old_dbuser=$(cat "$OPENSHIFT_DATA_DIR/postgresql_dbuser")
+
     # Restore the PostgreSQL databases
-    /bin/zcat $OPENSHIFT_DATA_DIR/postgresql_dump_snapshot.gz | /usr/bin/psql -d postgres
+    rexp="^\s*\(DROP\|CREATE\)\s*DATABASE\s*$old_dbname"
+    pargz="--username=$OPENSHIFT_DB_USERNAME --host=$OPENSHIFT_DB_HOST"
+
+    /bin/zcat $OPENSHIFT_DATA_DIR/postgresql_dump_snapshot.gz |        \
+        sed "s#$rexp#\\1 DATABASE $OPENSHIFT_APP_NAME#g;               \
+             s#\\connect $old_dbname#\\connect $OPENSHIFT_APP_NAME#g;  \
+             s#$old_dbuser#$OPENSHIFT_APP_UUID#g" |                    \
+        PGPASSWORD="$OPENSHIFT_DB_PASSWORD" /usr/bin/psql $pargz -d postgres
+
     if [ ! ${PIPESTATUS[1]} -eq 0 ]
     then
         echo 1>&2
