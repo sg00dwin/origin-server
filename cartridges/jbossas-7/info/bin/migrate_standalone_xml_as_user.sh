446f9a40b8bfbcb83f16130f3f252eb6aa718a3a
Dan McPherson <dmcphers@redhat.com>
2012-02-26 19:03:58 -0500

    add existence check for standalone.xml before migrating

--- cartridges/jbossas-7/info/bin/migrate_standalone_xml_as_user.sh
+++ cartridges/jbossas-7/info/bin/migrate_standalone_xml_as_user.sh
@@ -7,13 +7,18 @@ GIT_DIR=$2
 rm -rf $WORKING_DIR 
 git clone $GIT_DIR $WORKING_DIR
 pushd $WORKING_DIR > /dev/null
-    xsltproc -o .openshift/config/standalone.xml /usr/libexec/li/cartridges/jbossas-7/info/configuration/as-7.0.2-as-7.1.0.xsl .openshift/config/standalone.xml
-    git add .openshift/config/standalone.xml
-    if git commit -m 'Updating .openshift/config/standalone.xml to be 7.1.0 compatible'
+    if [ -f .openshift/config/standalone.xml ]
     then
-        git push origin master
+	    xsltproc -o .openshift/config/standalone.xml /usr/libexec/li/cartridges/jbossas-7/info/configuration/as-7.0.2-as-7.1.0.xsl .openshift/config/standalone.xml
+	    git add .openshift/config/standalone.xml
+	    if git commit -m 'Updating .openshift/config/standalone.xml to be 7.1.0 compatible'
+	    then
+	        git push origin master
+	    else
+	        echo "WARNING: No changes made to standalone.xml!"
+	    fi
     else
-        echo "WARNING: No changes made to standalone.xml!"
+        echo "WARNING: standalone.xml not found!"
     fi
 popd > /dev/null;
 rm -rf $WORKING_DIR
