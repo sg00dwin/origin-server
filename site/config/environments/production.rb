b4913bd8e38bcbf9eb6218fc87c018393653803b
Clayton Coleman <ccoleman@redhat.com>
2012-02-29 17:32:56 -0500

    Add mthompso@redhat.com to promo mailing list

--- site/config/environments/production.rb
+++ site/config/environments/production.rb
@@ -69,7 +69,7 @@ RedHatCloud::Application.configure do
 
   # Promo code Email notification setup
   config.email_from = 'OpenShift <noreply@openshift.redhat.com>'
-  config.marketing_mailing_list = 'Marketing Mailing List <jgurrero@redhat.com>'
+  config.marketing_mailing_list = ['Marketing Mailing List <jgurrero@redhat.com>', 'mthompso@redhat.com']
   
   # Express API base url
   config.express_api_url = 'https://localhost'
