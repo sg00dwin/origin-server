041f1fe7378182330da7a56daf00f0fa9e53e1c4
William DeCoste <bdecoste@gmail.com>
2012-02-28 18:38:49 -0800

    add env via sed

--- cartridges/jbossas-7/info/bin/migrate_standalone_xml_as_user.sh
+++ cartridges/jbossas-7/info/bin/migrate_standalone_xml_as_user.sh
@@ -17,6 +17,7 @@ pushd $WORKING_DIR > /dev/null
     if [ -f .openshift/config/standalone.xml ]
     then
 	    xsltproc -o .openshift/config/standalone.xml /usr/libexec/li/cartridges/jbossas-7/info/configuration/as-7.0.2-as-7.1.0.xsl .openshift/config/standalone.xml
+	    sed -i "s/{OPENSHIFT/{env.OPENSHIFT/g" standalone.xml
 	    git add .openshift/config/standalone.xml
 	    if git commit -m 'Updating .openshift/config/standalone.xml to be 7.1.0 compatible'
 	    then
