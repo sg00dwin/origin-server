959548c6582819ef3340434c0b53c21a8b2db01c
Dan McPherson <dmcphers@redhat.com>
2012-03-04 16:34:28 -0500

    handle stale package with --clean_metadata

--- build/lib/openshift/tito.rb
+++ build/lib/openshift/tito.rb
@@ -1,18 +1,23 @@
 require 'fileutils'
 
+CLOUD_SDK_REGEX = /^rubygem-cloud-sdk-([\w]+)-\d+/
+CARTRIDGE_REGEX = /^rhc-(cartridge-[\w-]+\d+[\.\d+]*)-\d+\.\d+\.\d+-/
+RHC_REGEX = /^rhc-([\w-]+)-\d+/
+DEVENV_REGEX = /^rhc-devenv-\d+/
+
 module OpenShift
   module Tito
     def get_build_dirs
       # Figure out what needs to be built
       packages = `tito report --untagged-commits`
       build_dirs = packages.split("\n").collect do |package|
-        if package =~ /^rhc-devenv-\d+/
+        if package =~ DEVENV_REGEX
           "misc/devenv"
-        elsif package =~ /^rubygem-cloud-sdk-([\w]+)-\d+/
+        elsif package =~ CLOUD_SDK_REGEX
           "cloud-sdk/#{$1}"
-        elsif package =~ /^rhc-(cartridge-[\w-]+\d+[\.\d+]*)-\d+\.\d+\.\d+-/
+        elsif package =~ CARTRIDGE_REGEX
           "cartridges/" + $1['cartridge-'.length..-1]
-        elsif package =~ /^rhc-([\w-]+)-\d+/
+        elsif package =~ RHC_REGEX
           $1
         end
       end.compact
@@ -29,6 +34,35 @@ module OpenShift
       build_dirs
     end
     
+    def get_stale_dirs
+      stale_dirs = []
+      Dir.glob("**/*.spec") do |file|
+        unless file.start_with?('build/')
+          build_dir = File.dirname(file)
+          name = File.basename(build_dir)
+          package_name = nil
+          if file.start_with?('cartridges')
+            package_name = "rhc-cartridge-#{name}"
+          elsif file.start_with?('cloud-sdk')
+            package_name = "rubygem-cloud-sdk-#{name}"
+          else
+            package_name = "rhc-#{name}"
+          end
+          installed_version = `yum list installed #{package_name} | tail -n1 | gawk '{print $2}'`
+          if installed_version =~ /(\d+\.\d+\.\d+)-/
+            installed_version = $1
+            spec_version = /Version: (.*)/.match(File.read(file))[1].strip
+            installed_version_parts = installed_version.split(".")
+            spec_version_parts = spec_version.split(".")
+            if (installed_version_parts <=> spec_version_parts) < 0
+              stale_dirs << [package_name, build_dir]
+            end
+          end
+        end
+      end
+      stale_dirs
+    end
+
     def get_sync_dirs
       # Figure out what needs to be synced
       tito_report = `tito report --untagged-commits`
@@ -40,17 +74,17 @@ module OpenShift
       sync_dirs = []
       packages = tito_report.split("\n")
       packages.each do |package|
-        if package =~ /^rhc-devenv-\d+/
+        if package =~ DEVENV_REGEX
           puts "The devenv package has an update but isn't being installed."
-        elsif package =~ /^rubygem-cloud-sdk-([\w]+)-\d+/
+        elsif package =~ CLOUD_SDK_REGEX
           dir_name = $1
           current_package = "rubygem-cloud-sdk-#{dir_name}"
           current_sync_dir = "cloud-sdk/#{dir_name}"
-        elsif package =~ /^rhc-(cartridge-[\w-]+\d+[\.\d+]*)-\d+\.\d+\.\d+-/
+        elsif package =~ CARTRIDGE_REGEX
           dir_name = $1['cartridge-'.length..-1]
           current_package = "rhc-cartridge-#{dir_name}"
           current_sync_dir = "cartridges/#{dir_name}"
-        elsif package =~ /^rhc-([\w-]+)-\d+/
+        elsif package =~ RHC_REGEX
           dir_name = $1
           current_package = "rhc-#{dir_name}"
           current_sync_dir = dir_name
