b6709de3ef46f270347c536a095673e21a1ba912
Lili Nader <lnader@redhat.com>
2012-03-05 12:32:39 -0800

    added code to check response error codes

--- tests/step_definitions/api_steps.rb
+++ tests/step_definitions/api_steps.rb
@@ -4,119 +4,191 @@ require 'nokogiri'
 require '/var/www/libra/broker/lib/express/broker/dns_service'
 require 'logger'
 
+@random = nil
 Before do
   @base_url = "https://localhost/broker/rest"
 end
 
 After do |scenario|
-  dns_service = Express::Broker::DnsService.new({:end_point => "https://api2.dynect.net", :customer_name => "demo-redhat", 
-  :user_name => "dev-rhcloud-user", :password => "vo8zaijoN7Aecoo", :domain_suffix => "dev.rhcloud.com", :zone => "rhcloud.com", :log_file => "/dev/null"})
+  dns_service = Express::Broker::DnsService.new({:end_point => "https://api2.dynect.net", :customer_name => "demo-redhat",
+    :user_name => "dev-rhcloud-user", :password => "vo8zaijoN7Aecoo", :domain_suffix => "dev.rhcloud.com", :zone => "rhcloud.com", :log_file => "/dev/null"})
 
-  domains = ["cucumber", "app-cucumber"]
-  i=0
-  while i<3 
-    domains.push("cucumber"+i.to_s)
-    i += 1
-  end
+  domains = ["cucumber#{@random}", "cucumber1#{@random}", "app-cucumber#{@random}"]
   domains.each do |domain|
     yes = dns_service.namespace_available?(domain)
     if !yes
-      dns_service.deregister_namespace(domain)
+    #puts "deregistering #{domain}"
+    dns_service.deregister_namespace(domain)
     end
   end
   dns_service.publish
   dns_service.close
+  @random = nil
 end
-    
-Given /^I am a valid user$/ do 
-  
+
+Given /^I am a valid user$/ do
+
   @username = @account['accountname']
   @password = "xyz123"
-  #TODO authenticate user
+#TODO authenticate user
 
 end
 
 Given /^I send and accept "([^\"]*)"$/ do |type|
-  @header = {:accept => type, :content_type => type}
+  @headers = {:accept => type, :content_type => type}
 end
 
 Given /^I accept "([^\"]*)"$/ do |type|
   @accept_type = type
-  @header = {:accept => type.to_s.downcase}
+  @headers = {:accept => type.to_s.downcase}
 end
 
 When /^I send a GET request to "([^\"]*)"$/ do |path|
+  path = sub_random(path)
   url = @base_url + path.to_s
-  @request = RestClient::Request.new(:method => :get, :url => url, 
-    :user => @username, :password => @password, :headers => @headers)
+  @request = RestClient::Request.new(:method => :get, :url => url,
+  :user => @username, :password => @password, :headers => @headers)
   begin
     @response = @request.execute()
   rescue => e
-    @response = e.response
+  @response = e.response
   end
 end
 
 When /^I send a POST request to "([^\"]*)" with the following:"([^\"]*)"$/ do |path, body|
+  path = sub_random(path)
+  body = sub_random(body)
+  #puts "path #{path}"
+  #puts "body #{body}"
   payload = {}
   params = body.split("&")
   params.each do |param|
     key, value = param.split("=", 2)
-    payload[key] = value  
+    payload[key] = value
   end
   url = @base_url + path.to_s
-  @request = RestClient::Request.new(:method => :post, :url => url, 
+  @request = RestClient::Request.new(:method => :post, :url => url,
   :user => @username, :password => @password, :headers => @headers,
   :payload => payload)
   begin
     @response = @request.execute()
   rescue => e
-    @response = e.response
+  @response = e.response
   end
 end
 
 When /^I send a PUT request to "([^\"]*)" with the following:"([^\"]*)"$/ do |path, body|
+  path = sub_random(path)
+  body = sub_random(body)
+  #puts "path #{path}"
+  #puts "body #{body}"
   payload = {}
   params = body.split("&")
   params.each do |param|
     key, value = param.split("=", 2)
-    payload[key] = value  
+    payload[key] = value
   end
   url = @base_url + path.to_s
-  @request = RestClient::Request.new(:method => :put, :url => url, 
+  @request = RestClient::Request.new(:method => :put, :url => url,
   :user => @username, :password => @password, :headers => @headers,
   :payload => payload)
   begin
     @response = @request.execute()
   rescue => e
-    @response = e.response
+  @response = e.response
   end
 end
 
 When /^I send a DELETE request to "([^\"]*)"$/ do |path|
-   url = @base_url + path.to_s
-  @request = RestClient::Request.new(:method => :delete, :url => url, 
-    :user => @username, :password => @password, :headers => @headers)
+  path = sub_random(path)
+  #puts "path #{path}"
+
+  url = @base_url + path.to_s
+  @request = RestClient::Request.new(:method => :delete, :url => url,
+  :user => @username, :password => @password, :headers => @headers)
   begin
     @response = @request.execute()
   rescue => e
-    @response = e.response
+  @response = e.response
   end
 end
 
 Then /^the response should be "([^\"]*)"$/ do |status|
-  puts "#{@response.body}" if @response.code != status.to_i
+  puts "#{@response.body}"  if @response.code != status.to_i
   @response.code.should == status.to_i
 end
 
-Then /^the "([^\"]*)" response should be a "([^\"]*)" array with (\d+) "([^\"]*)" elements$/ do |type, tag, number_of_children, child_tag|
-  if type.upcase == "XML"
-    page = Nokogiri::XML(@response.body)
-    page.xpath("//#{tag}/#{child_tag}").length.should == number_of_children.to_i
-  elsif type.upcase == "JSON"
-    page = JSON.parse(@response.body)
-    page.map { |d| d[name] }.length.should == number_of_children.to_i
+Then /^the response should be a "([^\"]*)" with attributes "([^\"]*)"$/ do |tag, attributes_str|
+  attributes_str = sub_random(attributes_str)
+  attributes_array = attributes_str.split("&")
+  if @accept_type.upcase == "XML"
+    #puts @response.body
+    result = Nokogiri::XML(@response.body)
+    attributes_array.each do |attributes|
+      key, value = attributes.split("=", 2)
+      #puts "#{result.xpath("//#{tag}/#{key}").text} #{value}"
+      result.xpath("//#{tag}/#{key}").text.should == value
+    end
+  elsif @accept_type.upcase == "JSON"
+    result = JSON.parse(@response.body)
+    obj = result["data"]
+    attributes_array.each do |attributes|
+      key, value = attributes.split("=", 2)
+      obj[key].should == value
+    end
   else
-    false
+  false
+  end
+end
+
+Then /^the response should be a list of "([^\"]*)" with attributes "([^\"]*)"$/ do |tag, attributes_str|
+  attributes_str = sub_random(attributes_str)
+  attributes_array = attributes_str.split("&")
+  if @accept_type.upcase == "XML"
+    #puts @response.body
+    result = Nokogiri::XML(@response.body)
+    attributes_array.each do |attributes|
+      key, value = attributes.split("=", 2)
+      #puts "#{result.xpath("//#{tag}/#{key}").text} #{value}"
+      result.xpath("//#{tag}/#{key}").text.should == value
+    end
+  elsif @accept_type.upcase == "JSON"
+    result = JSON.parse(@response.body)
+    obj = result["data"]
+    attributes_array.each do |attributes|
+      key, value = attributes.split("=", 2)
+      obj[key].should == value
+    end
+  else
+  false
+  end
+end
+
+Then /^the error message should have "([^\"]*)"$/ do |attributes_str|
+  attributes_str = sub_random(attributes_str)
+  attributes_array = attributes_str.split("&")
+  if @accept_type.upcase == "XML"
+    #puts @response.body
+    result = Nokogiri::XML(@response.body)
+    messages = result.xpath("//message")
+    attributes_array.each do |attributes|
+      key, value = attributes.split("=", 2)
+      key = key.sub("_", "-")
+      messages.each do |message|
+        result.xpath("/#{key}").text.should == value
+      end
+    end
+  elsif @accept_type.upcase == "JSON"
+    result = JSON.parse(@response.body)
+    messages = result["messages"]
+    attributes_array.each do |attributes|
+      key, value = attributes.split("=", 2)
+      messages.each do |message|
+        message[key].to_s.should == value
+      end
+    end
+  else
+  false
   end
 end
 
@@ -133,4 +205,12 @@ Then /^the response descriptor should have "([^\"]*)" as dependencies$/ do |deps
   deps.split(",").each do |dep|
     desc["Requires"].should include(dep)
   end
+end
+
+def sub_random(value)
+  if value and value.include? "<random>"
+    @random = rand(1000) unless @random
+    value = value.sub("<random>", @random.to_s)
+  end
+  return value
 end
