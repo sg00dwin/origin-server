be3aae66dda20f5419ca85b9724874832c19139b
Abhishek Gupta <abhgupta@redhat.com>
2012-03-05 16:43:23 -0800

    replacing libra with stickshift for 10gen mms cartridge

--- cartridges/10gen-mms-agent-0.1/info/bin/10gen_mms_agent_ctl.sh
+++ cartridges/10gen-mms-agent-0.1/info/bin/10gen_mms_agent_ctl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-CART_DIR=/usr/libexec/li/cartridges
-source ${CART_DIR}/abstract/info/lib/util
+source /etc/stickshift/stickshift-node.conf
+source ${CARTRIDGE_BASE_PATH}/abstract/info/lib/util
 
 # Import Environment Variables
 for f in ~/.env/*
