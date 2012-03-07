e94baa71d6e4116bff5ca2b21867a617306ef031
Ravi Sankar <rpenta@redhat.com>
2012-03-06 18:02:02 -0800

    add mongo_auth_service to broker initializer

--- stickshift/broker/config/initializers/broker.rb
+++ stickshift/broker/config/initializers/broker.rb
@@ -1,5 +1,6 @@
-require 'lib/stickshift/broker/application_container_proxy.rb'
+require 'lib/stickshift/broker/application_container_proxy'
 require 'lib/stickshift/broker/bind_dns_service'
+require 'lib/stickshift/broker/mongo_auth_service'
 
 StickShift::AuthService.provider=StickShift::AuthService
 StickShift::DataStore.provider=StickShift::MongoDataStore
