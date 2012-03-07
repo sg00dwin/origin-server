76adb1c0b9f9f004e71ffa32a31811e951f5ea83
Ravi Sankar <rpenta@redhat.com>
2012-03-06 17:14:39 -0800

    StickShift auth service based on mongodb

---
+++ stickshift/broker/lib/stickshift/broker/mongo_auth_service.rb
@@ -0,0 +1,26 @@
+require 'rubygems'
+require 'digest/md5'
+
+module StickShift
+  class MongoAuthService < StickShift::AuthService
+
+    def authenticate(request, login, password)
+      user_matched = nil
+      auth_config = Rails.application.config.ss[:auth]
+      ds = StickShift::MongoDataStore.new(auth_config[:mongo])
+      encoded_password = Digest::MD5.hexdigest(Digest::MD5.hexdigest(password) + auth_config[:salt])
+      hash = ds.find_ond({"_id" => login})
+      user_matched = login if hash && !hash.empty? && (hash["password"] == encoded_password)
+      return {:username => user_matched, :auth_method => :login}
+    end
+
+    def login(request, params, cookies)
+      if params['broker_auth_key'] && params['broker_auth_iv']
+        return {:username => params['broker_auth_key'], :auth_method => :broker_auth}
+      else
+        data = JSON.parse(params['json_data'])
+        return authenticate(request, data['rhlogin'], params['password'])
+      end
+    end
+  end
+end
