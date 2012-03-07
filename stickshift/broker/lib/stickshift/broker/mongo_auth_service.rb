cf0d4c1f17e1e49194ac3463094c44f389a219e6
Ravi Sankar <rpenta@redhat.com>
2012-03-06 18:25:38 -0800

    handle exceptions in mongo auth service

--- stickshift/broker/lib/stickshift/broker/mongo_auth_service.rb
+++ stickshift/broker/lib/stickshift/broker/mongo_auth_service.rb
@@ -6,11 +6,15 @@ module StickShift
 
     def authenticate(request, login, password)
       user_matched = nil
-      auth_config = Rails.application.config.ss[:auth]
-      ds = StickShift::MongoDataStore.new(auth_config[:mongo])
-      encoded_password = Digest::MD5.hexdigest(Digest::MD5.hexdigest(password) + auth_config[:salt])
-      hash = ds.find_ond({"_id" => login})
-      user_matched = login if hash && !hash.empty? && (hash["password"] == encoded_password)
+      begin
+        auth_config = Rails.application.config.ss[:auth]
+        ds = StickShift::MongoDataStore.new(auth_config[:mongo])
+        encoded_password = Digest::MD5.hexdigest(Digest::MD5.hexdigest(password) + auth_config[:salt])
+        hash = ds.find_one({"_id" => login})
+        user_matched = login if hash && !hash.empty? && (hash["password"] == encoded_password)
+      rescue Exception => e
+        Rails.logger.error "MongoAuthService::authenticate exception: #{e.message}"
+      end
       return {:username => user_matched, :auth_method => :login}
     end
 
