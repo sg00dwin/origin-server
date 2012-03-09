5ecd7f58ff4bdbf770b5d03156958d8a6cc881d7
Mike McGrath <mmcgrath@redhat.com>
2012-03-08 20:33:30 -0600

    finalizing some haproxy ctld bits

--- cartridges/haproxy-1.4/info/bin/build.sh
+++ cartridges/haproxy-1.4/info/bin/build.sh
@@ -11,13 +11,5 @@ then
     echo ".openshift/markers/force_clean_build found but disabled for haproxy" 1>&2
 fi
 
-if [ -f "${OPENSHIFT_REPO_DIR}/servers.txt" ]
-then
-    echo "Updating haproxy config"
-    cp ${OPENSHIFT_APP_DIR}/conf/haproxy.cfg.template ${OPENSHIFT_APP_DIR}/conf/haproxy.cfg
-    cat ${OPENSHIFT_REPO_DIR}/servers.txt >> ${OPENSHIFT_APP_DIR}/conf/haproxy.cfg
-else
-    echo "Could not find servers.txt in your repo file!"
-fi
 # Run user build
 user_build.sh
