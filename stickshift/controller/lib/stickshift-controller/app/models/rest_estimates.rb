f3f0840dec774935b61e8ce5a0c4c6f3024cea65
Ravi Sankar <rpenta@redhat.com>
2012-03-09 15:52:31 -0800

    add REST estimates model

---
+++ stickshift/controller/lib/stickshift-controller/app/models/rest_estimates.rb
@@ -0,0 +1,15 @@
+class RestEstimates < Cloud::Sdk::Model
+  
+  def initialize
+    self.links = {
+      "GET_APPLICATION" => Link.new("Get application estimate", "GET", "/estimates/application", [
+        Param.new("descriptor", "string", "application requirements")
+      ]) 
+    }
+  end
+  
+  def to_xml(options={})
+    options[:tag_name] = "estimates"
+    super(options)
+  end
+end
