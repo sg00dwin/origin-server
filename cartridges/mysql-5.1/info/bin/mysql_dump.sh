af3b287218821a96b3f8e261498608132198c8d5
Ram Ranganathan <ramr@redhat.com>
2012-03-13 17:15:16 -0700

    Fix to get snapshot working.

--- cartridges/mysql-5.1/info/bin/mysql_dump.sh
+++ cartridges/mysql-5.1/info/bin/mysql_dump.sh
@@ -10,7 +10,7 @@ source /etc/stickshift/stickshift-node.conf
 CART_INFO_DIR=${CARTRIDGE_BASE_PATH}/embedded/mysql-5.1/info
 source ${CART_INFO_DIR}/lib/util
 
-start_mysql_as_user
+start_mysql_as_user 1>&2
 
 echo $OPENSHIFT_DB_HOST > $OPENSHIFT_DATA_DIR/mysql_db_host
 /usr/bin/mysqldump -h $OPENSHIFT_DB_HOST -P $OPENSHIFT_DB_PORT -u $OPENSHIFT_DB_USERNAME --password="$OPENSHIFT_DB_PASSWORD" --all-databases --add-drop-table | /bin/gzip -v > $OPENSHIFT_DATA_DIR/mysql_dump_snapshot.gz
