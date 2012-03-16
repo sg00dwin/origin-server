0bcf549a8cedb19f3b95baf33309c80657a7a5f8
Clayton Coleman <ccoleman@redhat.com>
2012-03-16 15:26:48 -0400

    Fix duplicate constant error by making status site autoload

--- site/config/initializers/status_app.rb
+++ site/config/initializers/status_app.rb
@@ -1,1 +1,3 @@
-require 'app/subsites/status/status_app.rb'
+STATUS_APP_ROOT = File.join(File.expand_path(Rails.configuration.root), 'app','subsites','status')
+STATUS_APP_HOSTS = YAML.load(File.open(File.join(STATUS_APP_ROOT,'config','hosts.yml')))
+require 'status_app'
