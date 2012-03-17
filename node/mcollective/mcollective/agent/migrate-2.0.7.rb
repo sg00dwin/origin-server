151a8b5688dbfe5ae8e9ae690b0eda5cf2fc9228
Rob Millner <rmillner@redhat.com>
2012-03-16 19:37:17 -0700

    Had missed the existing jenkins block

--- node/mcollective/mcollective/agent/migrate-2.0.7.rb
+++ node/mcollective/mcollective/agent/migrate-2.0.7.rb
@@ -58,18 +58,6 @@ module LibraMigration
       end
 
       case app_type
-      when "jenkins-1.4"
-        jenkins_configs=["#{app_dir}/data/config.xml"]
-        Dir.glob("#{app_dir}/data/jobs/*/config.xml") do |jobcfg|
-          jenkins_configs << "#{jobcfg}"
-        end
-        jenkins_configs.each do |jenkins_config|
-          ['builderSize', 'defaultBuilderSize'].each do |builder_opt|
-            { "std" => "small", "large" => "medium" }.each do |srcsize, dstsize|
-              Util.replace_in_file("#{jenkins_config}", "<#{builder_opt}>#{srcsize}<\\/#{builder_opt}>", "<#{builder_opt}>#{dstsize}</#{builder_opt}>")
-            end
-          end
-        end
       when "python-2.6", "ruby-1.8", "perl-5.10"
         FileUtils.mv("#{app_dir}/conf.d/libra.conf", "#{app_dir}/conf.d/stickshift.conf") unless File.exists?("#{app_dir}/conf.d/stickshift.conf")
         Util.replace_in_file("#{app_dir}/conf.d/stickshift.conf", "/var/lib/libra", "/var/lib/stickshift")
@@ -117,6 +105,20 @@ module LibraMigration
           Util.replace_in_file("#{app_dir}/data/config.xml", "<jenkinsUrl>.*</jenkinsUrl>", "")
           #Util.replace_in_file("#{app_dir}/data/hudson.tasks.Mailer.xml", "<hudsonUrl>.*</hudsonUrl>", "<hudsonUrl>#{jenkins_url}</hudsonUrl>")
         end
+
+        jenkins_configs=["#{app_dir}/data/config.xml"]
+        Dir.glob("#{app_dir}/data/jobs/*/config.xml") do |jobcfg|
+          jenkins_configs << "#{jobcfg}"
+        end
+
+        jenkins_configs.each do |jenkins_config|
+          ['builderSize', 'defaultBuilderSize'].each do |builder_opt|
+            { "std" => "small", "large" => "medium" }.each do |srcsize, dstsize|
+              Util.replace_in_file("#{jenkins_config}", "<#{builder_opt}>#{srcsize}<\\/#{builder_opt}>", "<#{builder_opt}>#{dstsize}</#{builder_opt}>")
+            end
+          end
+        end
+
       end
 
       #mongodb-2.0
