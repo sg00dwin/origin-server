9e946aeda2861b2c6c3ed108bd0d39d26e8ec0f7
Ram Ranganathan <ramr@redhat.com>
2012-03-22 19:34:08 -0700

    Cleanup old code for git mirroring.

--- cartridges/haproxy-1.4/info/bin/deploy.sh
+++ cartridges/haproxy-1.4/info/bin/deploy.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-function git_mirror_push() {
-    gears=${1:-"all-gears"}
-    app_git_dir=~/git/${OPENSHIFT_GEAR_NAME}.git/
-
-    #  Need to push twice so that mirror states are also synced.
-    GIT_DIR=$app_git_dir  git push $gears --mirror
-    GIT_DIR=$app_git_dir  git push $gears --mirror
-}
-
-
-function get_gear_git_mirrors() {
-    app_git_dir=~/git/${OPENSHIFT_GEAR_NAME}.git/
-    echo $(GIT_DIR=$app_git_dir  git remote -v  |     \
-              grep -E "^gear\-.*(push)"  |  awk '{print $1}')
-}
-
-
 # Import Environment Variables
 for f in ~/.env/*
 do
@@ -27,13 +10,5 @@ source /etc/stickshift/stickshift-node.conf
 source ${CARTRIDGE_BASE_PATH}/abstract/info/lib/util
 
 export GIT_SSH="${CARTRIDGE_BASE_PATH}/haproxy-1.4/info/bin/ssh"
-
-#  Optimized case - push to all mirrors.
-if ! git_mirror_push all-gears; then
-    #  Failure case - we have a bad remote/mirror - so do it one by one.
-    for gear_name in $(get_gear_git_mirrors); do
-        git_mirror_push "$gear_name"
-    done
-fi
 
 user_deploy.sh
