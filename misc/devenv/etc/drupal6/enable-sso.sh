9f8c4404a489ae3479fbb5f052e8fe949209c898
Anderson Silva <ansilva@redhat.com>
2012-03-26 19:31:56 -0400

    fix permission on development.rb

--- misc/devenv/etc/drupal6/enable-sso.sh
+++ misc/devenv/etc/drupal6/enable-sso.sh
@@ -4,11 +4,13 @@ echo "Enabling SSO for Broker..."
 pushd /var/www/stickshift/broker/config/environments/
 mv development.rb development.rb.orig
 cp streamline-aws.rb development.rb
+chown .libra_user development.rb
 service libra-broker restart
 popd
 echo "Enabling SSO for Site..."
 pushd /var/www/stickshift/site/config/environments/
 mv development.rb development.rb.orig
 cp streamline-aws.rb development.rb
+chown .libra_user development.rb
 service libra-site restart
 popd
