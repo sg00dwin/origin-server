16547405a0b78eb0a31360c3d45685e915a6d1f4
Ram Ranganathan <ramr@redhat.com>
2012-04-10 13:40:09 -0700

    Fix for bugz 809567 - snapshot and restore for scalable apps - use the dns name on the control/haproxy gear.

--- cartridges/mysql-5.1/info/bin/mysql_dump.sh
+++ cartridges/mysql-5.1/info/bin/mysql_dump.sh
@@ -12,8 +12,9 @@ source ${CART_INFO_DIR}/lib/util
 
 start_mysql_as_user 1>&2
 
+dbhost=${OPENSHIFT_DB_GEAR_DNS:-$OPENSHIFT_DB_HOST}
 echo $OPENSHIFT_DB_HOST > $OPENSHIFT_DATA_DIR/mysql_db_host
-/usr/bin/mysqldump -h $OPENSHIFT_DB_HOST -P $OPENSHIFT_DB_PORT -u $OPENSHIFT_DB_USERNAME --password="$OPENSHIFT_DB_PASSWORD" --all-databases --add-drop-table | /bin/gzip -v > $OPENSHIFT_DATA_DIR/mysql_dump_snapshot.gz
+/usr/bin/mysqldump -h $dbhost -P $OPENSHIFT_DB_PORT -u $OPENSHIFT_DB_USERNAME --password="$OPENSHIFT_DB_PASSWORD" --all-databases --add-drop-table | /bin/gzip -v > $OPENSHIFT_DATA_DIR/mysql_dump_snapshot.gz
 
 if [ ! ${PIPESTATUS[0]} -eq 0 ]
 then
