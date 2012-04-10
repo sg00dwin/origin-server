615d8731d60ddcb5fc08549a5226c7a375c87f8a
John (J5) Palmieri <johnp@redhat.com>
2012-04-10 11:57:49 -0400

    fix github urls to agreed upon path for OpenSource repos
    links will still give a 404 until OpenSource repo goes live

--- site/app/controllers/opensource_controller.rb
+++ site/app/controllers/opensource_controller.rb
@@ -4,6 +4,7 @@ class OpensourceController < SiteController
   end
 
   def download
-    @openshift_github_path = "https://github.com/openshift"
+    @openshift_github_url = "https://github.com/openshift"
+    @openshift_cartridges_github_url = "#{@openshift_github_url}/cartridges/master/tree"
   end
 end
