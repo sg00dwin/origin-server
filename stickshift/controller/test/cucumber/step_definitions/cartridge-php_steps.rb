01f26b5822800f439d12ab343959b95444b13e03
Krishna Raman <kraman@gmail.com>
2012-04-12 10:34:02 -0700

    Added cucumber as a development dependency Disabled show/concel port tests in OSS code

--- stickshift/controller/test/cucumber/step_definitions/cartridge-php_steps.rb
+++ stickshift/controller/test/cucumber/step_definitions/cartridge-php_steps.rb
@@ -197,15 +197,15 @@ When /^I (add-alias|remove-alias) the php application$/ do |action|
 end
 
 When /^I (expose-port|conceal-port) the php application$/ do |action|
-  account_name = @account['accountname']
-  namespace = @app['namespace']
-  app_name = @app['name']
-
-  command = "#{$php_hooks}/%s %s %s %s" % [action, app_name, namespace, account_name]
-  exit_status = runcon command, $selinux_user, $selinux_role, $selinux_type, nil, 10
-  if exit_status != 0
-    raise "Unable to %s for %s %s %s" % [action, app_name, namespace, account_name]
-  end
+#  account_name = @account['accountname']
+#  namespace = @app['namespace']
+#  app_name = @app['name']
+#
+#  command = "#{$php_hooks}/%s %s %s %s" % [action, app_name, namespace, account_name]
+#  exit_status = runcon command, $selinux_user, $selinux_role, $selinux_type, nil, 10
+#  if exit_status != 0
+#    raise "Unable to %s for %s %s %s" % [action, app_name, namespace, account_name]
+#  end
 end
 
 When /^I (start|stop) the php application$/ do |action|
@@ -229,15 +229,15 @@ Then /^the php application will( not)? be aliased$/ do | negate |
 end
 
 Then /^the php application will( not)? be exposed$/ do | negate |
-  account_name = @account['accountname']
-  namespace = @app['namespace']
-  app_name = @app['name']
-
-  good_status = negate ? 1 : 0
-
-  command = "#{$php_hooks}/show-port %s %s %s | /bin/grep -q PROXY_PORT" % [app_name, namespace, account_name]
-  exit_status = runcon command, 'unconfined_u', 'unconfined_r', 'unconfined_t'
-  exit_status.should == good_status
+#  account_name = @account['accountname']
+#  namespace = @app['namespace']
+#  app_name = @app['name']
+#
+#  good_status = negate ? 1 : 0
+#
+#  command = "#{$php_hooks}/show-port %s %s %s | /bin/grep -q PROXY_PORT" % [app_name, namespace, account_name]
+#  exit_status = runcon command, 'unconfined_u', 'unconfined_r', 'unconfined_t'
+#  exit_status.should == good_status
 end
 
 Then /^the php application will( not)? be running$/ do | negate |
