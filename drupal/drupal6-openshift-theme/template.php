9f0d1ea354c722740d10e1561d669284f7565186
Clayton Coleman <ccoleman@redhat.com>
2012-04-20 17:41:13 -0400

    Drupal updates based on latest changes

--- drupal/drupal6-openshift-theme/template.php
+++ drupal/drupal6-openshift-theme/template.php
@@ -272,9 +272,11 @@ function _openshift_heading(&$vars) {
   case 'calendar':
   case 'event':
   case 'events': $heading = "Events"; break; // no title for some reason
+  case 'knowledge_base':
   case 'kb': $heading = "Knowledge Base"; break; // no title for some reason
   case 'blogs': // no title for some reason
   case 'blog': $heading = "Blogs"; break;
+  case 'faq': $heading = "Frequently Asked Questions"; break;
   case 'videos': // no title for some reason
   case 'video': $heading = "Videos"; break;
   default:
