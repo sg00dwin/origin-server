2068e251ada3b5af69cbcb5ee1c42d087162311d
Clayton Coleman <ccoleman@redhat.com>
2012-04-23 15:01:44 -0400

    Reorder items in user_profile_box to work better with Steve's styling

--- drupal/drupal6-openshift-theme/comment.tpl.php
+++ drupal/drupal6-openshift-theme/comment.tpl.php
@@ -7,7 +7,7 @@ $do = og_comment_perms_do();
     <div class="comment-author clearfix">
       <?php print views_embed_view('user_profile_box', 'block_2', $comment->uid); ?>
       <?php if ($submitted): ?>
-        <span class="submitted"><?php print 'Posted ' . format_date($comment->timestamp, $type='custom', $format = 'F j, Y \a\t g:i A'); ?></span>
+        <div class="submitted right"><?php print 'Posted ' . format_date($comment->timestamp, $type='custom', $format = 'F j, Y \a\t g:i A'); ?></div>
       <?php endif; ?> 
     </div>
     
