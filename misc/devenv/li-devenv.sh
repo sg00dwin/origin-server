5e587fe031c0b55e6e27c42e436316494b0e656f
Troy Dawson <tdawson@redhat.com>
2012-04-27 09:37:16 -0500

    li-devenv.sh: added ruby193 repo, disabled

--- misc/devenv/li-devenv.sh
+++ misc/devenv/li-devenv.sh
@@ -84,6 +84,19 @@ sslclientcert=/etc/pki/entitlement/product/content-rhel6.crt
 sslcacert=/etc/pki/entitlement/cdn.redhat.com-chain.crt
 includepkgs=java-1.6.0-openjdk*
 
+[ruby193]
+name=Ruby193 Software Collection for RHEL6 - $basearch
+baseurl=https://mirror1.stg.rhcloud.com/libra/ruby193-rhel-6-${1-candidate}/\$basearch/
+        https://mirror2.stg.rhcloud.com/libra/ruby193-rhel-6-${1-candidate}/\$basearch/
+failovermethod=priority
+enabled=0
+gpgcheck=0
+gpgkey=https://mirror1.stg.rhcloud.com/libra/RPM-GPG-KEY-redhat-beta
+ggpkey=https://mirror1.stg.rhcloud.com/libra/RPM-GPG-KEY-redhat-release
+sslverify=0
+sslclientcert=/var/lib/yum/client-cert.pem
+sslclientkey=/var/lib/yum/client-key.pem
+
 EOF
 
 # Install the 32 bit java before anything else
