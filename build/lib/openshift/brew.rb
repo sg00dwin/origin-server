572cfb10edc1f1b86ce9588d4168829de4872d9b
Adam Miller <admiller@redhat.com>
2012-05-02 14:06:43 -0500

    Added some more logic to sort out what packages need stage tags

--- build/lib/openshift/brew.rb
+++ build/lib/openshift/brew.rb
@@ -1,6 +1,9 @@
+require 'build/lib/openshift/tito.rb'# FIXME - I think this is wrong 
+
 module OpenShift
   module Brew
 
+    include OpenShift::Tito
 
     # Public: Obtain the set of packages needing to be taggedin to stage
     # 
@@ -14,7 +17,11 @@ module OpenShift
     # Returns an array of brew package names
     def get_packages_to_tag(source, target)
   
-      stage_candidates = []
+      stage_candidates = []   # store candidates from brew query
+      need_stage_tags = []    # final set of packages that need tagging
+      our_pkgs = []           # our packages w/ full version from brew query
+      our_pkgs_names = get_packages # <-- from Tito
+      
 
       # FIXME:  I'm open to suggestion about the awk, I just couldn't think of a
       #         more clean implementation to get only tagged lines and the pkg
@@ -23,10 +30,26 @@ module OpenShift
       target = `brew -q latest-pkg #{[target]} --all \
                                               | awk '{print $1}'`.split("\n")
 
+      # Find which packages are new since last stage
       source.each do | pkg |
         stage_candidates.push( pkg ) unless target.include? pkg
       end
-      stage_candidates
+
+      # Find which packages belong to us
+      stage_candidates.each do | stg_candidate_pkg |
+        our_pkgs_names.each do | pkg, paths |
+          if stg_candidate_pkg =~ /#{pkg}-\d+\.\d+.*/
+            our_pkgs.push(stg_candidate_pkg)
+          end
+        end
+      end
+
+      # Now put it all together (remove our_pkgs)
+      stage_candidates.each do | pkg |
+        need_stage_tags.push( pkg ) unless our_pkgs.include? pkg
+      end
+      
+      need_stage_tags
     end
   
   end
