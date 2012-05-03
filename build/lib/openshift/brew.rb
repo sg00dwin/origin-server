f91b510d7514cea241ff4e5351667c3f1e31e545
Adam Miller <admiller@redhat.com>
2012-05-03 11:26:24 -0500

    check against brew tagging events to exclude obsolete stg pkgs

--- build/lib/openshift/brew.rb
+++ build/lib/openshift/brew.rb
@@ -9,13 +9,14 @@ module OpenShift
     # 
     # source  - The source brew tag to lookup (example: libra-rhel-6.2-candidate)
     # target  - The target brew tag to lookup (example: libra-rhel-6.2-stage)
+    # date    - Date since last stage: remove brew obsoletes (ex: 2012-04-15)
     #
     # Examples:
-    #   get_packages_to_tag("libra-rhel-6.2-candidate", "libra-rhel-6.2-stage")
+    #   get_packages_to_tag("libra-rhel-6.2-candidate", "libra-rhel-6.2-stage", "2012-04-15")
     #   # => ["rhc-0.91.12-1.el6_2", "rhc-broker-0.91.18-1.el6_2"]
     #
     # Returns an array of brew package names
-    def get_packages_to_tag(source, target)
+    def get_packages_to_tag(source, target, date)
   
       stage_candidates = []   # store candidates from brew query
       need_stage_tags = []    # final set of packages that need tagging
@@ -44,11 +45,29 @@ module OpenShift
         end
       end
 
-      # Now put it all together (remove our_pkgs)
+      # Now put it all together (remove our_pkgs and obsoletes)
       stage_candidates.each do | pkg |
-        need_stage_tags.push( pkg ) unless our_pkgs.include? pkg
+
+        # Special thanks to markllama, tdawson, and kwoodson for helping on this
+        # little nugget - strips off the Version-Release from pkg build
+        pkg_base_name = pkg.match(/(.*)(-[^-]+-[^-]+$)/)[1]
+
+        # FIXME - This takes forever to run because of the brew query for each
+        #         candidate package. Not sure of a better way to fix just yet.
+        brew_recent_activity = `brew list-history --tag=#{[source]} \
+                                --package=#{pkg_base_name} \
+                                --after=#{[date]}` unless our_pkgs.include? pkg
+
+        # Add to need_stage_tags unless it is one of ours, brew knows nothing
+        # about it and make sure there is recent activity in brew for the pkg
+        need_stage_tags.push( pkg ) \
+                unless our_pkgs.include? pkg \
+                      or ( ! brew_recent_activity.match(/GenericError/) \
+                      and brew_recent_activity.length > 1 )
       end
       
+
+
       need_stage_tags
     end
   
