2b176863ef082df11670017d0b2cd1a7957d8831
Lili Nader <lnader@redhat.com>
2012-05-07 11:38:57 -0700

    added exception handling and logging to apptegic and nurture calls

--- broker/app/models/domain_observer.rb
+++ broker/app/models/domain_observer.rb
@@ -21,7 +21,7 @@ class DomainObserver < ActiveModel::Observer
       Express::Broker::Nurture.libra_contact(domain.user.login, domain.user.uuid, domain.namespace, 'update')
       Express::Broker::Apptegic.libra_contact(domain.user.login, domain.user.uuid, domain.namespace, 'update')
     rescue Exception => e
-      Rail.logger.error e.message
+      Rail.logger.error "ERROR: sending analytic data #{e.message}"
     end
   end
 end
