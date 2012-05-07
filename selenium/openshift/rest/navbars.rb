fd28f94082e7d39d947f3d26a04d7c5d6d4d0fa6
John (J5) Palmieri <johnp@redhat.com>
2012-05-07 17:05:56 -0400

    [sauce] fix some of the FIXME's and remove TODOs that were done
    * add the navbars module
    * add functionality to open up user menu and click on signout instead of
      going directly to the logout page

---
+++ selenium/openshift/rest/navbars.rb
@@ -0,0 +1,46 @@
+module OpenShift
+  module Rest
+    class NavBar
+      include ::OpenShift::CSSHelpers
+
+      def initialize(page,id)
+        @page = page
+        @id = id
+        @base = "nav##{id}"
+      end
+    end
+
+    class MainNav < NavBar
+      @@items = {
+        :signin => 'a.sign_in',
+        :signout => 'a.sign_out',
+        :user_dropdown => 'ul#identity a.dropdown-toggle',
+
+        :learn_more => 'a.learn_more',
+        :getting_started => 'a.getting_started',
+        :community => 'a.community',
+        :developers => 'a.developers'
+      }
+
+      def link(name)
+        @@items[name]
+      end
+
+      def links
+        @@items
+      end
+
+      def click_signout
+        @page.action.click(find_link(:user_dropdown)).click(find_link(:signout)).perform
+      end
+
+      def find_link(element)
+        @page.find_element(:css, link(element))
+      end
+
+      def click(element)
+        find_link(element).click
+      end
+    end
+  end
+end
