a5e94883c1bc1297ecf20f3cfc2bcedfe622c922
Ravi Sankar <rpenta@redhat.com>
2012-05-10 14:34:40 -0700

    Usage model changes

--- broker/app/models/usage.rb
+++ broker/app/models/usage.rb
@@ -8,12 +8,14 @@ class Usage < StickShift::Model
   VALID_ACTIONS = ['create', 'destroy']
   VALID_GEAR_TYPES = ['small', 'medium', 'large']
 
-  key :uuid,       String, :required => true, :unique => true
-  key :gear_uuid,  String, :required => true
-  key :gear_type,  String, :required => true, :in => VALID_GEAR_TYPES
-  key :action,     String, :required => true, :in => VALID_ACTIONS
-  key :created_at, Time,   :required => true
-  attr_accessible :uuid, :gear_uuid, :gear_type, :action, :created_at
+  key :uuid,         String, :required => true, :unique => true
+  key :user_id,      String, :required => true
+  key :gear_uuid,    String, :required => true
+  key :gear_type,    String, :required => true, :in => VALID_GEAR_TYPES
+  key :action,       String, :required => true, :in => VALID_ACTIONS
+  key :create_time,  Time,   :required => true
+  key :destroy_time, Time,   :default => nil
+  attr_accessible :uuid, :user_id, :gear_uuid, :gear_type, :action, :create_time, :destroy_time
 
   timestamps!  # provides attribute updated_at
 
@@ -40,22 +42,41 @@ class Usage < StickShift::Model
     MongoMapper.connection[@db].authenticate(@user, @password) if @user
   end
   
-  def construct(gear_uuid, gear_type=nil, action=nil, created_at=nil)
-#    self.uuid = StickShift::Model.gen_uuid
-    # TODO: Fix uuid
-    self.uuid = gear_uuid
+  def construct(user_id, gear_uuid, gear_type=nil, action=nil, 
+                create_time=nil, destroy_time=nil, uuid=nil)
+    self.uuid = uuid
+    self.uuid = StickShift::Model.gen_uuid unless uuid
     self._id = self.uuid
+    self.user_id = user_id
     self.gear_uuid = gear_uuid
     self.gear_type = gear_type
     self.action = action
-    self.created_at = created_at
+    self.create_time = create_time
+    self.destroy_time = destroy_time
   end
 
   def self.find(uuid)
     super(uuid)
   end
 
-  def self.find_by_gear_uuid(gear_uuid)
+  def self.find_by_user(user_id)
+    where(:user_id => user_id).all
+  end
+
+  def self.find_by_user_after_time(user_id, time)
+    where(:user_id => user_id, :create_time => {:$gte => time}).all    
+  end
+
+  def self.find_by_user_time_range(user_id, begin_time, end_time)
+    # Issue with $nor op in MongoMapper and fix available @ https://github.com/jnunemaker/plucky/issues/19
+    # where(:user_id => user_id, :$nor => [ {:$and => [ {:create_time => {:$lt => begin_time}}, {:destroy_time => {:$lt => begin_time}} ]},
+    #                                       {:create_time => {:$gt => end_time}} ]).all    
+    # Rewriting the query to avoid $nor op                                      
+    where(:user_id => user_id, :$and => [ {:$or => [ {:create_time => {:$gte => begin_time}}, {:destroy_time => {:$gte => begin_time}} ]},
+                                          {:create_time => {:$lte => end_time}} ]).all    
+  end
+
+  def self.find_by_gear(gear_uuid)
     where(:gear_uuid => gear_uuid).all
   end
 
@@ -64,7 +85,6 @@ class Usage < StickShift::Model
   end
 
   def save(sync=true)
-    self.created_at = Time.now unless self.created_at
     super({:safe => sync})
   end
 
--- broker/test/unit/usage_model_test.rb
+++ broker/test/unit/usage_model_test.rb
@@ -8,8 +8,8 @@ class UsageModelTest < ActiveSupport::TestCase
   test "create and find usage event" do
     orig = usage
     ue = Usage.new
-    ue.construct(orig.gear_uuid, orig.gear_type,
-                 orig.action, orig.created_at)
+    ue.construct(orig.user_id, orig.gear_uuid, orig.gear_type,
+                 orig.action, orig.create_time, orig.destroy_time, orig.uuid)
     ue.save!
     ue = Usage.find(orig.uuid)
     ue.updated_at = nil
@@ -25,7 +25,7 @@ class UsageModelTest < ActiveSupport::TestCase
     ue = Usage.find(ue.uuid)
     assert_equal(nil, ue)
   end
-  
+ 
   test "find all usage events" do
     ues = Usage.find_all
     ues.each do |ue|
@@ -38,11 +38,52 @@ class UsageModelTest < ActiveSupport::TestCase
     ues = Usage.find_all
     assert(ues.length == 2)
   end
-  
-  test "find usage by gear uuid" do
+ 
+  test "find all usage events by user" do
     ue = usage
     ue.save!
-    ue = Usage.find_by_gear_uuid(ue.gear_uuid)
+    ue = Usage.find_by_user(ue.user_id)
+    assert(ue.length == 1)
+  end
+ 
+  test "find all user usage events since given time" do
+    ue1 = usage
+    ue1.save!
+    ue2 = usage
+    ue2.user_id = ue1.user_id
+    ue2.create_time = ue1.create_time + 100
+    ue2.save!
+    ue = Usage.find_by_user_after_time(ue1.user_id, ue1.create_time + 10)
+    assert(ue.length == 1)
+  end
+
+  test "find all user usage events given time range" do
+    cur_tm = Time.now
+    ue1 = usage
+    ue1.create_time = cur_tm - 100
+    ue1.destroy_time = cur_tm - 10
+    ue1.save!
+    ue2 = usage
+    ue2.user_id = ue1.user_id
+    ue2.create_time = cur_tm
+    ue2.destroy_time = cur_tm + 100
+    ue2.save!
+    ue3 = usage
+    ue3.user_id = ue1.user_id
+    ue3.create_time = cur_tm + 200
+    ue3.save!
+    ue = Usage.find_by_user_time_range(ue1.user_id, cur_tm + 10, cur_tm + 150)
+    assert(ue.length == 1)
+    ue = Usage.find_by_user_time_range(ue1.user_id, cur_tm + 10, cur_tm + 250)
+    assert(ue.length == 2)
+    ue = Usage.find_by_user_time_range(ue1.user_id, cur_tm -20, cur_tm + 10)
+    assert(ue.length == 2)
+  end
+
+  test "find usage by gear" do
+    ue = usage
+    ue.save!
+    ue = Usage.find_by_gear(ue.gear_uuid)
     assert(ue != nil)
   end
 
@@ -51,10 +92,12 @@ class UsageModelTest < ActiveSupport::TestCase
     obj = Usage.new
     obj.uuid = uuid
     obj._id = uuid
-    obj.gear_uuid = uuid
+    obj.user_id = "user#{gen_uuid}"
+    obj.gear_uuid = "gear#{gen_uuid}"
     obj.gear_type = 'small'
     obj.action = 'create'
-    obj.created_at = Time.now
+    obj.create_time = Time.now
+    obj.destroy_time = nil
     obj.updated_at = nil
     obj
   end
