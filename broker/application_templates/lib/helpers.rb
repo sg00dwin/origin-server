367d24b06421e770a4e833cee27fd910ec3c1b5a
Fotios Lindiakos <fotioslindiakos@gmail.com>
2012-06-01 03:32:24 -0400

    Don't need to rotate logfile numbers since logger will just append nicely

--- broker/application_templates/lib/helpers.rb
+++ broker/application_templates/lib/helpers.rb
@@ -10,24 +10,5 @@ end
 def logfile(name = 'log')
   dir = 'log'
   Dir.mkdir dir unless File.directory?(dir)
-
-  tries = 0
-  logfile = (
-    begin
-      file = File.join(
-        dir,
-        "%s%s.log" % [name,(tries > 0 ? ".#{tries}" : '')]
-      )
-      throw if File.exists?(file)
-      file
-    rescue
-      tries += 1
-      if tries < 10
-        retry
-      else
-        exit
-      end
-    end
-  )
-  logfile
+  File.join(dir,"#{name}.log")
 end
