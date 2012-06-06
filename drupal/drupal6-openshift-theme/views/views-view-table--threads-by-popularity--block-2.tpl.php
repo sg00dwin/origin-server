717f6e10600bae3d266efb83d524c067195f355b
Clayton Coleman <ccoleman@redhat.com>
2012-06-05 23:16:43 -0400

    Add caching to drupal views and blocks for better performance.  Remove unnecessary sections from UI

--- drupal/drupal6-openshift-theme/views/views-view-table--threads-by-popularity--block-2.tpl.php
+++ drupal/drupal6-openshift-theme/views/views-view-table--threads-by-popularity--block-2.tpl.php
@@ -7,7 +7,6 @@
   // We need to calculate the number of people participating in each thread (not the number of comments).
   // A separate view pulls unique author names and loads them into an array.
   // We just need to count the number of array elements.
-  $unique_participants = views_embed_view('unique_users_per_thread', 'block_1', $row['nid']);
   ?>
   <div class="<?php print implode(' ', $row_classes[$count]); ?>">
     <h4 class="views-field views-field-title"><?php print $row['title']; ?></h4>
@@ -15,7 +14,7 @@
       <span class="views-field views-field-author">Started by <?php print $row['name']; ?></span>
       <div>
         <span class="views-field views-field-created"><?php print $row['created']; ?></span>
-        <span class="views-field views-field-replies"><?php print format_plural($row['comment_count'], '1 reply', '@count replies'); ?></span>
+        <span class="views-field views-field-replies"><?php print $row['comment_count']; ?></span>
       </div>
     </div>
   </div>
