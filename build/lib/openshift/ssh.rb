eb1aebe0b71af859d372e923c2759508f52b7586
Dan McPherson <dmcphers@redhat.com>
2012-06-05 23:27:14 -0400

    add retries around init repo and update remote tests

--- build/lib/openshift/ssh.rb
+++ build/lib/openshift/ssh.rb
@@ -4,19 +4,28 @@ module OpenShift
 
     SCP_CMD = "scp 2> /dev/null -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PasswordAuthentication=no -i " + RSA
 
-    def ssh(hostname, cmd, timeout=60, return_exit_code=false)
+    def ssh(hostname, cmd, timeout=60, return_exit_code=false, num_tries=1)
       log.debug "(ssh: hostname = #{hostname} timeout = #{timeout} / cmd = #{cmd})"
       output = ""
       exit_code = 1
-      begin
-        ssh_cmd = "#{SSH_CMD} root@#{hostname} '#{cmd} 2>&1'"
-        log.debug(ssh_cmd)
-        Timeout::timeout(timeout) do
-          output = `#{ssh_cmd}`.chomp
-          exit_code = $?.exitstatus
+      ssh_cmd = "#{SSH_CMD} root@#{hostname} '#{cmd} 2>&1'"
+      (1..num_tries).each do |i|
+        begin
+          log.debug(ssh_cmd)
+          Timeout::timeout(timeout) do
+            output = `#{ssh_cmd}`.chomp
+            exit_code = $?.exitstatus
+          end
+          if exit_code == 0
+            break
+          elsif i == num_tries
+            puts "\nSSH failed to #{hostname} with exit_code: #{exit_code}  output: #{output}"
+          else
+            sleep 10
+          end
+        rescue Timeout::Error
+          log.error "SSH command to #{hostname} timed out (timeout = #{timeout})"
         end
-      rescue Timeout::Error
-        log.error "SSH command to #{hostname} timed out (timeout = #{timeout})"
       end
       log.debug "----------------------------\n#{output}\n----------------------------"
 
@@ -43,25 +52,25 @@ module OpenShift
     def scp_to(hostname, local, remote, timeout=15, num_tries=5)
       log.debug "(scp_to: timeout = #{timeout}) / local = '#{local}' remote = '#{remote}'"
       output = ""
-      begin
-        scp_cmd = "#{SCP_CMD} -r #{local} root@#{hostname}:#{remote} 2>&1"
-        (1..num_tries).each do |i|
+      scp_cmd = "#{SCP_CMD} -r #{local} root@#{hostname}:#{remote} 2>&1"
+      (1..num_tries).each do |i|
+        begin
+          exit_code = 1
           Timeout::timeout(timeout) {
             output = `#{scp_cmd}`
             exit_code = $?
-            if exit_code == 0
-              break
-            elsif i == num_tries
-              puts "\nSCP failed to #{hostname} with output: #{output}"
-              exit 1
-            else
-              sleep 10
-            end
           }
-        sleep 5
+          if exit_code == 0
+            break
+          elsif i == num_tries
+            puts "\nSCP failed to #{hostname} with output: #{output}"
+            exit 1
+          else
+            sleep 10
+          end
+        rescue Timeout::Error
+          log.error "SCP command '#{scp_cmd}' timed out"
         end
-      rescue Timeout::Error
-        log.error "SCP command '#{scp_cmd}' timed out"
       end
       log.debug "----------------------------\n#{output}\n------------------------------"
       return output
