b82524a3051dd69b7228555652ce36565470cfed
Rajat Chopra <rchopra@redhat.com>
2012-06-11 17:43:28 -0700

    working revision for filesystem configure

--- cartridges/zend-5.6/info/bin/zend_configure_filesystem.sh
+++ cartridges/zend-5.6/info/bin/zend_configure_filesystem.sh
@@ -9,6 +9,12 @@ file_list=(
 "/usr/local/zend/gui/lighttpd/tmp"
 "/usr/local/zend/gui/application/data"
 )
+template_file_list=(
+"/usr/local/zend/etc/monitor_node.ini"
+"/usr/local/zend/etc/conf.d/ZendGlobalDirectives.ini"
+"/usr/local/zend/gui/application/data/zend-server.ini"
+"/usr/local/zend/etc/lighttpd.conf"
+)
 
 target_pam="/tmp/zend"
 restore=$1
@@ -47,12 +53,29 @@ function copy_fs_to_pam {
   for zpath in ${file_list[*]}; do
     zdir=`dirname $zpath`
     zfile=`basename $zpath`
-    if [ ! -f $zdir/${zfile}_openshift_original ]; then
+    if [ ! -e $zdir/${zfile}_openshift_original ]; then
       echo "File $zdir/${zfile}_openshift_original does not exist"
       continue
     fi
     echo "Copying $zdir/${zfile}_openshift_original $target_location/$zdir/$zfile"
+    mkdir -p $target_location/$zdir
     cp -rf $zdir/${zfile}_openshift_original $target_location/$zdir/$zfile
+  done
+  for zpath in ${template_file_list[*]}; do
+    zdir=`dirname $zpath`
+    zfile=`basename $zpath`
+    if [ ! -e $zdir/${zfile} ]; then
+      echo "File $zdir/${zfile} does not exist"
+      continue
+    fi
+    echo "Replacing ###INTERNAL_IP### with ${OPENSHIFT_INTERNAL_IP} in $zdir/${zfile}"
+    sed -i "s/###INTERNAL_IP###/${OPENSHIFT_INTERNAL_IP}/g" $zdir/${zfile}
+    MYUID=`id -u`
+    echo "Replacing ###UID### with $MYUID in $zdir/${zfile}"
+    sed -i "s/###UID###/${MYUID}/g" $zdir/${zfile}
+    GID=`id -g`
+    echo "Replacing ###GID### with $GID in $zdir/${zfile}"
+    sed -i "s/###GID###/${GID}/g" $zdir/${zfile}
   done
 }
 
@@ -62,6 +85,10 @@ if [ $uid -eq 0 ] ; then
  create_links
 else
  echo "Running as non-root..."
+ for f in ~/.env/*
+ do
+     . $f
+ done
  mkdir -p $target_pam
  copy_fs_to_pam $target_pam
 fi
