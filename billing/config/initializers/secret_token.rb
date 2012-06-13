b9e0a1e4919bdcb09aba45cd7f5d34335a0abf01
Ravi Sankar <rpenta@redhat.com>
2012-06-13 11:39:08 -0700

    Remove stale billing code

--- billing/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Billing::Application.config.secret_token = '841cf9258fbfb4d236600b731f9cc1ee6a020d2a3cf2cc51273820ef13cdf06e24a998ce7aedb3656abce94ad694c0c4e5c72b830343ca14bfdfa8099580f2f0'
--- billing/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Billing::Application.routes.draw do
-  scope "/rest" do
-    resource :api, :only => [:show], :controller => :base
-    resources :events, :controller => :billing_events, :only => [:create]
-  end
-end
