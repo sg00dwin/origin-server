b373d95de2e0b1f7dc5302c18b66742a2bb35026
Fotios Lindiakos <fotios@redhat.com>
2012-06-18 18:04:49 -0400

    BZ828116: Added logic for displaying credentials for applications created from templates
    Descriptor changes:
     - Automatically sorting descriptors to see changes in diffs better
     - Regenerated descriptors without doc-root
     - Added credentials metadata to drupal and wordpress templates

--- broker/application_templates/lib/helpers.rb
+++ broker/application_templates/lib/helpers.rb
@@ -18,6 +18,28 @@ def login(password = nil)
   )
 end
 
+def returning(value)
+  yield(value)
+  value
+end
+
+def deep_sort(object, deep = false)
+  # from http://seb.box.re/2010/1/15/deep-hash-ordering-with-ruby-1-8/
+  if object.is_a?(Hash)
+    # Hash is ordered in Ruby 1.9!
+    res = returning(RUBY_VERSION >= '1.9' ? Hash.new : ActiveSupport::OrderedHash.new) do |map|
+      object.each {|k, v| map[k] = deep ? convert_hash_to_ordered_hash_and_sort(v, deep) : v }
+    end
+    return res.class[res.sort {|a, b| a[0].to_s <=> b[0].to_s } ]
+  elsif deep && object.is_a?(Array)
+    array = Array.new
+    object.each_with_index {|v, i| array[i] = convert_hash_to_ordered_hash_and_sort(v, deep) }
+    return array
+  else
+    return object
+  end
+end
+
 class Array
   def filter(items = [],invert = false)
     # Allow us to pass in anything that can test for include
