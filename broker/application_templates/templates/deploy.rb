da73f33725a4249e66c2fb29be132d2b54865dd6
Fabiano Franz <contact@fabianofranz.com>
2012-07-05 22:48:35 -0300

    Adjusted broker/application_templates/templates/deploy.rb with YAML generated by Ruby 1.9

--- broker/application_templates/templates/deploy.rb
+++ broker/application_templates/templates/deploy.rb
@@ -29,8 +29,8 @@ templates.each do |name,opts|
 end
 
 __END__
---- !omap 
-- drupal: 
+---
+drupal: 
     :descriptor: 
       Connections: 
         mysql-5.1-php-5.3: 
@@ -51,17 +51,17 @@ __END__
       - php-5.3
       - mysql-5.1
     :script: rhc-admin-ctl-template --command 'add' --cost '1' --git-url 'git://github.com/openshift/drupal-example.git' --named 'Drupal' --tags 'php,drupal,wiki,framework,experimental'
-    :metadata: !omap 
-      - :credentials: 
-        - :password: OpenShiftAdmin
-          :username: Admin
-      - :description: An open source content management platform written in PHP powering millions of websites and applications. It is built, used, and supported by an active and diverse community of people around the world.
-      - :git_project_url: http://github.com/openshift/drupal-example
-      - :git_url: git://github.com/openshift/drupal-example.git
-      - :license: :"gpl2+"
-      - :version: 7.7
-      - :website: http://drupal.org/
-- kitchensink: 
+    :metadata: 
+      :credentials: 
+      - :password: OpenShiftAdmin
+        :username: Admin
+      :description: An open source content management platform written in PHP powering millions of websites and applications. It is built, used, and supported by an active and diverse community of people around the world.
+      :git_project_url: http://github.com/openshift/drupal-example
+      :git_url: git://github.com/openshift/drupal-example.git
+      :license: :"gpl2+"
+      :version: 7.7
+      :website: http://drupal.org/
+kitchensink: 
     :descriptor: 
       Scaling: 
         Max: -1
@@ -76,14 +76,14 @@ __END__
       Requires: 
       - jbossas-7
     :script: rhc-admin-ctl-template --command 'add' --cost '1' --git-url 'git://github.com/openshift/kitchensink-example.git' --named 'Kitchensink Example' --tags 'java,jboss,framework,experimental'
-    :metadata: !omap 
-      - :description: This quickstart uses JBoss AS7 to show off all the new features of Java EE 6 and makes a great starting point for your Java project.
-      - :git_project_url: http://github.com/openshift/kitchensink-example
-      - :git_url: git://github.com/openshift/kitchensink-example.git
-      - :license: :apache2
-      - :version: 7.0.0
-      - :website: https://docs.jboss.org/author/display/AS71/Kitchensink+quickstart
-- rails: 
+    :metadata: 
+      :description: This quickstart uses JBoss AS7 to show off all the new features of Java EE 6 and makes a great starting point for your Java project.
+      :git_project_url: http://github.com/openshift/kitchensink-example
+      :git_url: git://github.com/openshift/kitchensink-example.git
+      :license: :apache2
+      :version: 7.0.0
+      :website: https://docs.jboss.org/author/display/AS71/Kitchensink+quickstart
+rails: 
     :descriptor: 
       Connections: 
         mysql-5.1-ruby-1.8: 
@@ -104,36 +104,36 @@ __END__
       - ruby-1.8
       - mysql-5.1
     :script: rhc-admin-ctl-template --command 'add' --cost '1' --git-url 'git://github.com/openshift/rails-example.git' --named 'Ruby on Rails' --tags 'ruby,rails,framework,experimental'
-    :metadata: !omap 
-      - :description: An open source web framework for Ruby that is optimized for programmer happiness and sustainable productivity. It lets you write beautiful code by favoring convention over configuration.
-      - :git_project_url: http://github.com/openshift/rails-example
-      - :git_url: git://github.com/openshift/rails-example.git
-      - :license: :mit
-      - :version: 3.1.1
-      - :website: http://rubyonrails.org/
-- springeap6: 
-    :descriptor: !omap 
-      - Architecture: noarch
-      - Description: ""
-      - Display-Name: springeap6-0.0-noarch
-      - License: unknown
-      - Name: springeap6
-      - Requires: 
-        - jbosseap-6.0
-      - Scaling: 
-          Max: -1
-          Min: 1
-      - Vendor: unknown
-      - Version: "0.0"
+    :metadata: 
+      :description: An open source web framework for Ruby that is optimized for programmer happiness and sustainable productivity. It lets you write beautiful code by favoring convention over configuration.
+      :git_project_url: http://github.com/openshift/rails-example
+      :git_url: git://github.com/openshift/rails-example.git
+      :license: :mit
+      :version: 3.1.1
+      :website: http://rubyonrails.org/
+springeap6: 
+    :descriptor: 
+      Architecture: noarch
+      Description: ""
+      Display-Name: springeap6-0.0-noarch
+      License: unknown
+      Name: springeap6
+      Requires: 
+      - jbosseap-6.0
+      Scaling: 
+        Max: -1
+        Min: 1
+      Vendor: unknown
+      Version: "0.0"
     :script: rhc-admin-ctl-template --command 'add' --cost '1' --git-url 'git://github.com/openshift/spring-eap6-quickstart.git' --named 'Spring Framework on JBoss EAP6' --tags 'java,jboss,framework,experimental'
-    :metadata: !omap 
-      - :description: This quickstart allows you to use Spring Framework on JBoss JBoss EAP 6.
-      - :git_project_url: http://github.com/openshift/spring-eap6-quickstart
-      - :git_url: git://github.com/openshift/spring-eap6-quickstart.git
-      - :license: :apache2
-      - :version: 3.1.1
-      - :website: http://springframework.org/
-- wordpress: 
+    :metadata: 
+      :description: This quickstart allows you to use Spring Framework on JBoss JBoss EAP 6.
+      :git_project_url: http://github.com/openshift/spring-eap6-quickstart
+      :git_url: git://github.com/openshift/spring-eap6-quickstart.git
+      :license: :apache2
+      :version: 3.1.1
+      :website: http://springframework.org/
+wordpress: 
     :descriptor: 
       Connections: 
         mysql-5.1-php-5.3: 
@@ -154,13 +154,14 @@ __END__
       - php-5.3
       - mysql-5.1
     :script: rhc-admin-ctl-template --command 'add' --cost '1' --git-url 'git://github.com/openshift/wordpress-example.git' --named 'WordPress' --tags 'php,wordpress,blog,framework,experimental'
-    :metadata: !omap 
-      - :credentials: 
-        - :password: OpenShiftAdmin
-          :username: Admin
-      - :description: A semantic personal publishing platform written in PHP with a MySQL back end, focusing on aesthetics, web standards, and usability.
-      - :git_project_url: http://github.com/openshift/wordpress-example
-      - :git_url: git://github.com/openshift/wordpress-example.git
-      - :license: :"gpl2+"
-      - :version: 3.3.2
-      - :website: http://wordpress.org
+    :metadata: 
+      :credentials: 
+      - :password: OpenShiftAdmin
+        :username: Admin
+      :description: A semantic personal publishing platform written in PHP with a MySQL back end, focusing on aesthetics, web standards, and usability.
+      :git_project_url: http://github.com/openshift/wordpress-example
+      :git_url: git://github.com/openshift/wordpress-example.git
+      :license: :"gpl2+"
+      :version: 3.3.2
+      :website: http://wordpress.org
+
