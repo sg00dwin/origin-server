2a010963cc698489914c7ce802a5bb2d07b93041
Mrunal Patel <mpatel@redhat.com>
2012-07-24 10:47:13 -0700

    Fix district tests to use library call instead of mco.

--- tests/step_definitions/district_steps.rb
+++ tests/step_definitions/district_steps.rb
@@ -1,11 +1,16 @@
 require 'rubygems'
 require 'parseconfig'
+require 'mcollective'
+
+include MCollective::RPC
+options = MCollective::Util.default_options
 
 Given /^a district (.*) is active$/ do |uuid|
   # Clean up anything left over
   FileUtils.rm_f "/var/lib/stickshift/.settings/district.info"
-  exit_code = run "mco rpc stickshift set_district uuid=#{uuid} active='true'"
-  exit_code.should be == 0
+  mc = rpcclient("stickshift", {:options => options})
+  reply = mc.set_district(:uuid => uuid, :active => 'true')                                            
+  reply[0][:data][:exitcode].should be == 0
 end
 
 Then /^the file (.*) is active for district (.*)$/ do |file, uuid|
