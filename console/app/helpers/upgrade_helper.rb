5af3cd99765580a6a3efb4a7147b913ec9cb2075
Clayton Coleman <ccoleman@redhat.com>
2012-08-26 15:57:02 -0400

    Most tests pass, move stylesheets back to site.

--- console/app/helpers/upgrade_helper.rb
+++ console/app/helpers/upgrade_helper.rb
@@ -1,5 +1,5 @@
 module UpgradeHelper
   def upgrade_in_rails_31
-    raise "Code needs upgrade for rails 3.1+" if Rails.version[0..3] != '3.0.'
+    controller.send(:upgrade_in_rails_31)
   end
 end
