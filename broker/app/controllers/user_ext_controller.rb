87c9d2c00f5adbe60c33ececf9d7110baff6aed5
Ravi Sankar <rpenta@redhat.com>
2012-08-27 15:53:08 -0700

    Fix for bugs# 851735, 851345

--- broker/app/controllers/user_ext_controller.rb
+++ broker/app/controllers/user_ext_controller.rb
@@ -2,6 +2,17 @@ class UserExtController < UserController
   respond_to :xml, :json
   before_filter :authenticate, :check_version
 
+  # GET /user
+  def show
+    unless @cloud_user
+      log_action(@request_id, 'nil', @login, "SHOW_USER", false, "User '#{@login}' not found")
+      return render_error(:not_found, "User '#{@login}' not found", 99)
+    end
+    user = RestUser.new(@cloud_user, get_url, nolinks)
+    user.plan_id = Rails.application.config.billing[:aria][:default_plan].to_s unless user.plan_id
+    render_success(:ok, "user", user, "SHOW_USER")
+  end
+
   # PUT /user
   def update 
     unless @cloud_user
@@ -14,7 +25,7 @@ class UserExtController < UserController
     rescue Exception => e
       return render_format_exception(e, "UPDATE_USER")
     end
-    render_format_success(:ok, "account", RestUser.new(@cloud_user, get_url),
+    render_format_success(:ok, "account", RestUser.new(@cloud_user, get_url, nolinks),
                           "UPDATE_USER", "Plan successfully changed")
   end
 end
