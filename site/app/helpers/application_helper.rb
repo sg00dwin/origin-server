cc8428c1b47c22bf7ffbd65d4613c24265bbbdab
Clayton Coleman <ccoleman@redhat.com>
2012-09-04 18:03:41 -0400

    Abstract branding, begin moving images.

--- site/app/helpers/application_helper.rb
+++ site/app/helpers/application_helper.rb
@@ -14,4 +14,11 @@ module ApplicationHelper
   def product_title
     "OpenShift by Red Hat"
   end
+
+  def product_branding
+    [
+      content_tag(:span, nil, :class => 'brand-image'),
+      content_tag(:span, "<strong>Open</strong>Shift".html_safe, :class => 'brand-text headline'),
+    ].join.html_safe
+  end
 end
