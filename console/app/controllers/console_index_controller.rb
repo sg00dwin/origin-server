7c97f8eb46e5fd364089d92ea72b3bb39d0f01b4
Clayton Coleman <ccoleman@redhat.com>
2012-09-26 12:30:24 -0400

    Content moved to crankcase

--- console/app/controllers/console_index_controller.rb
+++
@@ -1,5 +0,0 @@
-class ConsoleIndexController < ConsoleController
-  def index
-    redirect_to applications_path
-  end
-end
--- console/lib/console.rb
+++
@@ -1,7 +0,0 @@
-require 'console/version.rb'
-require 'console/engine.rb'
-
-module Console
-  class AccessDenied < StandardError ; end
-  class ApiNotAvailable < StandardError ; end
-end
