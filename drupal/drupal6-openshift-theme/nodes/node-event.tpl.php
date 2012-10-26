49173fd51337c461afc1fe6b959842f477b04b77
Fabiano Franz <ffranz@redhat.com>
2012-10-26 17:13:02 -0200

    New Events section styles, added event detail page

--- drupal/drupal6-openshift-theme/nodes/node-event.tpl.php
+++ drupal/drupal6-openshift-theme/nodes/node-event.tpl.php
@@ -5,17 +5,49 @@
 //echo '</pre>';
 $do = og_comment_perms_do();
 ?>
-<div id="node-<?php print $node->nid; ?>" class="event <?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?>">
 
-  <h3><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h3>
+<section id="node-<?php print $node->nid; ?>" class="<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?>">
 
-  <div class="content">
-    <?php print $content ?>
-  </div>
-  <?php if ($do->perm == 'post'): ?>
-  
-  <br />
-  <?php endif; ?>
-  
-  
-</div>
+<div class="breadcrumb"><a href="/community/events">Events</a><span class="divider"> /</span>CodeStrong</div>
+
+<div class="events-post node node-events">
+
+  <div class="row">
+  <div class="span9">
+    <div class="event-metadata">
+    <figure class="pull-right related-block">
+    <div class="related-logo" style="margin-right: 10%;"><img src="http://lacot.org/media/original/blog/2011/09/logo-codestrong.png" alt="codestrong logo" />
+    </div>
+    </figure>
+    
+    <h1><?php print $title ?></h1>
+    <p class="event-date">
+    	<?php print $node->field_event_start_date[0]["view"] ?>
+    	<a href="" class="add-calendar">Add to calendar</a>
+    </p>
+    
+    <h4>Intercontinental Hotel</h4>
+    <?php print $node->field_event_city[0]["view"] ?>, 
+    <?php print $node->field_event_state[0]["view"] ?>
+    <?php print $node->field_event_country[0]["view"] ?><br />
+    <?php print $node->field_event_url[0]["view"] ?><br />
+    
+    </div>
+
+    <p>
+    	<?php print $node->content["body"]["#value"] ?>
+    </p>
+
+    <?php print $node->field_event_categories[0]["view"] ?>
+    
+    </div>
+  </div><!-- /row -->
+
+</div>  
+</section>
+
+<!--
+<pre>
+	<?php //print_r($node); ?>
+</pre>
+-->
