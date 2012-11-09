28f98329f4ce789b055eb81a3f8b938826c5a02a
Fabiano Franz <ffranz@redhat.com>
2012-11-09 16:55:26 -0200

    Styled the events list by country, sync date and country views, ical feed improvements

--- drupal/drupal6-openshift-theme/views/views-bonus-export-txt--event-view--feed.tpl.php
+++ drupal/drupal6-openshift-theme/views/views-bonus-export-txt--event-view--feed.tpl.php
@@ -1,6 +1,7 @@
 BEGIN:VCALENDAR
 VERSION:2.0
 METHOD:PUBLISH
+X-WR-CALNAME:OpenShift Events
 PRODID:-//Drupal iCal API//EN
 <?php
 foreach ($themed_rows as $count => $row):
@@ -12,8 +13,9 @@ foreach ($row as $field => $content):
 <?php endforeach; ?>
 END:VCALENDAR
 <?php
-header("Content-Type: text/calendar");
-header("Content-Disposition: attachment; filename=feed.ics");
-header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-header("Expires: Sat, 01 Jan 2000 05:00:00 GMT"); // Date in the past
+drupal_set_header("Content-Type: text/calendar");
+drupal_set_header("Content-Type: text/calendar");
+drupal_set_header("Content-Disposition: attachment; filename=feed.ics");
+drupal_set_header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+drupal_set_header("Expires: Sat, 01 Jan 2000 05:00:00 GMT"); // Date in the past
 ?>
