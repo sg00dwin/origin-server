940f9bb6a8bb901c93083857bea0cdeb9f63e664
Clayton Coleman <ccoleman@redhat.com>
2012-11-21 17:43:23 -0500

    US3055 - Enterprise content for stage

--- drupal/drupal6-openshift-theme/page.tpl.php
+++ drupal/drupal6-openshift-theme/page.tpl.php
@@ -63,7 +63,6 @@
       <div id="top" class="section-top">
         <div class="container">
         	<a title="Track open issues on the OpenShift status page" style="display:none;" id="outage" class="btn btn-small btn-warning" href="/app/status">Status</a>
-          <div class="pull-left mobile-block"><a href="/community/enterprise-paas"><span>Streamline App Delivery with  </span>OpenShift Enterprise</a></div>
           <div class="pull-right login">
 
           <form action="/community/search/node" method="get" id="search-top">
@@ -87,6 +86,7 @@
             }
             ?>
           </div>
+          <div class="mobile-block"><a href="/community/enterprise-paas"><span>Streamline App Delivery with  </span>OpenShift Enterprise</a></div>
         </div>
       </div>
 
