e00bd7b19f0dd1e9c2b87dc3f9ab40dbdbdd1769
Jhon Honce <jhonce@redhat.com>
2012-12-10 15:55:00 -0700

    Fix for Bug 880013
    Allow zend applications to be renamed using snapshot/restore

--- cartridges/zend-5.6/info/bin/restore_tar.sh
+++ cartridges/zend-5.6/info/bin/restore_tar.sh
@@ -11,7 +11,7 @@ cartridge_dir=$OPENSHIFT_HOMEDIR/$cartridge_type
 # including all data dirs and excluding the cartridge ones.
 includes=( "./*/*/data" "./*/zend-5.6/etc" "./*/zend-5.6/var" "./*/zend-5.6/gui" )
 
-transforms=( --transform="s|${cartridge_type}/data|app-root/data|" )
+transforms=( --transform="s|${cartridge_type}/data|app-root/data|" --transform="s|git/.*\.git|git/${OPENSHIFT_GEAR_NAME}.git|" )
 
 excludes=()
 carts=( $(source /etc/openshift/node.conf; ls $CARTRIDGE_BASE_PATH ; ls $CARTRIDGE_BASE_PATH/embedded) )
@@ -23,8 +23,8 @@ done
 
 if [ "$include_git" = "INCLUDE_GIT" ]
 then
-  echo "Restoring ~/git/${cartridge_type}.git and ~/app-root/data" 1>&2
-  excludes=( "${excludes[@]}" --exclude="./*/git/${cartridge_type}.git/hooks" )
+  echo "Restoring ~/git/${OPENSHIFT_GEAR_NAME}.git and ~/app-root/data" 1>&2
+  excludes=( "${excludes[@]}" --exclude="./*/git/*.git/hooks" )
   includes=( "${includes[@]}" "./*/git" )
 else
   echo "Restoring ~/app-root/data" 1>&2
