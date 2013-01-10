d1c7ef6f5c4bbc90089cabc274f24dee786728a6
Ravi Sankar <rpenta@redhat.com>
2013-01-09 18:30:42 -0800

    Added Distributed Lock using mongoid model + unit testcase

---
+++ rhc-broker/test/unit/distributed_lock_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class DistributedLockTest < ActiveSupport::TestCase
+  def setup
+    super
+  end
+
+  test "distributed lock" do
+    dl = DistributedLock
+    type = gen_uuid
+    assert(dl.obtain_lock(type, "1"))
+    assert(!dl.obtain_lock(type, "1"))
+    assert(!dl.obtain_lock(type, "2"))
+    dl.release_lock(type, "1")
+    assert(dl.obtain_lock(type, "2"))
+    assert(dl.obtain_lock(type, "2", true))
+    dl.release_lock(type)
+    assert(dl.obtain_lock(type, "2"))
+    dl.release_lock(type)
+  end
+end
