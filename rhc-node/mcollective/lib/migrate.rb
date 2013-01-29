28c88b42c70f94d9bd5d082bc8f990a3bd191370
Dan McPherson <dmcphers@redhat.com>
2013-01-28 23:13:24 -0500

    Bug 886182

--- rhc-node/mcollective/lib/migrate.rb
+++ rhc-node/mcollective/lib/migrate.rb
@@ -27,7 +27,7 @@ module OpenShiftMigration
   # at any point and continue to pick up where it left off or make
   # harmless changes the 2-n times around.
   def self.migrate(uuid, namespace, version)
-    unless version == "2.0.22"
+    unless version == "2.0.23"
       return "Invalid version: #{version}", 255
     end
 
@@ -55,43 +55,6 @@ module OpenShiftMigration
       exitcode = 127
       output += "Application not found to migrate: #{gear_home}\n"
       return output, exitcode
-    end
-
-    if File.directory?(File.join(gear_home, "postgresql-8.4"))
-      socket_value = Util.get_env_var_value(gear_home, "OPENSHIFT_POSTGRESQL_DB_SOCKET").to_s
-
-      if (socket_value =~ /socket.+$/ )
-        output += "OPENSHIFT_POSTGRESQL_DB_SOCKET: #{socket_value}\n"
-        socket_value.gsub!(/socket.+$/, "socket")
-        output += "Translating OPENSHIFT_POSTGRESQL_DB_SOCKET: #{socket_value}\n"
-        Util.set_env_var_value(gear_home, "OPENSHIFT_POSTGRESQL_DB_SOCKET", socket_value)
-      end
-
-      cart_dir = "#{gear_home}/postgresql-8.4"
-
-      Util.replace_in_file("#{cart_dir}/data/pg_hba.conf", "ident$", "md5")
-      Util.replace_in_file("#{cart_dir}/data/postgresql.conf", "unix_socket_directory = '/tmp'", "unix_socket_directory = '#{cart_dir}/socket'")
-    end
-
-    # Many files in the app git repo may have been reset to root as a result of
-    # https://bugzilla.redhat.com/show_bug.cgi?id=903152. Reset the ownership
-    # of the known affected files to the gear user/group.
-    app_git_dir = File.join(gear_home, "git", "#{app_name}.git")
-    if File.directory?(app_git_dir)
-      files_to_reset = [
-        File.join(app_git_dir, "packed-refs"),
-        File.join(app_git_dir, "objects", "info", "packs"),
-        File.join(app_git_dir, "info", "refs")
-      ]
-
-      pack_dir = File.join(app_git_dir, "objects", "pack", "*")
-
-      begin
-        FileUtils.chown(uuid, uuid, files_to_reset)
-        FileUtils.chown(uuid, uuid, Dir.glob(pack_dir))
-      rescue Exception => e
-        output += "ERROR: Couldn't reset git repo ownership for gear #{uuid}: #{e.message}\n#{e.backtrace}\n"
-      end
     end
 
     env_echos.each do |env_echo|
