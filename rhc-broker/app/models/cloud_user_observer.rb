24dafaed8fa396295c76af3285437c6b6db3f53e
Clayton Coleman <ccoleman@redhat.com>
2013-01-31 17:29:03 -0500

    Review - remove old lines

--- rhc-broker/app/models/cloud_user_observer.rb
+++ rhc-broker/app/models/cloud_user_observer.rb
@@ -4,12 +4,6 @@ class CloudUserObserver < ActiveModel::Observer
 
   def before_cloud_user_create(user)
     raise OpenShift::UserException.new("Invalid characters in login '#{user.login}' found", 107) if user.login =~ /["\$\^<>\|%\/;:,\\\*=~]/
-
-    #plan_id = user.plan_id || Rails.configuration.billing[:aria][:default_plan]
-    #user.assign_plan(plan_id) if plan_id
-    #if plan_id and !user.plan_id and not user.capabilities_changed?
-    #  user.assign_plan(plan_id)
-    #end
   end
 
   def cloud_user_create_success(user)
