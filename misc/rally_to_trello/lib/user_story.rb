432e8eceea859a91fe377d2699a0a27a4871d7db
Dan McPherson <dmcphers@redhat.com>
2013-02-21 00:18:25 -0500

    rally to trello script - WIP

---
+++ misc/rally_to_trello/lib/user_story.rb
@@ -0,0 +1,33 @@
+class UserStory
+  attr_accessor :data
+
+  def initialize(rest_data)
+    @data = rest_data
+  end
+
+  def method_missing(method,*args,&block)
+    case method
+    when *CONFIG.states.keys
+      check_state(method)
+    else
+      @data.send(method,*args,&block)
+    end
+  end
+
+  def check_tags(target)
+    if tags.nil?
+      false
+    else
+      tags.map{|x| x.to_s }.include?(target)
+    end
+  end
+
+  def check_notes(regex)
+    notes =~ regex
+  end
+
+  def check_state(val)
+    schedule_state == CONFIG.states[val]
+  end
+
+end
