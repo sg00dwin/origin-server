ef5654472f5866e6fbe18bafffe06617f3c91584
Clayton Coleman <ccoleman@redhat.com>
2013-02-24 11:13:34 -0500

    Bug 913816 - Fix log tailer to pick up the correct config

--- rhc-broker/config/application.rb
+++ rhc-broker/config/application.rb
@@ -21,8 +21,8 @@ module Broker
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Set logs to somewhere more standard
-    config.paths['log'] = "#{ENV['RAILS_LOG_PATH']}broker/#{Rails.env}.log" if ENV['RAILS_LOG_PATH']
+    # Set logs to a standard location
+    config.paths['log'] = "#{ENV['RAILS_LOG_PATH'] || '/var/log/openshift/'}broker/#{Rails.env}.log" 
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
