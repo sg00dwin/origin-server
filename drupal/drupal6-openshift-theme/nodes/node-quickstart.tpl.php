2c80156d66d6c3af4be1df2e9bbbc57143afdde5
Robb Hamilton <rhamilto@redhat.com>
2013-02-28 16:54:10 -0500

    US3113:  detabbing _community.scss, addressing .links.inline a { } bug, and first cut at styling of a quick start page
    Conflicts:
    	site/app/assets/stylesheets/site/_community.scss

--- drupal/drupal6-openshift-theme/nodes/node-quickstart.tpl.php
+++ drupal/drupal6-openshift-theme/nodes/node-quickstart.tpl.php
@@ -6,31 +6,32 @@
 <div class="metadata"><?php 
   $trust = $node->field_quickstart_trust[0]; 
   if ($trust['value'] == 'partner' || $trust['value'] == 'openshift') {
-    print "<span class='provider'>";
+    print "<span class='provider trusted'>";
   } else {
     print "<span class='provider'>";
   }
   print check_plain($trust['view']);
   ?></span>
-    <span class="divider"> | </span>
   <span class="created">Added by <?php print $node->name; ?> on <?php print readabledate($node->created); ?></span>
   <?php if ($node->comment_count > 0) { ?><span class="divider"> | </span><span class="comment-count"><?php print $node->comment_count; ?> comments</span><?php } ?>
-  <?php if ($taxonomy): ?>
-    <div class="terms">Tagged with: <?php print $terms ?></div>
-  <?php endif;?>
   </div>
 
 
   <div class="content clear-block">
     <?php print $node->content['body']['#value']; ?>
     <p class="action-quickstart">
-      <a class="pull-right btn btn-primary" href="<?php print openshift_server_url(); ?>/app/console/application_types/quickstart!<?php print check_plain($node->nid); ?>">Deploy Now!</a><?php 
+      <a class="btn btn-primary" href="<?php print openshift_server_url(); ?>/app/console/application_types/quickstart!<?php print check_plain($node->nid); ?>">Deploy Now!</a>
+      <span class="requires"><?php 
         print application_quickstarts_summary(
           $node->field_cartridges_list[0]['value'], 
           $node->field_git_repository_url[0]['value']);
-        ?></p>
+        ?></span>
+    </p>
     <?php print $field_code_language_rendered; ?>
     <?php print $field_website_rendered; ?>
+    <?php if ($taxonomy): ?>
+      <div class="terms">Tagged with: <?php print $terms ?></div>
+    <?php endif;?>
   </div>
 
   <div class="clear-block">
