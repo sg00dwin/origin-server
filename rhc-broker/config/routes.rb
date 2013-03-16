32b331684f56e5befabf06289f09109ea168bb63
Abhishek Gupta <abhgupta@redhat.com>
2013-03-15 18:19:47 -0700

    Removing keys resources since its covered in the origin-server

--- rhc-broker/config/routes.rb
+++ rhc-broker/config/routes.rb
@@ -1,59 +1,9 @@
 Broker::Application.routes.draw do
-  # Map all the actions on the home controller
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
   match 'nurture' => 'broker#nurture_post', :via => [:post]
 
   scope "/rest" do
-    resource :user, :only => [:show, :update, :destroy], :controller => :user_ext do
-      resources :keys, :controller => :keys, :constraints => { :id => /[\w]+/ } 
-    end
+    resource :user, :only => [:show, :update, :destroy], :controller => :user_ext
     resources :plans, :only => [:index, :show]
   end
-  
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  #root :to => "base#show"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
