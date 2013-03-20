22a2729cf0fa603e6226dfc7f19bcf2324368d44
Clayton Coleman <ccoleman@redhat.com>
2013-03-20 15:18:42 -0400

    Final tweaks, last round of items

--- drupal/drupal6-openshift-theme/nodes/node-quickstart.tpl.php
+++ drupal/drupal6-openshift-theme/nodes/node-quickstart.tpl.php
@@ -1,23 +1,28 @@
 <article id="node-<?php print $node->nid; ?>" class="quickstart node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> node-quickstart">
 
-<!-- <?php print_r($node); ?> -->
-<?php if ($node->field_quickstart_icon[0]){ ?><div class="pull-right"><?php print $node->field_quickstart_icon[0]['view']; ?></div><?php } ?>
-<h1 class="title"><?php print $title; ?></h1>
-<div class="metadata"><?php 
+  <?php if ($node->field_quickstart_icon[0]){ ?><div class="pull-right"><?php print $node->field_quickstart_icon[0]['view']; ?></div><?php } ?>
+  <h2 class="title"><?php print $title; ?></h2>
+  <div class="metadata"><?php 
   $trust = $node->field_quickstart_trust[0]; 
-  if ($trust['value'] == 'partner' || $trust['value'] == 'openshift') {
-    print "<span class='provider trusted'>";
-  } else {
-    print "<span class='provider'>";
+  if (!empty($trust['value'])) {
+
+    if ($trust['value'] == 'partner' || $trust['value'] == 'openshift') {
+      print "<span class='provider trusted'>";
+    } else {
+      print "<span class='provider'>";
+    }
+    print check_plain($trust['view']);
+
+    ?></span><?php
   }
-  print check_plain($trust['view']);
-  ?></span>
+  ?>
   <span class="created">Added by <?php print $node->name; ?> on <?php print readabledate($node->created); ?></span>
   <?php if ($node->comment_count > 0) { ?><span class="divider"> | </span><span class="comment-count"><?php print $node->comment_count; ?> comments</span><?php } ?>
   </div>
 
 
   <div class="content clear-block">
+    <div class="pull-right" style="margin-left: 10px;"><?php print $node->content['vud_node_widget_display']['#value']; ?></div>
     <?php print $node->content['body']['#value']; ?>
     <p class="action-quickstart">
       <a class="btn btn-primary" href="<?php print openshift_server_url(); ?>/app/console/application_types/quickstart!<?php print check_plain($node->nid); ?>">Deploy Now!</a>
