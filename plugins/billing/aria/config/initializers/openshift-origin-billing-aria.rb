e4f358c07a7af1d5af03447df86e257b3ae8258a
Clayton Coleman <ccoleman@redhat.com>
2013-03-26 15:02:42 -0400

    Remove test default config, billing config loaded in dev/test env

--- plugins/billing/aria/config/initializers/openshift-origin-billing-aria.rb
+++ plugins/billing/aria/config/initializers/openshift-origin-billing-aria.rb
@@ -1,15 +1,13 @@
 require 'openshift-origin-common'
 
 Broker::Application.configure do
-  unless Rails.env.test?
-    conf_file = File.join(OpenShift::Config::PLUGINS_DIR, File.basename(__FILE__, '.rb') + '.conf')
-    if Rails.env.development?
-      dev_conf_file = File.join(OpenShift::Config::PLUGINS_DIR, File.basename(__FILE__, '.rb') + '-dev.conf')
-      if File.exist? dev_conf_file
-        conf_file = dev_conf_file
-      else
-        Rails.logger.info "Development configuration for #{File.basename(__FILE__, '.rb')} not found. Using production configuration."
-      end
+  conf_file = File.join(OpenShift::Config::PLUGINS_DIR, File.basename(__FILE__, '.rb') + '.conf')
+  if Rails.env.development? or Rails.env.test?
+    dev_conf_file = File.join(OpenShift::Config::PLUGINS_DIR, File.basename(__FILE__, '.rb') + '-dev.conf')
+    if File.exist? dev_conf_file
+      conf_file = dev_conf_file
+    else
+      Rails.logger.info "Development configuration for #{File.basename(__FILE__, '.rb')} not found. Using production configuration."
     end
     conf = OpenShift::Config.new(conf_file)
 
@@ -90,6 +88,6 @@ Broker::Application.configure do
         }
       }
     }
-    config.billing = aria_billing_info
-  end
+  }
+  config.billing = aria_billing_info
 end
