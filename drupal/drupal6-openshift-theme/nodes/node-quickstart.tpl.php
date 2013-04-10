181dc474ae762bd62a8edcbfecd5f819e86b79bb
Clayton Coleman <ccoleman@redhat.com>
2013-04-10 10:56:04 -0400

    Update quickstarts with correct icon sizes, fix typos and spelling errors.

--- drupal/drupal6-openshift-theme/nodes/node-quickstart.tpl.php
+++ drupal/drupal6-openshift-theme/nodes/node-quickstart.tpl.php
@@ -7,9 +7,9 @@
   if (!empty($trust['value'])) {
 
     if ($trust['value'] == 'partner' || $trust['value'] == 'openshift') {
-      print "<span class='provider trusted'>";
+      print "<span class='provider trusted' title='This QuickStart is provided by OpenShift or a partner.'>";
     } else {
-      print "<span class='provider'>";
+      print "<span class='provider' title='This QuickStart is provided by the community - please review before deploying.'>";
     }
     print check_plain($trust['view']);
 
@@ -20,12 +20,12 @@
   <?php if ($node->comment_count > 0) { ?><span class="divider"> | </span><span class="comment-count"><?php print $node->comment_count; ?> comments</span><?php } ?>
   </div>
 
+  <p><a class="btn btn-primary" href="<?php print openshift_server_url(); ?>/app/console/application_types/quickstart!<?php print check_plain($node->nid); ?>">Deploy Now</a></p>
 
   <div class="content clear-block">
     <div class="pull-right" style="margin-left: 10px;"><?php print $node->content['vud_node_widget_display']['#value']; ?></div>
     <?php print $node->content['body']['#value']; ?>
     <p class="action-quickstart">
-      <a class="btn btn-primary" href="<?php print openshift_server_url(); ?>/app/console/application_types/quickstart!<?php print check_plain($node->nid); ?>">Deploy Now!</a>
       <span class="requires"><?php 
         print application_quickstarts_summary(
           $node->field_cartridges_list[0]['value'], 
