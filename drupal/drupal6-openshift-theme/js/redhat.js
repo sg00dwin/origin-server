1503a0fbebba8364f195504a9438e316bdde2520
Clayton Coleman <ccoleman@redhat.com>
2013-05-09 13:42:12 -0400

    Remove old resources that should not be used, remove comment about secret key.

--- drupal/drupal6-openshift-theme/js/redhat.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.kb-title a').click(function (e) {
-    e.preventDefault();
-    var toggleID = $(this).parents('div.kb-title').attr('id');
-    toggleID = toggleID.replace('kb-title-', '');
-    $('#kb-toggle-'+toggleID).slideToggle(300);
-  });
-});
