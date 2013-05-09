ea8ea4dc6b86ec3e89c2d890c26df2ee490090c0
William DeCoste <bdecoste@gmail.com>
2013-05-09 09:44:32 -0700

    as/eap v2 migration

--- rhc-node/mcollective/lib/migrate-v2-jbosseap-6.0.rb
+++ rhc-node/mcollective/lib/migrate-v2-jbosseap-6.0.rb
@@ -3,6 +3,19 @@ module OpenShiftMigration
     def post_process(user)
       output = "applying jbosseap-6.0 migration post-process\n"
 
+      Util.mv_env_var_value(user.homedir, 'OPENSHIFT_JBOSSEAP_PORT', 'OPENSHIFT_JBOSSEAP_HTTP_PORT')
+            
+      cartridge_dir = File.join(user.homedir, 'jbosseap')
+            
+      modules_jar = File.join(cartridge_dir, 'jboss-modules.jar')
+      modules_dir = File.join(cartridge_dir, 'modules')
+            
+      FileUtils.ln_s('/etc/alternatives/jbosseap-6.0/jboss-modules.jar', modules_jar)
+      FileUtils.ln_s('/etc/alternatives/jbosseap-6.0/modules', modules_dir)
+        
+      Util.make_user_owned(modules_jar, user)
+      Util.make_user_owned(modules_dir, user)
+
       output
     end
   end
