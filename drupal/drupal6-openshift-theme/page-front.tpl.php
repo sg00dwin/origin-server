073302fb61e128466782e144b6607e934f15b45c
Clayton Coleman <ccoleman@redhat.com>
2013-05-30 16:28:18 -0400

    Move content to blocks in site

--- drupal/drupal6-openshift-theme/page-front.tpl.php
+++ drupal/drupal6-openshift-theme/page-front.tpl.php
@@ -1,148 +1,18 @@
-<?php include 'page_header.inc' ?>
+<?php 
+  $block = module_invoke('block', 'block', 'view', '4'); 
+  $messaging = $block['content'];
+
+?><?php include 'page_header.inc' ?>
+
 <header>
   <?php include 'page_top.inc' ?>
   <?php include 'page_nav.inc' ?>
 </header>
+
 <div id="home" class="">
   <div class="container">
-    <section class="products" id="products">
-      <ul class="row-fluid">
-        <li class="span4 active online">
-          <div>
-            <a href="#" class="block">
-              <header>
-                <h1>Online</h1>
-                <h2>Public PaaS</h2>
-              </header>
-              <p>
-                OpenShift Online is a public cloud application development and hosting platform which leverages a Platform-as-a-Service (PaaS) architecture.
-              </p>
-            </a>
-            <a href="#" class="learn">Learn more</a>
-            <a href="#" class="cta">Sign up for free <span aria-hidden="true" data-icon="&#xe007;"> </span></a>
-          </div>
-        </li>
-        <li class="span4 enterprise">
-          <div>
-            <a href="#" class="block">
-              <header>
-                <h1>Enterprise</h1>
-                <h2>Private PaaS</h2>
-              </header>
-              <p>
-                OpenShift Enterprise offers the benefits of PaaS in an on-premise software product deployable in data-centers or private clouds.
-              </p>
-            </a>
-            <a href="#" class="learn">Learn more</a>
-            <a href="#" class="cta">Request evaluation <span aria-hidden="true" data-icon="&#xe007;"> </span></a>
-          </div>
-        </li>
-        <li class="span4 origin">
-          <div>
-            <a href="#" class="block">
-              <header>
-                <h1>Origin</h1>
-                <h2>Community PaaS</h2>
-              </header>
-              <p>
-                OpenShift Origin is the community-driven upstream code base that feeds RedHat's OpenShift Online &amp; Enterprise product offerings.
-              </p>
-            </a>
-            <a href="#" class="learn">Learn more</a>
-            <a href="#" class="cta">Join the community <span aria-hidden="true" data-icon="&#xe007;"> </span></a>
-          </div>
-        </li>
-      </ul>
-    </section>
-    <section class="redhat full-width" id="redhat">
-      <header>
-        <h1>Public and private PaaS by the open source leader</h1>
-      </header>
-      <img src="<?php print openshift_assets_url(); ?>/redhat.png" alt="Red Hat" />    
-      <p>
-        OpenShift is available in two different consumption models: the <a href="#">OpenShift Online</a> hosted service and the <a href="#">OpenShift Enterprise</a> on-premise PaaS software product.
-      </p>
-    </section>
-    <section>
-      <header>
-        <h1>Write your apps the way you want</h1>
-      </header>
-      <ul class="logos">
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-      </ul>
-    </section>
-    <section>
-      <header>
-        <h1>A rich ecosystem of trusted service providers</h1>
-      </header>
-      <ul class="logos">
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-        <li>
-          <img src="<?php print openshift_assets_url(); ?>/placeholder.png" alt="" />    
-        </li>
-      </ul>
-    </section>
+    <?php $block = module_invoke('block', 'block', 'view', '6'); print $block['content']; ?>
+
     <div id="buzz" class="section-base">
       <div class="container">
         <div class="row row-buzz">
@@ -153,44 +23,19 @@
               </h1>
               <hr>
               <div class="row-fluid">
-                <div id="buzz-testimonials" class="span5">
-                  <div class="testimonial">
-                    <blockquote>
-                      <p>
-                        With OpenShift, I can have a developer deploy her first line of business logic in less time than a meeting with an IT organization.
-                      </p>
-                      <small>Gregori Faroux</small>
-                    </blockquote>
-                  </div>
-                  <div class="testimonial">
-                    <blockquote>
-                      <p>
-                        Openshift has also a great community site in which you will learn new ways of doing what you have in mind with amazing articles put together by subject matter experts. Openshift is based on a gears-cartdriges architecture that you will love from the minute one of use.
-                      </p>
-                      <small>Germán Sánchez</small>
-                    </blockquote>
-                  </div>
-                </div>
+                <div id="buzz-testimonials" class="span5"><?php $block = module_invoke('block', 'block', 'view', '3'); print $block['content']; ?></div>
                 <div class="span1">&nbsp;</div>
-                <div id="buzz-testimonials2" class="span6">
-                  <div class="testimonial">
-                    <blockquote>
-                      <p>
-                        The best thing about OpenShift is the Community support that has been built up around it.
-                      </p>
-                      <small>Corey Daley</small>
-                    </blockquote>
-                  </div>
-                  <div class="testimonial">
-                    <blockquote>
-                      <p>
-                        Our vision was to reduce operational costs associated with maintaining configuration and updates with IaaS, while still allowing us much of the configuration flexibility. The Openshift service seemed to hit the sweet spot for us.
-                      </p>
-                      <small>Andy Lim</small>
-                    </blockquote>
-                  </div>
+                <div id="buzz-retweets" class="span6"><?php print _redhat_frontpage_load_retweets(); ?></div>
+              </div>
+
+              <div class="row-fluid buzz-actions">
+                <div class="span6"><a class="link-with-action" href="/products#why-openshift"><strong>Why</strong> OpenShift?</a></div>
+                <div class="span6">
+                  <div class="align-right"><a class="link-with-action" href="http://www.twitter.com/#!/openshift"><strong>Follow</strong> OpenShift</a></div>
+                  <div class="align-right"><a class="link-with-action" href="http://twitter.com/#!/search/%23OpenShift"><strong>More</strong> #OpenShift buzz</a></div>
                 </div>
               </div>
+
             </div>
           </div>
         </div>
@@ -198,7 +43,6 @@
     </div>
   </div>
 </div>
-<?php include 'page_footer.inc' ?>
 <script type="text/javascript">
   $.fn.setAllToMaxHeight = function(){
     return this.height( Math.max.apply(this, $.map( this , function(e){ return $(e).height() }) ) );
@@ -217,3 +61,4 @@
     });
   });
 </script>
+<?php include 'page_footer.inc' ?>
