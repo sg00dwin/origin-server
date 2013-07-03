562b5529bea40ea9e518c171f97dc6cb0c6cdfa3
Luke Meyer <lmeyer@redhat.com>
2013-07-03 11:10:57 -0400

    <broker> modify /broker routing out of passenger into routes.rb

--- rhc-broker/config/routes.rb
+++ rhc-broker/config/routes.rb
@@ -1,7 +1,7 @@
 Broker::Application.routes.draw do
   match 'nurture' => 'broker#nurture_post', :via => [:post]
 
-  scope "/rest" do
+  scope "/broker/rest" do
     resource :user, :only => [:show, :update, :destroy], :controller => :user_ext
     resources :plans, :only => [:index, :show]
   end
