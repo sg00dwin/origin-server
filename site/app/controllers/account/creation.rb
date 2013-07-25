3c31a035fd88ea49e0216d15c7aa2b905b1e1476
Jessica Forrester <jforrest@redhat.com>
2013-07-25 17:00:28 -0400

    Encode then param for app create redirect

--- site/app/controllers/account/creation.rb
+++ site/app/controllers/account/creation.rb
@@ -72,7 +72,7 @@ module Account
                                 :controller => 'email_confirm',
                                 :only_path => false,
                                 :protocol => 'https',
-                                :then => @redirect)
+                                :then => CGI::escape(@redirect))
 
       @user.register(confirmationUrl, @user.promo_code)
 
