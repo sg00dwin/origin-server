e0091849306eced5f6f4fa2cb46dd43145038102
Dan McPherson <dmcphers@redhat.com>
2013-08-24 18:58:16 -0400

    Syncing jenkins jobs

--- misc/trello/lib/bugzilla_helper.rb
+++ misc/trello/lib/bugzilla_helper.rb
@@ -4,7 +4,7 @@ require 'base64'
 class BugzillaHelper
   # Bugzilla Config
   attr_accessor :username, :password, :product
-  
+
   attr_accessor :bug
 
   def initialize(opts)
@@ -14,14 +14,14 @@ class BugzillaHelper
 
     xmlrpc = Bugzilla::XMLRPC.new("bugzilla.redhat.com")
     user = Bugzilla::User.new(xmlrpc)
-    
+
     user.login({'login'=>username, 'password'=>Base64.decode64(password), 'remember'=>true})
     @bug = Bugzilla::Bug.new(xmlrpc)
   end
-  
+
   def bug_status_by_url(url)
     status = 'NOTFOUND'
-    if url =~ /https:\/\/bugzilla\.redhat\.com\/show_bug\.cgi\?id=(\d+)/
+    if url =~ /https?:\/\/bugzilla\.redhat\.com\/show_bug\.cgi\?id=(\d+)/
       id = $1
       result = bug.get_bugs([id], ::Bugzilla::Bug::FIELDS_DETAILS)
       if !result.empty?
