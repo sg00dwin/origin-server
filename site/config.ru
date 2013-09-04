4798028cbe29d27dfc3d68c5661453b57d9621dc
Jessica Forrester <jforrest@redhat.com>
2013-09-04 11:00:56 -0400

    Conditionally monitor site and broker with New Relic

--- site/config.ru
+++ site/config.ru
@@ -1,5 +1,5 @@
 # This file is used by Rack-based servers to start the application.
-
+ENV["ENABLE_NR_MONITORING"] = "true"
 require ::File.expand_path('../config/environment',  __FILE__)
 run RedHatCloud::Application
 
---
+++ site/config/initializers/new_relic.rb
@@ -0,0 +1,14 @@
+begin
+  if File.exists?('/etc/openshift/newrelic.yml') && ENV["ENABLE_NR_MONITORING"]
+    ENV["NRCONFIG"] = "/etc/openshift/newrelic.yml"
+    require 'newrelic_rpm'
+    new_relic_conf = YAML.load_file(ENV["NRCONFIG"])
+    args = {}
+    args[:app_name] = 'site-' + new_relic_conf["common"]["app_name"]
+    args[:log_file_name] = "site-" + new_relic_conf["common"]["log_file_name"]
+    NewRelic::Agent.manual_start(args)  
+  end
+rescue Exception => e
+  Rails.logger.error "Unable to start New Relic: #{e.message}"
+  Rails.logger.error e.backtrace.inspect  
+end
