ade2c0cf3696142e8cae87e881d1e2c5a0e8b973
Jessica Forrester <jforrest@redhat.com>
2013-09-11 13:55:27 -0400

    Instrument broker mco proxy methods to determine time spent on node

--- rhc-broker/config/initializers/new_relic.rb
+++ rhc-broker/config/initializers/new_relic.rb
@@ -7,7 +7,24 @@ begin
     args = {}
     args[:app_name] = 'broker-' + new_relic_conf["common"]["app_name"]
     args[:log_file_name] = "broker-" + new_relic_conf["common"]["log_file_name"]
-    NewRelic::Agent.manual_start(args)  
+    NewRelic::Agent.manual_start(args)
+
+    OpenShift::ApplicationContainerProxy.class_eval do
+      include NewRelic::Agent::MethodTracer
+
+      self.singleton_methods(false).each do |m|
+        add_method_tracer m if m.to_s.match(/^provider=$|^instance$|.*_impl\??$/).nil?
+      end
+
+      @proxy_provider.class_eval do
+        include NewRelic::Agent::MethodTracer
+
+        self.public_instance_methods(false).each do |m|
+          add_method_tracer m if m.to_s.match(/^id$|\=$|^build_base_/).nil?
+        end
+      end
+
+    end
   end
 rescue Exception => e
   Rails.logger.error "Unable to start New Relic: #{e.message}"
